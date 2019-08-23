da0ec3c3598143e917dc676c3e00afcd63fffc4c
mine <duhuawei@beyondsoft.com>
2019-08-23 13:29:25 +0800

    数据源新增功能及测试类

--- hrds_A/src/main/java/hrds/biz/B0101/DataSourceAction.java
+++ hrds_A/src/main/java/hrds/biz/B0101/DataSourceAction.java
@@ -5,7 +5,12 @@ import fd.ng.web.annotation.RequestBean;
 import fd.ng.web.util.Dbo;
 import hrds.entity.SourceRelationDep;
 import hrds.entity.DataSource;
+import hrds.entity.SysPara;
 import hrds.exception.BusinessException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.math.BigDecimal;
 
 /**
  * DataSource class
@@ -14,17 +19,98 @@ import hrds.exception.BusinessException;
  * @date 2019-08-22 16:29:19
  */
 public class DataSourceAction extends AbstractWebappBaseAction {
+    private static final Logger logger = LogManager.getLogger();
 
-    public  void  addDataSource(@RequestBean SourceRelationDep dataSource, @RequestBean DataSource sourceRelationDep){
+    /**
+     * 新增数据源
+     *
+     * @param dataSource 数据源编号
+     * @param dep_id     部门编号
+     */
+    public void addDataSource(@RequestBean DataSource dataSource, String[] dep_id) {
         // 新增数据源
-        if (dataSource.add(Dbo.db("local"))!=1) {
-            throw new BusinessException("添加数据失败！data="+dataSource);
+        if (dataSource.add(Dbo.db()) != 1) {
+            throw new BusinessException("添加数据失败！dataSource=" + dataSource);
         }
 
         // 新增数据源与部门关系
-        if (sourceRelationDep.add(Dbo.db("local"))!=1) {
-            throw new BusinessException("添加数据失败！data="+sourceRelationDep);
+        for (String depId : dep_id) {
+            SourceRelationDep srd = new SourceRelationDep();
+            srd.setSource_id(dataSource.getSource_id());
+            srd.setDep_id(new BigDecimal(depId));
+            if (srd.add(Dbo.db()) != 1) {
+                throw new BusinessException("添加数据失败！srd=" + srd);
+            }
         }
+
+    }
+
+    /**
+     * 更新数据源
+     *
+     * @param source_id       数据源编号
+     * @param dep_id          部门编号
+     * @param datasource_name 数据源名称
+     * @param source_remark   数据源描述
+     */
+    public void updateDataSource(String source_id, String[] dep_id, String datasource_name, String source_remark) {
+        DataSource dataSource = new DataSource();
+        dataSource.setSource_id(new BigDecimal(source_id));
+        dataSource.setDatasource_name(datasource_name);
+        dataSource.setSource_remark(source_remark);
+        // 更新数据源信息
+        if (dataSource.update(Dbo.db()) != 1) {
+            throw new BusinessException(String.format("更新数据失败！datasource_name=%s, source_remark=%s", datasource_name, source_remark));
+        }
+
+        // 更新数据源与部门关系表信息
+        SourceRelationDep srd = new SourceRelationDep();
+        srd.setSource_id(new BigDecimal(source_id));
+        for (String id : dep_id) {
+            // 先删除关系，再建立新关系
+            deleteSourceRelationDep(source_id);
+            srd.setDep_id(new BigDecimal(id));
+            if (srd.add(Dbo.db()) != 1) {
+                throw new BusinessException("添加数据失败！srd=" + srd);
+            }
+        }
+    }
+
+    /**
+     * 删除数据源与部门关系表信息
+     *
+     * @param source_id 数据源编号
+     */
+    public void deleteSourceRelationDep(String source_id) {
+        // 删除data_source表信息
+        int num = Dbo.execute("delete from " + SourceRelationDep.TableName + " where source_id=?", source_id);
+        if (num != 1) {
+            if (num == 0) {
+                throw new BusinessException(String.format("没有数据被删除！source_id=%s", source_id));
+            } else {
+                throw new BusinessException(String.format("删除数据异常！source_id=%s", source_id));
+            }
+        }
+    }
+
+    /**
+     * 删除数据源信息
+     *
+     * @param source_id 数据源编号
+     */
+    public void deleteDataSource(String source_id) {
+        // 删除data_source表信息
+        int num = Dbo.execute("delete from " + DataSource.TableName + " where source_id=?", source_id);
+        if (num != 1) {
+            if (num == 0) {
+                throw new BusinessException(String.format("没有数据被删除！source_id=%s", source_id));
+            } else {
+                throw new BusinessException(String.format("删除数据异常！source_id=%s", source_id));
+            }
+        }
+
+        // 删除source_relation_dep信息
+        deleteSourceRelationDep(source_id);
     }
 
 }
--- hrds_A/src/test/java/hrds/biz/B0101/DataSourceActionTest.java
+++ hrds_A/src/test/java/hrds/biz/B0101/DataSourceActionTest.java
@@ -4,51 +4,143 @@ import fd.ng.core.utils.DateUtil;
 import fd.ng.core.utils.JsonUtil;
 import fd.ng.db.jdbc.DatabaseWrapper;
 import fd.ng.db.jdbc.SqlOperator;
+import fd.ng.db.resultset.Result;
 import fd.ng.netclient.http.HttpClient;
 import fd.ng.web.action.ActionResult;
+import hrds.entity.DataSource;
 import hrds.entity.SourceRelationDep;
 import hrds.testbase.WebBaseTestCase;
-import org.hamcrest.MatcherAssert;
-
+import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.OptionalLong;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class DataSourceActionTest extends WebBaseTestCase {
-    private static final String UATTableName = SourceRelationDep.TableName;
-    private static final int Init_Rows = 10; // 向表中初始化的数据条数。
+    private static final String SRDTableName = SourceRelationDep.TableName;
+
+    private static final String DSTableName = DataSource.TableName;
 
     @Test
     public void addDataSource() {
-        String source_id = "1234567890";
+        int source_id = 1000000001;
         String datasource_name = "ceshi";
         String create_date = DateUtil.getSysDate();
         String create_time = DateUtil.getDateTime();
-        String user_id = "1001";
-        String bodyString = new HttpClient().addData("source_id", source_id).addData("datasource_name", datasource_name).addData("create_date", create_date).addData("create_time", create_time).addData("user_id", user_id).post(getActionUrl("addDataSource")).getBodyString();
-        ActionResult ar = JsonUtil.toObject(bodyString, ActionResult.class);
-        MatcherAssert.assertThat(ar.isSuccess(), is(true));
+        int user_id = 1001;
+        String source_remark = "测试";
+        String[] dep_id = {"1000000001","1000000002","1000000003"};
+        String responseValue = new HttpClient().addData("source_id", source_id).addData("datasource_name", datasource_name).addData("create_date", create_date).addData("create_time", create_time).addData("user_id", user_id).addData("source_remark", source_remark).addData("dep_id", dep_id).post(getActionUrl("addDataSource")).getBodyString();
+        ActionResult ar = JsonUtil.toObject(responseValue, ActionResult.class);
+        assertThat(ar.isSuccess(), is(true));
 
         // 验证DB里面的数据是否正确
         try (DatabaseWrapper db = new DatabaseWrapper.Builder().dbname("local").create()) {
             Map<String, Object> result = SqlOperator.queryOneObject(db,
-                    "select * from " + UATTableName + " where source_id=?", source_id);
-            String new_datasource_name = (String)result.get("datasource_name");
-            String new_create_date = (String)result.get("create_date");
-            String new_create_time = (String)result.get("create_time");
-            String new_user_id = (String)result.get("user_id");
+                    "select * from " + DSTableName + " where source_id=?", source_id);
+            String new_datasource_name = (String) result.get("datasource_name");
+            String new_create_date = (String) result.get("create_date");
+            String new_create_time = (String) result.get("create_time");
+            String new_user_id = (String) result.get("user_id");
 
             assertThat(datasource_name, is(new_datasource_name));
             assertThat(create_date, is(new_create_date));
             assertThat(create_time, is(new_create_time));
             assertThat(user_id, is(new_user_id));
 
+            for (String depId : dep_id) {
+                Map<String, Object> SRDResult = SqlOperator.queryOneObject(db,
+                        "select * from " + SRDTableName + " where dep_id=?", depId);
+                int new_source_id = Integer.parseInt((String) result.get("source_id"));
+                assertThat(source_id, is(new_source_id));
+            }
         }
+    }
 
+    @Test
+    public void updateDataSource() {
+        String source_id="1000000001";
+        String[] dep_id={"1000000001","1000000002"};
+        String datasource_name="ceshi2";
+        String source_remark="测试2";
+        String responseValue = new HttpClient()
+                .addData("datasource_name", datasource_name).addData("source_id", source_id).addData("dep_id",dep_id).addData("source_remark",source_remark)
+                .post(getActionUrl("updateDataSource"))
+                .getBodyString();
+        ActionResult ar = JsonUtil.toObject(responseValue, ActionResult.class);
+        assertThat(ar.isSuccess(), is(true));
 
+        // 验证DB里面的数据是否正确
+        try (DatabaseWrapper db = new DatabaseWrapper()) {
+            // 验证data_source表信息
+            Map<String, Object> result = SqlOperator.queryOneObject(db,
+                    "select * from " + DataSource.TableName + " where source_id=?", source_id);
+            String new_datasource_name = (String) result.get("datasource_name");
+            String new_source_remark = (String) result.get("source_remark");
+            assertThat(datasource_name, is(new_datasource_name));
+            assertThat(source_remark, is(new_source_remark));
+
+            // 验证source_relation_dep表信息
+            Result rs = SqlOperator.queryResult(db,
+                    "select * from " + SourceRelationDep.TableName + " where source_id=?", source_id);
+            for (int i = 0; i < rs.getRowCount(); i++) {
+                BigDecimal new_dep_id =  rs.getBigDecimal(i,"dep_id");
+                assertThat(datasource_name, is(new_datasource_name));
+                assertThat(dep_id, is(new_dep_id));
+            }
+        }
+    }
+
+    @Test
+    public void deleteDataSource() {
+        try (DatabaseWrapper db = new DatabaseWrapper()) {
+            String source_id="1000000001";
+            // 验证DB里面预期被删除的数据是存在的
+            OptionalLong result = SqlOperator.queryNumber(db,
+                    "select count(1) from " + DataSource.TableName + " where source_id=?", source_id);
+            assertThat(result.orElse(Long.MIN_VALUE), is(1L)); // 被删除前为1
+
+            // 业务处理
+            String responseValue = new HttpClient()
+                    .addData("source_id", source_id)
+                    .post(getActionUrl("deleteDataSource"))
+                    .getBodyString();
+            ActionResult ar = JsonUtil.toObject(responseValue, ActionResult.class);
+            assertThat(ar.isSuccess(), is(true));
+
+            // 验证DB里面的数据是否正确
+            result = SqlOperator.queryNumber(db,
+                    "select count(1) from " + DataSource.TableName + " where source_id=?", source_id);
+            assertThat(result.orElse(Long.MIN_VALUE), is(0L)); // 被删除了所以为0
+        }
+    }
+
+    public void deleteSourceRelationDep(){
+        try (DatabaseWrapper db = new DatabaseWrapper()) {
+            String source_id="1000000001";
+            // 验证DB里面预期被删除的数据是存在的
+            OptionalLong result = SqlOperator.queryNumber(db,
+                    "select count(1) from " + SourceRelationDep.TableName + " where source_id=?", source_id);
+            assertThat(result.orElse(Long.MIN_VALUE), is(3L)); // 被删除前为3
+            // 业务处理
+            String responseValue = new HttpClient()
+                    .addData("source_id", source_id)
+                    .post(getActionUrl("deleteSourceRelationDep"))
+                    .getBodyString();
+            ActionResult ar = JsonUtil.toObject(responseValue, ActionResult.class);
+            assertThat(ar.isSuccess(), is(true));
+
+            // 验证DB里面的数据是否正确
+            result = SqlOperator.queryNumber(db,
+                    "select count(1) from " + SourceRelationDep.TableName + " where source_id=?", source_id);
+            assertThat(result.orElse(Long.MIN_VALUE), is(0L)); // 被删除了所以为0
+        }
     }
 
 }
