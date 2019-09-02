b0aa38832e05aedd2c831b01e3991945198bceae
Qiang-Lee <hliqiang1989@gmail.com>
2019-09-02 19:44:14 +0800

    修改用户登陆验证

--- hrds_A/src/main/java/hrds/a/biz/login/LoginAction.java
+++ hrds_A/src/main/java/hrds/a/biz/login/LoginAction.java
@@ -1,6 +1,9 @@
 package hrds.a.biz.login;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import fd.ng.core.utils.StringUtil;
+import fd.ng.web.action.ActionResult;
 import fd.ng.web.util.Dbo;
 import hrds.commons.base.BaseAction;
 import hrds.commons.entity.Department_info;
@@ -10,7 +13,8 @@ import hrds.commons.exception.ExceptionEnum;
 import hrds.commons.utils.ActionUtil;
 import hrds.commons.utils.User;
 
-import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * <p>标    题: 海云数服 V5.0</p>
@@ -24,6 +28,50 @@ import java.util.Optional;
 public class LoginAction extends BaseAction {
 
 	/**
+	 * <p>方法描述: 用户登陆入口</p>
+	 * <p>@author: Mr.Lee </p>
+	 * <p>创建时间: 2019-09-02</p>
+	 * <p>参   数:  </p>
+	 * <p>return:  </p>
+	 */
+	public void login(HttpServletRequest request) {
+
+		_doPreProcess(request);
+	}
+
+	/**
+	 * <p>方法描述: 重写登陆时的用户验证情况</p>
+	 * <p>@author: Mr.Lee </p>
+	 * <p>创建时间: 2019-09-02</p>
+	 * <p>参   数:  </p>
+	 * <p>return:  </p>
+	 */
+	@Override
+	protected ActionResult _doPreProcess(HttpServletRequest request) {
+
+		/*
+		 * 1 : 用户名为空
+		 * 2 : 密码为空
+		 */
+		String user_id = request.getParameter("username");
+		System.out.println("==========>" + user_id);
+
+		//1 : 用户名为空
+		if( StringUtil.isBlank(user_id + "") ) {
+			throw new BusinessException(ExceptionEnum.USER_NOT_EMPTY.getMessage());
+		}
+		String pwd = request.getParameter("password");
+		System.out.println("==============" + pwd);
+		//2 : 密码为空
+		if( StringUtil.isBlank(pwd) ) {
+			throw new BusinessException(ExceptionEnum.USER_PWD_EMPTY.getMessage());
+		}
+		checkLogin(Integer.parseInt(user_id), pwd);
+
+		return null;
+	}
+
+	/**
 	 * <p>方法描述: 检查当前用户的登陆是否正确</p>
 	 * <p>@author: Mr.Lee </p>
 	 * <p>创建时间: 2019-09-02</p>
@@ -33,37 +81,25 @@ public class LoginAction extends BaseAction {
 	public void checkLogin(int user_id, String pwd) {
 
 		/*
-		 * 1 : 用户名为空
-		 * 2 : 密码为空
-		 * 3 : 查询当前用户的记录信息
-		 * 4 : 如果为获取到信息,则表示当前用户的信息不存在
-		 * 5 : 设置登陆用户的cookie
+		 * 1 : 查询当前用户的记录信息
+		 * 2 : 如果为获取到信息,则表示当前用户的信息不存在
+		 * 3 : 设置登陆用户的cookie
 		 */
-		//1 : 用户名为空
-		if( StringUtil.isBlank(user_id + "") ) {
-			throw new BusinessException(ExceptionEnum.USER_NOT_EMPTY.getMessage());
-		}
 
-		//2 : 密码为空
-		if( StringUtil.isBlank(pwd) ) {
-			throw new BusinessException(ExceptionEnum.USER_PWD_EMPTY.getMessage());
-		}
-
-		//3 : 查询当前用户的记录信息
-		Optional<Sys_user> sys_user = Dbo.queryOneObject(Sys_user.class, "select * from " + Sys_user.TableName + " where user_id = ?", user_id);
-		Sys_user logInUser = sys_user.get();
-
-		//4 : 如果为获取到信息,则表示当前用户的信息不存在
+		//1 : 查询当前用户的记录信息
+		Map<String, Object> stringObjectMap = Dbo.queryOneObject("select * from " + Sys_user.TableName + " where user_id = ?", user_id);
+		Sys_user logInUser = (Sys_user)JSON.toJavaObject(JSONObject.parseObject(JSON.toJSONString(stringObjectMap)), Sys_user.class);
+		//2 : 如果为获取到信息,则表示当前用户的信息不存在
 		if( StringUtil.isBlank(logInUser.getUser_id() + "") ) {
 			throw new BusinessException(ExceptionEnum.USER_NOE_EXISTS.getMessage());
 		}
 		else {
 			String user_password = logInUser.getUser_password();
-			if( pwd.equals(user_password) ) {
+			if( !pwd.equals(user_password) ) {
 				throw new BusinessException(ExceptionEnum.PASSWORD_ERROR.getMessage());
 			}
 			else {
-				//5 : 设置登陆用户的cookie
+				//3 : 设置登陆用户的cookie
 				User user = putUserInfo(logInUser);
 				ActionUtil.setCookieUser(user);
 			}
@@ -79,10 +115,11 @@ public class LoginAction extends BaseAction {
 		 */
 
 		// 1 : 根据登陆成功的用户信息获取部门信息
-		Optional<Department_info> department = Dbo
-						.queryOneObject(Department_info.class, "select * from " + Department_info.TableName + " where dep_id = ?;",
-										logInUser.getDep_id());
-		Department_info department_info = department.get();
+
+		Map<String, Object> department = Dbo.queryOneObject("select * from " + Department_info.TableName + " where dep_id = ?",
+						logInUser.getDep_id());
+		Department_info department_info = (Department_info)JSON
+						.toJavaObject(JSONObject.parseObject(JSON.toJSONString(department)), Department_info.class);
 		//2 : 组成需要生成的Cookie
 		User user = new User();
 		user.setUserId(logInUser.getUser_id());
