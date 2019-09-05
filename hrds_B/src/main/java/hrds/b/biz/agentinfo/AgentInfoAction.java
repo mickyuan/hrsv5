c3666e62680cda769833e7543ebf1fd97d8bfdec
mine <duhuawei@beyondsoft.com>
2019-09-05 11:14:13 +0800

    数据源与agent增删改修改

---
+++ hrds_B/src/main/java/hrds/b/biz/agentinfo/AgentInfoAction.java
@@ -0,0 +1,118 @@
+package hrds.b.biz.agentinfo;
+
+import fd.ng.db.resultset.Result;
+import fd.ng.netclient.http.HttpClient;
+import fd.ng.web.annotation.RequestBean;
+import fd.ng.web.util.Dbo;
+import hrds.commons.base.BaseAction;
+import hrds.commons.entity.Agent_info;
+import hrds.commons.exception.BusinessException;
+import hrds.commons.exception.ExceptionEnum;
+import hrds.commons.utils.ActionUtil;
+import hrds.commons.utils.key.PrimayKeyGener;
+
+import java.util.Map;
+
+/**
+ * agent增删改类
+ *
+ * @author mine
+ * @date 2019-09-04 17:30:27
+ */
+public class AgentInfoAction extends BaseAction {
+    /**
+     * 保存agent信息
+     * <p>
+     * 1.判断端口是否被占用，被占用抛异常，否则正常保存
+     * 2.判断agent编号是否为空，为空则新增，不为空则编辑
+     * 3.保存或更新agent信息
+     *
+     * @param agentInfo agent实体对象
+     * @return
+     */
+    public void saveAgent(@RequestBean Agent_info agentInfo) {
+        // 1.判断端口是否被占用
+        boolean flag = isPortOccupied(agentInfo.getAgent_ip(), Integer.parseInt(agentInfo.getAgent_port()));
+        if (flag) {
+            // 端口被占用不可使用
+            throw new BusinessException("端口被占用");
+        }
+        // 2.判断agent编号是否为空
+        if (agentInfo.getAgent_id() == null) {
+            // 2.为空，新增
+            agentInfo.setSource_id(PrimayKeyGener.getNextId());
+            agentInfo.setUser_id(ActionUtil.getUser().getUserId());
+            // 3.保存agent信息
+            if (agentInfo.add(Dbo.db()) != 1) {
+                throw new BusinessException(ExceptionEnum.DATA_ADD_ERROR);
+            }
+        } else {
+            // 2.不为空，编辑
+            // 3.更新agent信息
+            if (agentInfo.update(Dbo.db()) != 1) {
+                throw new BusinessException(ExceptionEnum.DATA_UPDATE_ERROR);
+            }
+        }
+
+    }
+
+    /**
+     * 监控agent端口是否被占用
+     * <p>
+     * 1.通过http方式去测试端口连通情况，测通则被占用，不通则可以使用
+     *
+     * @param ip
+     * @param port
+     * @return
+     */
+    public boolean isPortOccupied(String ip, int port) {
+
+        // 1.通过http方式去测试端口连通情况，测通则被占用，不通则可以使用
+        HttpClient httpClient = new HttpClient();
+        String url = "http://".concat(ip).concat(":").concat(port + "");
+        HttpClient.ResponseValue post = httpClient.post(url);
+
+        if (post.getCode() != 200) {
+            // 未连通，端口可用
+            return false;
+        } else {
+            // 连通，端口被使用中
+            return true;
+        }
+    }
+
+    /**
+     * 删除agent
+     * <p>
+     * 1.删除前查询此agent是否已部署，已部署不能删除
+     * 2.判断此数据源与agent下是否有任务，有任务不能删除
+     * 3.删除agent
+     *
+     * @param agent_id
+     * @param agent_type
+     */
+    public void deleteAgent(Long agent_id, String agent_type) {
+
+        // 1.删除前查询此agent是否已部署
+        Map<String, Object> map = Dbo.queryOneObject("select * from agent_down_info where agent_id=?", agent_id);
+        if ("0".equals(map.get("deploy"))) {
+            // 此agent已部署不能删除
+            throw new BusinessException("此agent已部署不能删除");
+        }
+        // 2.判断此数据源与agent下是否有任务
+        Result result = Dbo.queryResult(" SELECT task_name FROM agent_info t1 join database_set t2 on t1.agent_id=t2.agent_id WHERE" +
+                "  t1.agent_id=? and  t1.agent_type=?", agent_id, agent_type);
+        if (!result.isEmpty()) {
+            // 此数据源与agent下有任务，不能删除
+            throw new BusinessException("此数据源与agent下有任务，不能删除");
+        }
+
+        // 3.删除agent
+        int num = Dbo.execute("delete  from agent_info where agent_id=?", agent_id);
+        if (num != 1) {
+            throw new BusinessException(ExceptionEnum.DATA_DELETE_ERROR);
+        }
+    }
+
+}
+
--- hrds_B/src/main/java/hrds/b/biz/datasource/DataSourceAction.java
+++ hrds_B/src/main/java/hrds/b/biz/datasource/DataSourceAction.java
@@ -1,12 +1,9 @@
 package hrds.b.biz.datasource;
 
 import fd.ng.db.resultset.Result;
-import fd.ng.netclient.http.HttpClient;
 import fd.ng.web.annotation.RequestBean;
-import fd.ng.web.annotation.RequestParam;
 import fd.ng.web.util.Dbo;
 import hrds.commons.base.BaseAction;
-import hrds.commons.entity.Agent_info;
 import hrds.commons.entity.Data_source;
 import hrds.commons.entity.Source_relation_dep;
 import hrds.commons.exception.BusinessException;
@@ -17,7 +14,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * DataSource Class
+ * 数据源增删改，导入、下载类
  *
  * @author mine
  * @date 2019-09-03 16:44:25
@@ -30,51 +27,55 @@ public class DataSourceAction extends BaseAction {
      * <p>
      * 1.判断数据源编号是否为空，为空则为新增，不为空则为编辑
      * 2.新增前查询数据源编号是否已存在，存在则抛异常，不存在就新增
-     * 3.保存数据源与部门关系信息
+     * 3.保存或更新数据源信息
+     * 4.如果是编辑先删除数据源与部门关系
+     * 5.保存或更新数据源与部门关系信息
      *
      * @param dataSource 数据源编号
      */
     public void saveDataSource(@RequestBean Data_source dataSource, String dep_id) {
 
-        // 新增数据源
+        // 1.判断数据源编号是否为空
         if (dataSource.getSource_id() == null) {
             // 新增
             dataSource.setSource_id(PrimayKeyGener.getNextId());
             dataSource.setUser_id(ActionUtil.getUser().getUserId());
-            // 新增前查询数据源编号是否已存在
+            // 2.新增前查询数据源编号是否已存在
             Result result = Dbo.queryResult("select datasource_number from " + Data_source.TableName +
                     "  where datasource_number=?", dataSource.getDatasource_number());
             if (!result.isEmpty()) {
                 // 数据源编号重复
                 throw new BusinessException("数据源编号重复");
-            } else {
-                if (dataSource.add(Dbo.db()) != 1) {
-                    // 新增保存失败
-                    throw new BusinessException(ExceptionEnum.DATA_ADD_ERROR);
-                }
             }
+            // 3.保存数据源信息
+            if (dataSource.add(Dbo.db()) != 1) {
+                // 新增保存失败
+                throw new BusinessException(ExceptionEnum.DATA_ADD_ERROR);
+            }
+
         } else {
             // 编辑
+            // 3.更新数据源信息
             if (dataSource.update(Dbo.db()) != 1) {
                 // 编辑保存失败
                 throw new BusinessException(ExceptionEnum.DATA_UPDATE_ERROR);
             }
 
-            // 先删除数据源与部门关系信息
+            // 4.先删除数据源与部门关系信息
             int num = Dbo.execute("delete from " + Source_relation_dep.TableName +
                     " where source_id=?", dataSource.getSource_id());
             if (num != 1) {
                 throw new BusinessException(ExceptionEnum.DATA_DELETE_ERROR);
             }
         }
-        // 保存数据源与部门关系信息
+        // 5.保存或更新数据源与部门关系信息
         saveSourceRelationDep(dataSource.getSource_id(), dep_id);
     }
 
     /**
      * 保存数据源与部门关系表信息
      * <p>
-     * 1.建立数据源与部门关系信息
+     * 1.循环保存或更新数据源与部门关系信息
      *
      * @param source_id 数据源编号
      * @param dep_id    部门编号
@@ -85,8 +86,9 @@ public class DataSourceAction extends BaseAction {
         Source_relation_dep srd = new Source_relation_dep();
         srd.setSource_id(source_id);
         String[] depIds = dep_id.split(",");
+        // 1.循环保存或更新数据源与部门关系信息
         for (String depId : depIds) {
-            srd.setDep_id(Long.parseLong(dep_id));
+            srd.setDep_id(Long.parseLong(depId));
             if (srd.add(Dbo.db()) != 1) {
                 throw new BusinessException(ExceptionEnum.DATA_ADD_ERROR);
             }
@@ -96,39 +98,36 @@ public class DataSourceAction extends BaseAction {
     /**
      * 编辑前根据数据源编号查询数据源及数据源与部门关系信息
      * <p>
-     * 1.判断数据源编号是否为空，不为空则抛异常
-     * 2.查询该数据源下的数据源及数据源与部门关系信息
-     * 3.判断该数据源下是否有数据，没有抛异常，有则返回查询结果
+     * 1.判断该数据源下是否有数据，没有抛异常，有则返回查询结果
      *
      * @param source_id 数据源编号
      * @return
      */
     public Result searchDataSource(Long source_id) {
-        // 查询该数据源下的数据源及数据源与部门关系信息
+        // 1.判断该数据源下是否有数据，没有抛异常，有则返回查询结果
         Result result = Dbo.queryResult("select ds.*,srd.dep_id from data_source ds " +
                 "join source_relation_dep srd on ds.source_id=srd.source_id where ds.source_id = ?", source_id);
         if (result.isEmpty()) {
             // 该数据源下数据为空(此为编辑情况下数据不能为空）
             throw new BusinessException(ExceptionEnum.DATA_NOT_EXIST);
-        } else {
-            // 不为空，返回查询结果
-            return result;
         }
+        // 不为空，返回查询结果
+        return result;
 
     }
 
     /**
      * 删除数据源与部门关系表信息
      * <p>
-     * 1.删除数据源与部门关系表信息
-     * 2.失败就抛异常，否则就正常删除
+     * 1.删除数据源与部门关系表信息，失败就抛异常，否则就正常删除
      *
      * @param source_id 数据源编号
      */
     public void deleteSourceRelationDep(Long source_id) {
-        // 删除数据源与部门关系表信息
+        // 1.删除数据源与部门关系表信息，
         int num = Dbo.execute("delete from " + Source_relation_dep.TableName + " where source_id=?", source_id);
         if (num != 1) {
+            // 删除失败
             throw new BusinessException(ExceptionEnum.DATA_DELETE_ERROR);
         }
     }
@@ -143,76 +142,105 @@ public class DataSourceAction extends BaseAction {
      */
     public void deleteDataSource(Long source_id) {
 
-        // 先查询该datasource下是否还有agent,有不能删除，没有，可以删除
+        // 1。先查询该datasource下是否还有agent
         Result result = Dbo.queryResult("SELECT * FROM agent_info WHERE source_id=? ", source_id);
         if (!result.isEmpty()) {
             // 此数据源下还有agent，不能删除
             throw new BusinessException("此数据源下还有agent，不能删除");
         }
 
-        // 删除data_source表信息
+        // 2.删除data_source表信息
         int num = Dbo.execute("delete from " + Data_source.TableName + " where source_id=?", source_id);
         if (num != 1) {
+            // 删除失败
             throw new BusinessException(ExceptionEnum.DATA_DELETE_ERROR);
         }
-        // 删除source_relation_dep信息
+        // 3.删除source_relation_dep信息
         deleteSourceRelationDep(source_id);
 
     }
 
     /**
-     * 保存agent信息
-     * <p>
-     * 1.判断端口是否被占用，被占用抛异常，否则正常保存
-     * 2.判断agent编号是否为空，为空则新增，不为空则编辑
-     *
-     * @param agentInfo agent实体对象
-     * @return
+     * 导入数据源
      */
-    public void saveAgent(@RequestBean Agent_info agentInfo) {
-        boolean flag = isPortOccupied(agentInfo.getAgent_ip(), Integer.parseInt(agentInfo.getAgent_port()));
-        if (flag) {
-            // 端口不可使用
-            throw new BusinessException("端口被占用");
-        }
-        if (agentInfo.getAgent_id() == null) {
-            // 新增
-            agentInfo.setSource_id(PrimayKeyGener.getNextId());
-            agentInfo.setUser_id(ActionUtil.getUser().getUserId());
-            if (agentInfo.add(Dbo.db()) != 1) {
-                throw new BusinessException(ExceptionEnum.DATA_ADD_ERROR);
-            }
-        } else {
-            // 编辑
-            if (agentInfo.update(Dbo.db()) != 1) {
-                throw new BusinessException(ExceptionEnum.DATA_UPDATE_ERROR);
-            }
-        }
-
-    }
-
-    /**
-     * 监控agent端口是否被占用
-     * <p>
-     * 1.通过http方式去测试端口连通情况，测通则被占用，不通则可以使用
-     *
-     * @param ip
-     * @param port
-     * @return
-     */
-    public boolean isPortOccupied(String ip, int port) {
-
-        HttpClient httpClient = new HttpClient();
-        String url = "http://".concat(ip).concat(":").concat(port + "");
-        HttpClient.ResponseValue post = httpClient.post(url);
-
-        if (post.getCode() != 200) {
-            // 未连通，端口可用
-            return false;
-        } else {
-            // 连通，端口被使用中
-            return true;
-        }
-    }
+    //public void upLoadFileSDO() {
+    //    String ip = "";
+    //    String port = "";
+    //    String userId = "";
+    //    StringBuffer temp = new StringBuffer();
+    //    //消息提示
+    //    try {
+    //        //使用Apache文件上传组件处理文件上传步骤：
+    //        //1、创建一个DiskFileItemFactory工厂
+    //        DiskFileItemFactory factory = new DiskFileItemFactory();
+    //        //设置工厂的缓冲区的大小，当上传的文件大小超过缓冲区的大小时，就会生成一个临时文件存放到指定的临时目录当中。
+    //        //设置缓冲区的大小为100KB，如果不指定，那么缓冲区的大小默认是10KB
+    //        factory.setSizeThreshold(1024 * 100);
+    //        //2、创建一个文件上传解析器
+    //        ServletFileUpload upload = new ServletFileUpload(factory);
+    //        //解决上传文件名的中文乱码
+    //        upload.setHeaderEncoding("UTF-8");
+    //        //3、判断提交上来的数据是否是上传表单的数据
+    //        if (!ServletFileUpload.isMultipartContent(RequestUtil.getRequest())) {
+    //            //按照传统方式获取数据
+    //        }
+    //        //设置上传文件总量的最大值，最大值=同时上传的多个文件的大小的最大值的和，目前设置为10MB
+    //        upload.setSizeMax(1024 * 1024 * 100);
+    //        //4、使用ServletFileUpload解析器解析上传数据，解析结果返回的是一个List<FileItem>集合，每一个FileItem对应一个Form表单的输入项
+    //        List<FileItem> list = upload.parseRequest(RequestUtil.getRequest());
+    //        for (FileItem item : list) {
+    //            //如果FileItem中封装的是普通输入项的数据
+    //            if (item.isFormField()) {
+    //                String name = item.getFieldName();
+    //                //解决普通输入项的数据的中文乱码问题
+    //                String value = item.getString("UTF-8");
+    //                if ("upload_ip".equals(name)) {
+    //                    ip = value;
+    //                }
+    //                if ("upload_port".equals(name)) {
+    //                    port = value;
+    //                }
+    //                if ("userId".equals(name)) {
+    //                    userId = value;
+    //                }
+    //            } else {
+    //                //如果FileItem中封装的是上传文件,得到上传的文件名称
+    //                String name = item.getFieldName();
+    //                String value = item.getString("UTF-8");
+    //                if ("userId".equals(name)) {
+    //                    userId = value;
+    //                }
+    //                String filename = item.getName();
+    //                if (filename == null || filename.trim().equals("")) {
+    //                    continue;
+    //                }
+    //                //注意：不同的浏览器提交的文件名是不一样的，有些浏览器提交上来的文件名是带有路径的，如： c:\a\b\1.txt，而有些只是单纯的文件名，如：1.txt
+    //                //处理获取到的上传文件的文件名的路径部分，只保留文件名部分
+    //                filename = filename.substring(filename.lastIndexOf(File.separator + File.separator) + 1);
+    //                //得到上传文件的扩展名
+    //                //如果需要限制上传的文件类型，那么可以通过文件的扩展名来判断上传的文件类型是否合法
+    //                //获取item中的上传文件的输入流
+    //                InputStream in = item.getInputStream();
+    //
+    //                //创建一个缓冲区
+    //                byte[] buffer = new byte[1];
+    //                //循环将输入流读入到缓冲区当中，(len=in.read(buffer))>0就表示in里面还有数据
+    //                while ((in.read(buffer)) > 0) {
+    //                    temp.append((new String(buffer, "UTF-8")).toString());
+    //                }
+    //                //关闭输入流
+    //                in.close();
+    //                //关闭输出流
+    //                String strTemp = new String(Base64.decode(temp.toString()).getBytes("UTF-8"), "UTF-8");
+    //                objTemp = JSONObject.parseObject(strTemp);
+    //            }
+    //        }
+    //        //入库
+    //        ImportMetadata.importData(objTemp, PathUtil.DCL, ip, port, userId, user_collect_id);
+    //
+    //    } catch (Exception e) {
+    //        throw new BusinessException(I18nMessage.getMessage("IndexManager.fileUpLoad.wjscsb") + "：" + e);
+    //    }
+    //}
 
 }
