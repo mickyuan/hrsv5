6e252b3c95ec5eea7f67cd6b4598f49f00e55094
xchao <xchao>
2019-09-05 12:16:47 +0800

    agent通讯及数据库测试连接

--- hrds-commons/src/main/java/hrds/commons/communication/HrdsReceiveAgent.java
+++ hrds-commons/src/main/java/hrds/commons/communication/HrdsReceiveAgent.java
@@ -21,22 +21,18 @@ public class HrdsReceiveAgent extends WebServer {
     }
 
     public static void main(String[] args) {
+        HrdsReceiveAgent hrdsReceiveAgent = new HrdsReceiveAgent(HrdsReceiceAgentConf.confBean);
+        hrdsReceiveAgent.init();
+        hrdsReceiveAgent.config();
         try {
-            HrdsReceiveAgent hrdsReceiveAgent = new HrdsReceiveAgent(HrdsReceiceAgentConf.confBean);
-            hrdsReceiveAgent.init();
-            hrdsReceiveAgent.config();
-            try {
-                ClassUtil.loadClass(ActionInstanceHelper.class.getName());
-                logger.info("Initialize actions done.");
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-            hrdsReceiveAgent.start();
-            // 如果不需要对启动过程进行细粒度的控制，可使用下面一句代替 init, config, start
-            //webServer.running();
-        } catch (Exception e) {
-            e.printStackTrace();
+            ClassUtil.loadClass(ActionInstanceHelper.class.getName());
+            logger.info("Initialize actions done.");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
+        hrdsReceiveAgent.start();
+        // 如果不需要对启动过程进行细粒度的控制，可使用下面一句代替 init, config, start
+        //webServer.running();
     }
 
     @Override
