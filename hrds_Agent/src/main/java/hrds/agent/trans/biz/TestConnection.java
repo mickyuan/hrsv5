ca2a82c3d8cd7262f4e526a3ada33544fe5d04f3
wzc171500 <654951577@qq.com>
2019-09-10 17:40:34 +0800

    提交代码修改，解决FIXME

--- hrds_Agent/src/main/java/hrds/agent/trans/biz/TestConnection.java
+++ hrds_Agent/src/main/java/hrds/agent/trans/biz/TestConnection.java
@@ -1,15 +1,12 @@
 package hrds.agent.trans.biz;
 
-import fd.ng.core.utils.JsonUtil;
 import fd.ng.db.conf.ConnWay;
 import fd.ng.db.conf.DbinfosConf;
 import fd.ng.db.conf.Dbtype;
 import fd.ng.db.jdbc.DatabaseWrapper;
 import fd.ng.web.action.AbstractWebappBaseAction;
-import fd.ng.web.util.RequestUtil;
 import hrds.commons.codes.DatabaseType;
-
-import java.util.Map;
+import hrds.commons.entity.Database_set;
 
 /**
  * @program: hrsv5
@@ -26,19 +23,16 @@ public class TestConnection extends AbstractWebappBaseAction {
 	 *
 	 * @return
 	 */
-	public boolean testConn() {
-		// 1、通过request获取服务发过来的数据
-		String json = RequestUtil.getJson();
-		Map<String, String> map = JsonUtil.toObject(json, Map.class);
+	public boolean testConn(Database_set dbSet) {
 		//2、使用dbinfo将需要测试连接的内容填充
 		DbinfosConf.Dbinfo dbinfo = new DbinfosConf.Dbinfo();
 		dbinfo.setName(DbinfosConf.DEFAULT_DBNAME);
-		dbinfo.setDriver(map.get("driver"));
-		dbinfo.setUrl(map.get("url"));
-		dbinfo.setUsername(map.get("username"));
-		dbinfo.setPassword(map.get("password"));
+		dbinfo.setDriver(dbSet.getDatabase_drive());
+		dbinfo.setUrl(dbSet.getDatabase_drive());
+		dbinfo.setUsername(dbSet.getUser_name());
+		dbinfo.setPassword(dbSet.getDatabase_pad());
 		dbinfo.setWay(ConnWay.JDBC);
-		if (map.get("dbtype").equals(DatabaseType.Postgresql.getCatCode()))
+		if (dbSet.getDatabase_type().equals(DatabaseType.Postgresql.getCatCode()))
 			dbinfo.setDbtype(Dbtype.POSTGRESQL);
 		dbinfo.setShow_conn_time(true);
 		dbinfo.setShow_sql(true);
--- hrds_B/src/main/java/hrds/b/biz/agent/dbagentconf/DBConfStepAction.java
+++ hrds_B/src/main/java/hrds/b/biz/agent/dbagentconf/DBConfStepAction.java
@@ -2,79 +2,72 @@ package hrds.b.biz.agent.dbagentconf;
 
 import com.alibaba.fastjson.JSONObject;
 import fd.ng.core.utils.JsonUtil;
+import fd.ng.core.utils.StringUtil;
 import fd.ng.db.resultset.Result;
 import fd.ng.netclient.http.HttpClient;
-import fd.ng.netclient.http.SubmitMediaType;
+import fd.ng.netserver.conf.HttpServerConf;
+import fd.ng.netserver.conf.HttpServerConfBean;
 import fd.ng.web.action.ActionResult;
 import fd.ng.web.annotation.RequestBean;
 import fd.ng.web.util.Dbo;
+import hrds.commons.base.BaseAction;
 import hrds.commons.codes.CleanType;
 import hrds.commons.codes.IsFlag;
 import hrds.commons.entity.Database_set;
 import hrds.commons.exception.BusinessException;
-import hrds.commons.exception.ExceptionEnum;
 import hrds.commons.utils.key.PrimayKeyGener;
 
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
 
 /**
- * @description: 数据库直连采集各个步骤服务类
+ * @description: 应用管理端数据库采集配置步骤一、数据库连接信息配置
  * @author: WangZhengcheng
  * @create: 2019-09-04 11:22
  **/
-public class DBConfStepAction {
+public class DBConfStepAction extends BaseAction{
 
 	/**
-	 * @Description: 数据库直连采集，数据源配置信息页面后台服务类，用于根据参数进行查询并在页面上回显数据
-	 * @Param: [agentId]
+	 * @Description: 数据库直连采集，根据agentId进行查询并在页面上回显数据源配置信息
+	 * @Param: [agentId : AgentID, 取值范围 : 不限]
 	 * @return: void
 	 * @Author: WangZhengcheng
 	 * @Date: 2019/9/4
 	 * 步骤;
-	 *      1、在数据库设置表(database_set)中，根据agentId查看当前agent是否设置完成并发送成功
-	 *      2、如果得到结果，说明先前agent设置完成并发送成功，需要回显数据给客户编辑
-	 *      3、在结果中得到database_id,根据database_id查询出相应的信息
-	 *          3-1、在数据库设置表表中，关联采集作业分类表(collect_job_classify)，查询出当前database_id的所有信息
-	 *          3-2、在collect_frequency(卸数作业参数表)表中，查询出id,文件存储路径，作业编号信息
-	 *          3-3、二者汇总成一个Result对象返回
-	 *      4、根据数据库类型获取驱动，用于前台展示
-	 *      5、返回
+	 * 1、在数据库设置表(database_set)中，根据databaseId查看当前是否设置完成并发送成功,如果已经发送了，则不允许编辑
+	 * 2、判断是否查询到数据，如果查询不到，抛异常给前端
+	 * 3、如果任务已经设置完成并发送成功，则不允许编辑
+	 *      3-1、在数据库设置表表中，关联采集作业分类表(collect_job_classify)，查询出当前database_id的所有信息
+	 *      3-2、在collect_frequency(卸数作业参数表)表中，查询出id,文件存储路径，作业编号信息
+	 *      3-3、二者汇总成一个Result对象返回
+	 * 4、返回
 	 */
-	public void getDBConfInfo(String agentId) {
-		//1、在数据库设置表中，根据agentId查看当前agent是否设置完成并发送成功
-		Result firResult = Dbo.queryResult("select * from database_set where agent_id = ?", agentId);
-		if (firResult != null) {
-			//2、如果得到结果，说明先前agent设置完成并发送成功，需要回显数据给客户编辑
-			if (firResult.getRowCount() == 1) {
-				if (IsFlag.Shi == IsFlag.getCodeObj(firResult.getString(0, "is_sendok"))) {
-					//3、在结果中得到database_id,根据database_id查询出相应的信息
-					String databaseId = firResult.getString(0, "database_id");
-					//3-1、在数据库设置表表中，关联采集作业分类表(collect_job_classify)，查询出当前database_id的所有信息
-					Result secResult = Dbo.queryResult("select * from database_set t1 left join collect_job_classify t2 on t1.classify_id = t2.classify_id  where database_id = ?", databaseId);
-					//3-2、在collect_frequency(卸数作业参数表)表中，查询出id,文件存储路径，作业编号信息
-					Result thiResult = Dbo.queryResult("select file_path,cf_jobnum,cf_id from collect_frequency where collect_set_id = ?", databaseId);
-					//3-3、二者汇总成一个Result对象返回
-					secResult.setObject(0, "file_path", thiResult.getString(0, "file_path"));
-					secResult.setObject(0, "cf_jobnum", thiResult.getString(0, "cf_jobnum"));
-					secResult.setObject(0, "cf_id", thiResult.getString(0, "cf_id"));
-					//4、根据数据库类型获取驱动，用于前台展示
-					//5、返回
-				} else {
-					throw new BusinessException("Agent设置未完成或未发送");
-				}
-			} else {
-				throw new BusinessException("Agent不唯一");
-			}
-		} else {
-			throw new BusinessException(ExceptionEnum.DATA_NOT_EXIST);
+	public Result getDBConfInfo(long databaseId) {
+		//1、在数据库设置表中，根据databaseId查看当前是否设置完成并发送成功,如果已经发送了，则不允许编辑
+		Optional<Database_set> firResult = Dbo.queryOneObject(Database_set.class,
+				"select * from database_set where database_id = ? ", databaseId);
+		//2、判断是否查询到数据，如果查询不到，抛异常给前端
+		Database_set dbSet = firResult.orElseThrow(() -> new BusinessException("未能找到该任务"));
+		//3、如果任务已经设置完成并发送成功，则不允许编辑
+		if(IsFlag.Shi == IsFlag.getCodeObj(dbSet.getIs_sendok())){
+			throw new BusinessException("该任务已经设置完成并发送成功，不允许编辑");
 		}
+		//3-1、在数据库设置表表中，关联采集作业分类表(collect_job_classify)，查询出当前database_id的所有信息
+		Result secResult = Dbo.queryResult("select * from database_set t1 left join collect_job_classify t2 on " +
+				"t1.classify_id = t2.classify_id  where database_id = ?", databaseId);
+		//3-2、在collect_frequency(卸数作业参数表)表中，查询出id,文件存储路径，作业编号信息
+		Result thiResult = Dbo.queryResult("select file_path,cf_jobnum,cf_id from collect_frequency " +
+				"where collect_set_id = ?", databaseId);
+		//3-3、二者汇总成一个Result对象返回
+		secResult.setObject(0, "file_path", thiResult.getString(0, "file_path"));
+		secResult.setObject(0, "cf_jobnum", thiResult.getString(0, "cf_jobnum"));
+		secResult.setObject(0, "cf_id", thiResult.getString(0, "cf_id"));
+		return secResult;
 	}
 
 	/**
 	 * @Description: 根据数据库类型和端口获得数据库连接url等信息
-	 * @Param: [dbType, port]
+	 * @Param: [dbType : 数据库类型, 取值范围 : DatabaseType枚举类]
+	 * @Param: [port : , 端口号, 取值范围 : 不限]
 	 * @return: java.lang.String
 	 * @Author: WangZhengcheng
 	 * @Date: 2019/9/4
@@ -86,37 +79,37 @@ public class DBConfStepAction {
 
 	/**
 	 * @Description: 保存数据库采集Agent数据库配置信息
-	 * @Param: [databaseSet]
+	 * @Param: [databaseSet : 数据库设置对象, 取值范围 : 不限]
 	 * @return: void
 	 * @Author: WangZhengcheng
 	 * @Date: 2019/9/4
 	 * 步骤：
-	 *      1、获取实体中的database_id，判断在database_set表中是否存在
-	 *      2、如果存在，则更新信息
-	 *      3、如果不存在，则新增信息
+	 * 1、获取实体中的database_id
+	 * 2、如果存在，则更新信息
+	 * 3、如果不存在，则新增信息
 	 */
 	public void saveDbConf(@RequestBean Database_set databaseSet) {
-		//1、获取实体中的database_id，判断在database_set表中是否存在
-		Result result = Dbo.queryResult("select * from database_set where database_id = ?", databaseSet.getDatabase_id());
-		if (!result.isEmpty()) {
+		//1、获取实体中的database_id
+		if(StringUtil.isNotBlank(String.valueOf(databaseSet.getDatabase_id()))){
 			//2、如果存在，则更新信息
 			if (databaseSet.update(Dbo.db()) != 1)
 				throw new BusinessException("新增数据失败！data=" + databaseSet);
 		} else {
 			//3、如果不存在，则新增信息
+			//任务级别的清洗规则，在这里新增时定义一个默认顺序，后面的页面可能改动这个顺序,后面在取这个清洗顺序的时候，用枚举==的方式
 			JSONObject cleanObj = new JSONObject(true);
-			cleanObj.put("complement", Integer.valueOf(CleanType.ZiFuBuQi.getCode()));
-			cleanObj.put("replacement", Integer.valueOf(CleanType.ZiFuTiHuan.getCode()));
-			cleanObj.put("formatting", Integer.valueOf(CleanType.ShiJianZhuanHuan.getCode()));
-			cleanObj.put("conversion", Integer.valueOf(CleanType.MaZhiZhuanHuan.getCode()));
-			cleanObj.put("consolidation", Integer.valueOf(CleanType.ZiFuHeBing.getCode()));
-			cleanObj.put("split", Integer.valueOf(CleanType.ZiFuChaiFen.getCode()));
-			cleanObj.put("trim", Integer.valueOf(CleanType.ZiFuTrim.getCode()));
+			cleanObj.put(CleanType.ZiFuBuQi.getCode(), 1);
+			cleanObj.put(CleanType.ZiFuTiHuan.getCode(), 2);
+			cleanObj.put(CleanType.ShiJianZhuanHuan.getCode(), 3);
+			cleanObj.put(CleanType.MaZhiZhuanHuan.getCode(), 4);
+			cleanObj.put(CleanType.ZiFuHeBing.getCode(), 5);
+			cleanObj.put(CleanType.ZiFuChaiFen.getCode(), 6);
+			cleanObj.put(CleanType.ZiFuTrim.getCode(), 7);
 			String id = PrimayKeyGener.getNextId();
 			databaseSet.setDatabase_number(id);
 			databaseSet.setDatabase_id(id);
-			databaseSet.setDb_agent(IsFlag.Fou.toString());
-			databaseSet.setIs_sendok(IsFlag.Fou.toString());
+			databaseSet.setDb_agent(IsFlag.Fou.getCode());
+			databaseSet.setIs_sendok(IsFlag.Fou.getCode());
 			databaseSet.setCp_or(cleanObj.toJSONString());
 			if (databaseSet.add(Dbo.db()) != 1)
 				throw new BusinessException("新增数据失败！data=" + databaseSet);
@@ -125,46 +118,41 @@ public class DBConfStepAction {
 
 	/**
 	 * @Description: 测试连接
-	 * @Param: [databaseSet]
+	 * @Param: [databaseSet : 数据库设置对象, 取值范围 : 不限]
 	 * @return: boolean
 	 * @Author: WangZhengcheng
 	 * @Date: 2019/9/4
 	 * 步骤：
-	 *      1、根据agent_id获得agent_ip,agent_port
-	 *      2、调用工具类方法给agent发消息，并获取agent响应
-	 *      3、将响应封装成ActionResult的对象
+	 * 1、根据agent_id获得agent_ip,agent_port
+	 * 2、在配置文件中获取webContext和actionPattern
+	 * 2、调用工具类方法给agent发消息，并获取agent响应
+	 * 3、将响应封装成ActionResult的对象
 	 */
 	public ActionResult testConnection(@RequestBean Database_set databaseSet) {
 		//1、根据agent_id获得agent_ip,agent_port
-		Result result = Dbo.queryResult("select * from agent_info where agent_id = ?", databaseSet.getAgent_id());
-		if (!result.isEmpty()) {
-			if (result.getRowCount() == 1) {
-				String agentIp = result.getString(0, "agent_ip");
-				String agentPort = result.getString(0, "agent_port");
-				String url = "http://" + agentIp + ":" + agentPort + "/agent/receive/";
-				String action = "hrds/agent/trans/biz/testConn";
-				Map<String, Object> map = new HashMap<>();
-				Class clazz = databaseSet.getClass();
-				Field[] fields = clazz.getDeclaredFields();
-				try {
-					for (Field field : fields) {
-						field.setAccessible(true);
-						map.put(field.getName(), field.get(databaseSet));
-					}
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
-				}
-				//2、调用工具类方法给agent发消息，并获取agent响应
-				HttpClient.ResponseValue resVal = new HttpClient(SubmitMediaType.JSON)
-						.addJson(JsonUtil.toJson(map))
-						.post(url + action);
-				//3、将响应封装成ActionResult的对象
-				return JsonUtil.toObject(resVal.getBodyString(), ActionResult.class);
-			} else {
-				throw new BusinessException("Agent不唯一");
-			}
-		} else {
-			throw new BusinessException(ExceptionEnum.DATA_NOT_EXIST);
+		Result result = Dbo.queryResult("select agent_ip, agent_port from agent_info where agent_id = ?",
+				databaseSet.getAgent_id());
+		if(result.isEmpty()){
+			throw new BusinessException("未能找到Agent信息");
 		}
+		if(result.getRowCount() != 1){
+			throw new BusinessException("找到的Agent信息不唯一");
+		}
+		HttpServerConfBean test = HttpServerConf.getHttpServer("testConnection");
+		String webContext = test.getWebContext();
+		String actionPattern = test.getActionPattern();
+		String agentIp = result.getString(0, "agent_ip");
+		String agentPort = result.getString(0, "agent_port");
+		String url = "http://" + agentIp + ":" + agentPort + webContext;
+		//2、调用工具类方法给agent发消息，并获取agent响应
+		HttpClient.ResponseValue resVal = new HttpClient()
+				.addData("driver", databaseSet.getDatabase_drive())
+				.addData("url", databaseSet.getJdbc_url())
+				.addData("username", databaseSet.getUser_name())
+				.addData("password", databaseSet.getDatabase_pad())
+				.addData("dbtype", databaseSet.getDatabase_type())
+				.post(url + actionPattern);
+		//3、将响应封装成ActionResult的对象
+		return JsonUtil.toObject(resVal.getBodyString(), ActionResult.class);
 	}
 }
