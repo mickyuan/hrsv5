f0e2a89edc429d64b9b58ef70367be50f59236b5
wzc171500 <654951577@qq.com>
2019-09-19 10:20:10 +0800

    提交hrds-commons/src/main/java/hrds/commons/utils/Constant.java，里面定义了一些项目中常用的常量

---
+++ hrds-commons/src/main/java/hrds/commons/utils/Constant.java
@@ -0,0 +1,22 @@
+package hrds.commons.utils;
+
+/**
+ * @Description: 项目中经常用到的常量值
+ * @Author: wangz
+ * @CreateTime: 2019-09-19-10:13
+ * @BelongsProject: hrsv5
+ * @BelongsPackage: hrds.commons.utils
+ **/
+public class Constant {
+
+	public static final String SDATENAME = "HYREN_S_DATE";
+	public static final String EDATENAME = "HYREN_E_DATE";
+	public static final String MD5NAME = "HYREN_MD5_VAL";
+	public static final String HD = "HD";
+	public static final String DB = "DB";
+	public static final String MAXDATE = "99991231";
+	public static final String SFTP_PORT = "22";
+
+	public Constant() {
+	}
+}
