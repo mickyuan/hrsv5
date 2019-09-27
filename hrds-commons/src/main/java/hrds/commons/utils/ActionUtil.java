090bc007da4152d927f3aae153ab0635989f8dab
xchao <xchao>
2019-09-27 17:21:21 +0800

    获取页面cookie中数据格式调整

--- hrds-commons/src/main/java/hrds/commons/utils/ActionUtil.java
+++ hrds-commons/src/main/java/hrds/commons/utils/ActionUtil.java
@@ -1,13 +1,15 @@
 package hrds.commons.utils;
 
+import com.alibaba.fastjson.JSONObject;
+import fd.ng.core.utils.JsonUtil;
 import fd.ng.web.util.RequestUtil;
 import hrds.commons.exception.BusinessException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ActionUtil {
-    private static final Logger logger = LogManager.getLogger(ActionUtil.class.getName());
-    private static final String _userCookieName = "userCookie";
+    private static final Logger logger = LogManager.getLogger();
+    private static final String _userCookieName = "Hyren_userCookie";
 
     /**
      * 獲取userCookie中的数据
@@ -18,7 +20,8 @@ public class ActionUtil {
         User user = new User();
         try {
             //这样便可以获取一个cookie数组
-            User userCookie = RequestUtil.getCookieObject(_userCookieName, user.getClass());
+            String cookieSrc = RequestUtil.getCookieObject(_userCookieName, String.class);
+            User userCookie  = JsonUtil.toObjectSafety(cookieSrc,User.class).get();
             return userCookie == null ? user : userCookie;
         } catch (Exception e) {
             logger.error("获取user失败", e);
@@ -32,7 +35,8 @@ public class ActionUtil {
      * @param user
      */
     public static void setCookieUser(User user) {
-        RequestUtil.putCookieObject(_userCookieName, user.getClass());
+        String jsonUser = JsonUtil.toJson(user);
+        RequestUtil.putCookieObject(_userCookieName, jsonUser);
     }
 
 }
