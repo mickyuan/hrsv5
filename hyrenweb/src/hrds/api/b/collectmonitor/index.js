498ec3a373f19f465d99f50d43a74abef366da9d
Qiang-Lee <54127010+Qiang-Lee@users.noreply.github.com>
2019-10-09 17:25:37 +0800

    迁移  web前端

--- hyrenweb/src/hrds/api/b/collectmonitor/index.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function getAgentNumAndSourceNum() {
-
-    return request({
-        url : 'http://127.0.0.1:8089/B/action/hrds/b/biz/collectmonitor/getAgentNumAndSourceNum',
-        method : 'post'
-    })
-}
