97eb4cbb9736d5ac5b22ff770f60a916420dfc71
wzc171500 <654951577@qq.com>
2019-10-24 10:12:50 +0800

    将构造测试数据的代码从测试用例中分离出来，让测试用例调用

---
+++ hrds_B/src/test/java/hrds/b/biz/agent/dbagentconf/dbconf/InitAndDestDataForDBConf.java
@@ -0,0 +1,183 @@
+package hrds.b.biz.agent.dbagentconf.dbconf;
+
+import fd.ng.core.annotation.DocClass;
+import fd.ng.core.utils.DateUtil;
+import fd.ng.db.jdbc.DatabaseWrapper;
+import fd.ng.db.jdbc.SqlOperator;
+import hrds.commons.codes.AgentStatus;
+import hrds.commons.codes.AgentType;
+import hrds.commons.codes.DatabaseType;
+import hrds.commons.codes.IsFlag;
+import hrds.commons.entity.Agent_info;
+import hrds.commons.entity.Collect_job_classify;
+import hrds.commons.entity.Data_source;
+import hrds.commons.entity.Database_set;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@DocClass(desc = "为DBConfStepAction单元测试类初始化和销毁数据", author = "WangZhengcheng")
+public class InitAndDestDataForDBConf {
+	//测试数据用户ID
+	private static final long TEST_USER_ID = -9997L;
+	//source_id
+	private static final long SOURCE_ID = 1L;
+	private static final long FIRST_DB_AGENT_ID = 7001L;
+	private static final long SECOND_DB_AGENT_ID = 7002L;
+	private static final long FIRST_CLASSIFY_ID = 10086L;
+	private static final long SECOND_CLASSIFY_ID = 10010L;
+	private static final long THIRD_CLASSIFY_ID = 12306L;
+
+	public static void before(){
+		//1、构建data_source表测试数据
+		Data_source dataSource = new Data_source();
+		dataSource.setSource_id(SOURCE_ID);
+		dataSource.setDatasource_number("ds_");
+		dataSource.setDatasource_name("wzctest_");
+		dataSource.setDatasource_remark("wzctestremark_");
+		dataSource.setCreate_date(DateUtil.getSysDate());
+		dataSource.setCreate_time(DateUtil.getSysTime());
+		dataSource.setCreate_user_id(TEST_USER_ID);
+
+		//2、构建agent_info表测试数据
+		List<Agent_info> agents = new ArrayList<>();
+		for (int i = 1; i <= 2; i++) {
+			String agentType = null;
+			long agentId = 0L;
+			switch (i) {
+				case 1:
+					agentType = AgentType.ShuJuKu.getCode();
+					agentId = FIRST_DB_AGENT_ID;
+					break;
+				case 2:
+					agentType = AgentType.ShuJuKu.getCode();
+					agentId = SECOND_DB_AGENT_ID;
+					break;
+			}
+			Agent_info agentInfo = new Agent_info();
+			agentInfo.setAgent_id(agentId);
+			agentInfo.setAgent_name("agent_" + i);
+			agentInfo.setAgent_type(agentType);
+			agentInfo.setAgent_ip("127.0.0.1");
+			agentInfo.setAgent_port("55555");
+			agentInfo.setAgent_status(AgentStatus.WeiLianJie.getCode());
+			agentInfo.setCreate_date(DateUtil.getSysDate());
+			agentInfo.setCreate_time(DateUtil.getSysTime());
+			agentInfo.setUser_id(TEST_USER_ID);
+			agentInfo.setSource_id(SOURCE_ID);
+
+			agents.add(agentInfo);
+		}
+
+		//3、构建database_set表测试数据
+		List<Database_set> databases = new ArrayList<>();
+		for (int i = 0; i < 2; i++) {
+			long agentId = i % 2 == 0 ? FIRST_DB_AGENT_ID : SECOND_DB_AGENT_ID;
+			long classifyId = i % 2 == 0 ? FIRST_CLASSIFY_ID : SECOND_CLASSIFY_ID;
+			long id = i % 2 == 0 ? 1001L : 1002L;
+			String isSendOk = i % 2 == 0 ? IsFlag.Shi.getCode() : IsFlag.Fou.getCode();
+			String databaseType = i % 2 == 0 ? DatabaseType.Postgresql.getCode() : DatabaseType.DB2.getCode();
+			Database_set databaseSet = new Database_set();
+			databaseSet.setDatabase_id(id);
+			databaseSet.setAgent_id(agentId);
+			databaseSet.setDatabase_number("dbtest" + i);
+			databaseSet.setDb_agent(IsFlag.Shi.getCode());
+			databaseSet.setIs_load(IsFlag.Shi.getCode());
+			databaseSet.setIs_hidden(IsFlag.Shi.getCode());
+			databaseSet.setIs_sendok(isSendOk);
+			databaseSet.setIs_header(IsFlag.Shi.getCode());
+			databaseSet.setClassify_id(classifyId);
+			databaseSet.setTask_name("wzcTaskName" + i);
+			databaseSet.setDatabase_type(databaseType);
+
+			databases.add(databaseSet);
+		}
+
+		//4、构建collect_job_classify表测试数据
+		List<Collect_job_classify> classifies = new ArrayList<>();
+		for(int i = 0; i < 2; i++){
+			Collect_job_classify classify = new Collect_job_classify();
+			long classifyId = i % 2 == 0 ? FIRST_CLASSIFY_ID : SECOND_CLASSIFY_ID;
+			long agentId = i % 2 == 0 ? FIRST_DB_AGENT_ID : SECOND_DB_AGENT_ID;
+			classify.setClassify_id(classifyId);
+			classify.setClassify_num("wzc_test_classify_num" + i);
+			classify.setClassify_name("wzc_test_classify_name" + i);
+			classify.setUser_id(TEST_USER_ID);
+			classify.setAgent_id(agentId);
+
+			classifies.add(classify);
+		}
+		Collect_job_classify thridClassify = new Collect_job_classify();
+		thridClassify.setClassify_id(THIRD_CLASSIFY_ID);
+		thridClassify.setAgent_id(FIRST_DB_AGENT_ID);
+		thridClassify.setClassify_num("wzc_test_classify_num3");
+		thridClassify.setClassify_name("wzc_test_classify_name3");
+		thridClassify.setUser_id(TEST_USER_ID);
+		classifies.add(thridClassify);
+
+
+		//插入数据
+		try (DatabaseWrapper db = new DatabaseWrapper()) {
+			//插入数据源表(data_source)测试数据
+			int dataSourceCount = dataSource.add(db);
+			assertThat("数据源测试数据初始化", dataSourceCount, is(1));
+
+			//插入Agent信息表(agent_info)测试数据
+			for(Agent_info agentInfo : agents){
+				agentInfo.add(db);
+			}
+			assertThat("Agent测试数据初始化", agents.size(), is(2));
+
+			//插入database_set表测试数据
+			for(Database_set databaseSet : databases){
+				databaseSet.add(db);
+			}
+			assertThat("数据库设置测试数据初始化", databases.size(), is(2));
+
+			//插入collect_job_classify表测试数据
+			for(Collect_job_classify classify : classifies){
+				classify.add(db);
+			}
+			assertThat("采集任务分类表测试数据初始化", classifies.size(), is(3));
+
+			SqlOperator.commitTransaction(db);
+		}
+	}
+
+	public static void after(){
+		try (DatabaseWrapper db = new DatabaseWrapper()) {
+			//1、删除数据源表(data_source)测试数据
+			int deleteSourceNum = SqlOperator.execute(db, "delete from " + Data_source.TableName + " WHERE create_user_id = ?", TEST_USER_ID);
+			//2、删除Agent信息表(agent_info)测试数据
+			int deleteAgentNum = SqlOperator.execute(db, "delete from " + Agent_info.TableName + " WHERE user_id = ?", TEST_USER_ID);
+			//3、删除database_set表测试数据
+			int deleteDsNumOne = SqlOperator.execute(db, "delete from " + Database_set.TableName + " WHERE agent_id = ?", FIRST_DB_AGENT_ID);
+			int deleteDsNumTwo = SqlOperator.execute(db, "delete from " + Database_set.TableName + " WHERE agent_id = ?", SECOND_DB_AGENT_ID);
+			//4、删除collect_job_classify表测试数据
+			int deleteCJCNum = SqlOperator.execute(db, "delete from " + Collect_job_classify.TableName + " WHERE user_id = ?", TEST_USER_ID);
+
+			SqlOperator.commitTransaction(db);
+
+			long dataSources = SqlOperator.queryNumber(db, "select count(1) from " + Data_source.TableName + " WHERE create_user_id = ?", TEST_USER_ID)
+					.orElseThrow(() -> new RuntimeException("count fail!"));
+			assertThat("测试完成后删除的数据源数据有:" + deleteSourceNum + "条", dataSources, is(0L));
+
+			long agents = SqlOperator.queryNumber(db, "select count(1) from " + Agent_info.TableName + " WHERE user_id = ?", TEST_USER_ID)
+					.orElseThrow(() -> new RuntimeException("count fail!"));
+			assertThat("测试完成后删除的Agent数据有:" + deleteAgentNum + "条", agents, is(0L));
+
+			long dataSourceSetsOne = SqlOperator.queryNumber(db, "select count(1) from " + Database_set.TableName + " WHERE agent_id = ?", FIRST_DB_AGENT_ID)
+					.orElseThrow(() -> new RuntimeException("count fail!"));
+			long dataSourceSetsTwo = SqlOperator.queryNumber(db, "select count(1) from " + Database_set.TableName + " WHERE agent_id = ?", SECOND_DB_AGENT_ID)
+					.orElseThrow(() -> new RuntimeException("count fail!"));
+			assertThat("测试完成后删除的数据库设置表数据有:" + (deleteDsNumOne + deleteDsNumTwo) + "条", dataSourceSetsOne + dataSourceSetsTwo, is(0L));
+
+			long collectJobClassifyNum = SqlOperator.queryNumber(db, "select count(1) from " + Collect_job_classify.TableName + " WHERE user_id = ?", TEST_USER_ID)
+					.orElseThrow(() -> new RuntimeException("count fail!"));
+			assertThat("测试完成后删除的采集作业分类表数据有:" + deleteCJCNum + "条", collectJobClassifyNum, is(0L));
+		}
+	}
+}
