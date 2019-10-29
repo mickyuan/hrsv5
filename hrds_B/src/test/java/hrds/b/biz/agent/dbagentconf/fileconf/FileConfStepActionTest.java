1c1de7d643735722f8ca87af3070df363e131fdd
wzc171500 <654951577@qq.com>
2019-10-29 18:20:05 +0800

    提交代码

---
+++ hrds_B/src/test/java/hrds/b/biz/agent/dbagentconf/fileconf/FileConfStepActionTest.java
@@ -0,0 +1,14 @@
+package hrds.b.biz.agent.dbagentconf.fileconf;
+
+import hrds.testbase.WebBaseTestCase;
+
+/**
+ * @Description:
+ * @Author: wangz
+ * @CreateTime: 2019-10-24-17:34
+ * @BelongsProject: hrsv5
+ * @BelongsPackage: hrds.b.biz.agent.dbagentconf.fileconf
+ **/
+public class FileConfStepActionTest extends WebBaseTestCase{
+
+}
