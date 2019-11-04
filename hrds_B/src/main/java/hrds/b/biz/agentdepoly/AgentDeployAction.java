08245d43c8af820d3e4f5006e26880a76a4788c7
Qiang-Lee <hliqiang1989@gmail.com>
2019-11-04 15:08:44 +0800

    修改Agent部署,采集首页

--- hrds_B/src/main/java/hrds/b/biz/agentdepoly/AgentDeployAction.java
+++ hrds_B/src/main/java/hrds/b/biz/agentdepoly/AgentDeployAction.java
@@ -20,75 +20,74 @@ import java.util.Map;
 @DocClass(desc = "Agent部署管理", author = "Mr.Lee", createdate = "2019-08-30 10:01")
 public class AgentDeployAction extends BaseAction {
 
-	@Method(desc = "获取当前用户的数据源信息",
-					logicStep = "查询当前用户的数据源信息")
-	@Return(desc = "返回用户的数据源信息", range = "可以为空,为空表示该用户没有数据源信息")
-	public List<Map<String, Object>> getDataSourceInfo() {
+  @Method(desc = "获取当前用户的数据源信息", logicStep = "查询当前用户的数据源信息")
+  @Return(desc = "返回用户的数据源信息", range = "可以为空,为空表示该用户没有数据源信息")
+  public List<Map<String, Object>> getDataSourceInfo() {
 
-		//1: 查询当前用户的数据源信息
-		return Dbo.queryList(
-						"SELECT t2.source_id,t2.datasource_name FROM " + Agent_info.TableName + " t1 JOIN  " +
-										Data_source.TableName + " t2 ON t1.source_id = t2.source_id  " +
-										"WHERE t1.user_id = ? GROUP BY t2.source_id,t2.datasource_name",
-						getUserId());
-	}
+    // 1: 查询当前用户的数据源信息
+    return Dbo.queryList(
+        "SELECT t2.source_id,t2.datasource_name FROM "
+            + Agent_info.TableName
+            + " t1 JOIN  "
+            + Data_source.TableName
+            + " t2 ON t1.source_id = t2.source_id  "
+            + "WHERE t1.user_id = ? GROUP BY t2.source_id,t2.datasource_name",
+        getUserId());
+  }
 
-	@Method(desc = "获取数据源下对应某种Agent的信息", logicStep = "根据选择的数据源及Agent类型查询其对应的Agent")
-	@Param(name = "source_id", desc = "数据源ID", range = "不能为空的整数")
-	@Param(name = "agent_type", desc = "Agent类型", range = "不能为空的字符串")
-	@Return(desc = "查询的Agent集合数据", range = "可以为空,为空表示为获取到Agent信息")
-	public List<Map<String, Object>> getAgentInfo(long source_id, String agent_type) {
+  @Method(desc = "获取数据源下对应某种Agent的信息", logicStep = "根据选择的数据源及Agent类型查询其对应的Agent")
+  @Param(name = "source_id", desc = "数据源ID", range = "不能为空的整数")
+  @Param(name = "agent_type", desc = "Agent类型", range = "不能为空的字符串")
+  @Return(desc = "查询的Agent集合数据", range = "可以为空,为空表示为获取到Agent信息")
+  public List<Map<String, Object>> getAgentInfo(long source_id, String agent_type) {
 
-		return Dbo.queryList("SELECT * FROM agent_info WHERE source_id = ? AND agent_type = ? AND user_id = ?", source_id, agent_type,
-						getUserId());
-	}
+    return Dbo.queryList(
+        "SELECT * FROM agent_info WHERE source_id = ? AND agent_type = ? AND user_id = ?",
+        source_id,
+        agent_type,
+        getUserId());
+  }
 
-	@Method(desc = "获取当前部署的Agent信息", logicStep = "根据选择的数据源及Agent类型查询其对应的Agent")
-	@Param(name = "agent_id", desc = "数据源ID", range = "不能为空的整数")
-	@Param(name = "agent_type", desc = "Agent类型", range = "不能为空的字符串")
-	@Return(desc = "当前Agent的部署信息", range = "可以为空,因为会出现是第一次部署", isBean = true)
-	public Agent_down_info getAgentDownInfo(long agent_id, String agent_type) {
+  @Method(desc = "获取当前部署的Agent信息", logicStep = "根据选择的数据源及Agent类型查询其对应的Agent")
+  @Param(name = "agent_id", desc = "数据源ID", range = "不能为空的整数")
+  @Param(name = "agent_type", desc = "Agent类型 (AgentType)", range = "不能为空的字符串")
+  @Return(desc = "当前Agent的部署信息", range = "可以为空,因为会出现是第一次部署")
+  public Map<String, Object> getAgentDownInfo(long agent_id, String agent_type) {
 
-		return Dbo.queryOneObject(Agent_down_info.class, "SELECT * FROM " + Agent_down_info.TableName + " WHERE " +
-										"agent_id = ? AND agent_type = ? AND user_id = ?",
-						agent_id, agent_type, getUserId()).orElseThrow(() -> new BusinessException("部署Agent信息查询错误!"));
-	}
+    return Dbo.queryOneObject(
+        "SELECT * FROM "
+            + Agent_down_info.TableName
+            + " WHERE "
+            + "agent_id = ? AND agent_type = ? AND user_id = ?",
+        agent_id,
+        agent_type,
+        getUserId());
+  }
 
-	@Method(desc = "获取Agent的全部类型信息", logicStep = "获取Agent的全部类型信息")
-	@Return(desc = "返回Agent的类型集合", range = "不可为空")
-	public Map<String, String> getAgentType() {
+  @Method(desc = "获取Agent的全部类型信息", logicStep = "获取Agent的全部类型信息")
+  @Return(desc = "返回Agent的类型集合", range = "不可为空")
+  public Map<String, String> getAgentType() {
 
-		Map<String, String> agentTypeMap = new HashMap<>();
-		agentTypeMap.put("shujuku", AgentType.ShuJuKu.getCode());
-		agentTypeMap.put("shujuwenjian", AgentType.DBWenJian.getCode());
-		agentTypeMap.put("feijiegouhua", AgentType.WenJianXiTong.getCode());
-		agentTypeMap.put("banjiegouhua", AgentType.DuiXiang.getCode());
-		agentTypeMap.put("ftp", AgentType.FTP.getCode());
+    Map<String, String> agentTypeMap = new HashMap<>();
+    agentTypeMap.put("shujuku", AgentType.ShuJuKu.getCode());
+    agentTypeMap.put("shujuwenjian", AgentType.DBWenJian.getCode());
+    agentTypeMap.put("feijiegouhua", AgentType.WenJianXiTong.getCode());
+    agentTypeMap.put("banjiegouhua", AgentType.DuiXiang.getCode());
+    agentTypeMap.put("ftp", AgentType.FTP.getCode());
 
-		return agentTypeMap;
-	}
+    return agentTypeMap;
+  }
 
-	@Method(desc = "获取当前部署的Agent信息", logicStep = "根据选择的数据源及Agent类型查询其对应的Agent")
-	@Param(name = "agent_down_info", desc = "部署Agent的填写信息," +
-					"agent_name : Agent名称," +
-					"agent_ip : Agent IP," +
-					"agent_port : Agent端口," +
-					"user_name : 用户名," +
-					"passwd : 密码," +
-					"save_dir :存放目录," +
-					"log_dir : 日志目录," +
-					"deploy : 是否部署,1-表示部署完成后,不自动启动,0-表示部署完成后自动启动," +
-					"ai_desc : 描述", range = "不能为空的整数", isBean = true)
-	public void deployAgent(Agent_down_info agent_down_info) {
+  @Method(desc = "获取当前部署的Agent信息", logicStep = "根据选择的数据源及Agent类型查询其对应的Agent")
+  @Param(name = "agent_down_info", desc = "Agent部署实体类", range = "不能为空的整数", isBean = true)
+  public void deployAgent(Agent_down_info agent_down_info) {}
 
-	}
+  @Method(desc = "保存此次部署Agent的信息", logicStep = "保存此次部署Agent的信息")
+  @Param(name = "agent_down_info", desc = "保存部署Agent信息", range = "不能为空的整数", isBean = true)
+  private void saveAgentDownInfo(Agent_down_info agent_down_info) {
 
-	@Method(desc = "保存此次部署Agent的信息", logicStep = "保存此次部署Agent的信息")
-	@Param(name = "agent_down_info", desc = "保存部署Agent信息", range = "不能为空的整数", isBean = true)
-	private void saveAgentDownInfo(Agent_down_info agent_down_info) {
-
-		if( agent_down_info.add(Dbo.db()) != 1 ) {
-			throw new BusinessException(ExceptionEnum.AGENT_DOWN_ERROR.getMessage());
-		}
-	}
+    if (agent_down_info.add(Dbo.db()) != 1) {
+      throw new BusinessException(ExceptionEnum.AGENT_DOWN_ERROR.getMessage());
+    }
+  }
 }
--- hrds_B/src/main/java/hrds/b/biz/collectmonitor/CollectMonitorAction.java
+++ hrds_B/src/main/java/hrds/b/biz/collectmonitor/CollectMonitorAction.java
@@ -4,6 +4,9 @@ import fd.ng.core.annotation.DocClass;
 import fd.ng.core.annotation.Method;
 import fd.ng.core.annotation.Param;
 import fd.ng.core.annotation.Return;
+import fd.ng.core.utils.DateUtil;
+import fd.ng.core.utils.FileUtil;
+import fd.ng.db.resultset.Result;
 import fd.ng.web.util.Dbo;
 import hrds.commons.base.BaseAction;
 import hrds.commons.codes.AgentType;
@@ -12,75 +15,167 @@ import hrds.commons.codes.IsFlag;
 import hrds.commons.entity.Agent_info;
 import hrds.commons.entity.Collect_case;
 import hrds.commons.entity.Source_file_attribute;
+import hrds.commons.exception.BusinessException;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 @DocClass(desc = "采集首页的监控信息", author = "Mr.Lee", createdate = "2019-09-04 12:09")
 public class CollectMonitorAction extends BaseAction {
 
-	@Method(desc = "查询Agent,数据源配置数量(数据源存在着Agent的时候才会显示具体数量)信息",
-					logicStep = "查询Agent,数据源配置数量(数据源存在着Agent的时候才会显示具体数量)信息")
-	@Return(desc = "查询的Agent集合数据", range = "可以为空,为空表示为获取到Agent信息")
-	public Map<String, Object> getAgentNumAndSourceNum() {
+  @Method(
+      desc = "查询Agent,数据源配置数量(数据源存在着Agent的时候才会显示具体数量)信息",
+      logicStep = "查询Agent,数据源配置数量(数据源存在着Agent的时候才会显示具体数量)信息")
+  @Return(desc = "查询的Agent集合数据", range = "可以为空,为空表示为获取到Agent信息")
+  public Map<String, Object> getAgentNumAndSourceNum() {
 
-		return Dbo.queryOneObject("SELECT COUNT(agent_id) agentNum,COUNT(DISTINCT(source_id)) sourceNum FROM " + Agent_info.TableName
-						+ " WHERE user_id = ?", getUserId());
-	}
+    return Dbo.queryOneObject(
+        "SELECT COUNT(agent_id) agentNum,COUNT(DISTINCT(source_id)) sourceNum FROM "
+            + Agent_info.TableName
+            + " WHERE user_id = ?",
+        getUserId());
+  }
 
-	@Method(desc = "获取当前用户采集的任务信息", logicStep = "获取当前用户采集的任务信息")
-	@Return(desc = "采集任务信息", range = "不能为空")
-	public List<Map<String, Object>> getDatabaseSet() {
+  @Method(desc = "获取当前用户采集的任务信息", logicStep = "获取当前用户采集的任务信息")
+  @Return(desc = "采集任务信息", range = "不能为空")
+  public List<Map<String, Object>> getDatabaseSet() {
 
-		return Dbo.queryList("select task_name taskname ,database_id taskid,task.Agent_id,agent_type from database_set task join agent_info ai" +
-										"on task.Agent_id = ai.agent_id where user_id = ? and task.is_sendok = ? and agent_type in (?,?) order by taskid desc ",
-						getUserId(), IsFlag.Shi.getCode(), AgentType.ShuJuKu.getCode(), AgentType.DBWenJian.getCode());
-	}
+    return Dbo.queryList(
+        "select task_name taskname ,database_id taskid,task.Agent_id,agent_type from database_set task "
+            + "join agent_info ai on task.Agent_id = ai.agent_id where user_id = ? and "
+            + "task.is_sendok = ? and agent_type in (?,?) order by taskid desc ",
+        getUserId(),
+        IsFlag.Shi.getCode(),
+        AgentType.ShuJuKu.getCode(),
+        AgentType.DBWenJian.getCode());
+  }
 
-	@Method(desc = "获取数据采集信息总况",
-					logicStep = "1 : 获取已采集的文件数据量大小(DB文件和数据库采集),这里显示的bytes可能需要进行转换单位" +
-									"2 : 获取当前用户的全部采集任务数(只要是整个流程完成的,不区分是否采集过文件)" +
-									"3 : 将数据信息合并,并返回</p>")
-	@Return(desc = "数据采集信息总况", range = "可以为空")
-	public Map<String, Object> getDataCollectInfo() {
+  @Method(
+      desc = "获取数据采集信息总况",
+      logicStep =
+          "1 : 获取已采集的文件及DB文件和数据库采集大小,这里显示的bytes可能需要进行转换单位"
+              + "2 : 获取当前用户的全部采集任务数(只要是整个流程配置完成的)"
+              + "3 : 将数据信息合并,并返回")
+  @Return(desc = "数据采集信息总况", range = "可以为空")
+  public Result getDataCollectInfo() {
 
-		//文件采集的代码项值
-		//FIXME 为什么要提前定义变量？
-		String fileCode = CollectType.WenJianCaiJi.getCode();
+    // 1 : 获取已采集的文件及DB文件和数据库采集大小,这里显示的bytes可能需要进行转换单位
+    Result result =
+        Dbo.queryResult(
+            " select sum((case when collect_type = ? then file_size else 0 end)) fileSize,"
+                + "sum((case when collect_type in (?,?) then file_size else 0 end)) dbSize FROM "
+                + Source_file_attribute.TableName
+                + " sfa join  agent_info ai on sfa.agent_id = ai.agent_id where user_id = ?",
+            CollectType.WenJianCaiJi.getCode(),
+            CollectType.ShuJuKuCaiJi.getCode(),
+            CollectType.DBWenJianCaiJi.getCode(),
+            getUserId());
+    result.setObject(
+        0, "filesize", FileUtil.fileSizeConversion(result.getLongDefaultZero(0, "filesize")));
+    result.setObject(
+        0, "dbsize", FileUtil.fileSizeConversion(result.getLongDefaultZero(0, "dbsize")));
 
-		//1 : 获取已采集的文件数据量大小(DB文件和数据库采集),这里显示的bytes可能需要进行转换单位
-		//TODO 这里缺少文件字节的转换,待修改
-		Map<String, Object> dataCollectInfo = Dbo
-						.queryOneObject(" select sum((case when collect_type = ? then file_size else 0 end)) fileSize," +
-										"sum((case when collect_type <> ? then file_size else 0 end)) dbSize FROM " + Source_file_attribute.TableName
-										+ "join  agent_info ai on sfa.agent_id = ai.agent_id where user_id = ?", fileCode, fileCode, getUserId());
-		//FIXME collect_type不等于fileCode就是dbSize？
+    // 2 : 获取当前用户的全部采集任务数(只要是整个流程配置完成的)
+    long taskNum =
+        Dbo.queryNumber(
+                "SELECT COUNT( 1 ) taskNum FROM ( SELECT database_id id, agent_id, is_sendok FROM database_set "
+                    + "UNION ALL SELECT fcs_id id, agent_id, is_sendok FROM file_collect_set ) A"
+                    + " WHERE EXISTS ( SELECT 1 FROM agent_info ai WHERE ai.user_id = ? "
+                    + "AND ai.agent_id = A.Agent_id ) AND is_sendok = ?",
+                getUserId(),
+                IsFlag.Shi.getCode())
+            .orElseThrow(() -> new BusinessException("未获取到采集任务数量"));
+    // 3 : 将数据信息合并,并返回
+    result.setObject(0, "taskNum", taskNum);
+    return result;
+  }
 
-		//2 : 获取当前用户的全部采集任务数(只要是整个流程完成的,不区分是否采集过文件)
-		Map<String, Object> taskNum = Dbo.queryOneObject(
-						"SELECT COUNT( 1 ) taskNum FROM" +
-										"( SELECT database_id id, agent_id, is_sendok FROM database_set UNION ALL SELECT fcs_id id, agent_id, is_sendok FROM file_collect_set ) A" +
-										" WHERE " +
-										" EXISTS ( SELECT 1 FROM agent_info ai WHERE ai.user_id = ? AND ai.agent_id = A.Agent_id ) " +
-										" AND is_sendok = ?", getUserId(), IsFlag.Shi.getCode());
-		//3 : 将数据信息合并,并返回
-		taskNum.putAll(dataCollectInfo);
-		return taskNum;
-	}
+  @Method(desc = "获取当前任务的采集作业信息", logicStep = "1 : 获取相应的采集情况" + "2 : 将作业的数据信息处理为有错误的优先显示在前面")
+  @Param(name = "database_id", desc = "任务ID", range = "不能为空,否则将无法查询表信息")
+  @Return(desc = "返回采集任务表信息", range = "可以为空, 表示未有采集表信息")
+  public List<Map<String, String>> getCurrentTaskJob(long database_id) {
 
-	@Method(desc = "获取当前任务的采集作业信息",
-					logicStep = "1 : 获取相应的采集情况" +
-									"2 : 将作业的数据信息处理为有错误的优先显示在前面")
-	public static void currentTaskJob(Long database_id) {
+    // 1: 获取相应的采集情况
+    List<Collect_case> collectJobList =
+        Dbo.queryList(
+            Collect_case.class,
+            "SELECT table_name,collect_type,job_type,execute_state,collect_s_date,collect_s_time,"
+                + "collect_e_date,collect_e_time,cc_remark FROM collect_case WHERE collect_set_id = ?"
+                + " AND collect_s_date = (select max(collect_s_date) from collect_case where "
+                + "collect_set_id = ? ) ORDER BY table_name",
+            database_id,
+            database_id);
+    // 2 : 将作业的数据信息处理为有错误的优先显示在前面
+    List<Map<String, String>> tableDetails = JobTableDetails.getTableDetails(collectJobList);
 
-		// 1: 获取相应的采集情况
-		List<Collect_case> collectJobList = Dbo.queryList(Collect_case.class,
-						"SELECT table_name,collect_type,job_type,execute_state,collect_s_date,collect_s_time,collect_e_date,collect_e_time,cc_remark " +
-										"FROM collect_case WHERE collect_set_id = ? AND collect_s_date = (select max(collect_s_date) from collect_case where " +
-										"collect_set_id = ? ) ORDER BY table_name",
-						database_id, database_id);
-		//2 : 将作业的数据信息处理为有错误的优先显示在前面
-		JobDetails.getDetails(collectJobList);
-	}
+    return tableDetails;
+  }
 
+  @Method(
+      desc = "获取采集作业最近15天信息",
+      logicStep =
+          "1: 查询历史任务采集的数据量(文件采集量,数据采集量,其中数据采集量包含:数据库采集,数据文件采集),倒序查询15条数据"
+              + "2: 查询到结果,将日期,数据采集量,文件采集量经行处理,否则直接返回"
+              + "3: 将处理后的数据信息返回")
+  @Return(desc = "返回历史采集处理后的数据", range = "可以为空,为空表示没有历史采集数据信息")
+  public List<Map<String, String>> getHoStoryCollect() {
+
+    // 1: 查询历史任务采集的数据量(文件采集量,数据采集量,其中数据采集量包含:数据库采集,数据文件采集),倒序查询15条数据
+    Result result =
+        Dbo.queryResult(
+            "SELECT * FROM (SELECT SUM(CAST(cf.collet_database_size AS NUMERIC)) dbsize,cf.etl_date AS dbdate FROM "
+                + Agent_info.TableName
+                + " ai JOIN "
+                + Collect_case.TableName
+                + " cf ON cf.agent_id = ai.agent_id WHERE "
+                + " ai.user_id = ? AND cf.collect_type IN ( ?, ? ) GROUP BY cf.etl_date ORDER BY cf.etl_date DESC) aa "
+                + "FULL JOIN (SELECT SUM( file_size ) filesize,cc.etl_date AS filedate FROM ( SELECT * FROM collect_case "
+                + "WHERE collect_type = ? ) cc JOIN ( SELECT * FROM "
+                + Source_file_attribute.TableName
+                + " WHERE "
+                + "collect_type = ? ) sfa ON cc.agent_id = sfa.agent_id AND cc.collect_set_id = sfa.collect_set_id JOIN "
+                + Agent_info.TableName
+                + " ai ON cc.agent_id = ai.agent_id WHERE ai.user_id = ? GROUP BY"
+                + " cc.etl_date ORDER BY cc.etl_date DESC) bb ON aa.dbdate = bb.filedate LIMIT 15",
+            getUserId(),
+            CollectType.DBWenJianCaiJi.getCode(),
+            CollectType.ShuJuKuCaiJi.getCode(),
+            CollectType.WenJianCaiJi.getCode(),
+            CollectType.WenJianCaiJi.getCode(),
+            getUserId());
+
+    // 2: 查询到结果,将日期,数据采集量,文件采集量经行处理,否则直接返回
+    if (result.isEmpty()) {
+      return null;
+    }
+    // 返回的数据集合Map
+    List<Map<String, String>> resultMap = new ArrayList<Map<String, String>>();
+    Map<String, String> itemMap = null;
+    for (int i = 0; i < result.getRowCount(); i++) {
+      itemMap = new LinkedHashMap<>();
+      // 获取日期数据并处理为带有分割符的日期数据
+      itemMap.put(
+          "date", DateUtil.parseStr2DateWith8Char(result.getString(i, "dbdate")).toString());
+      // 获取数据采集数据,并进行单位统一转换为 MB,并保留2位小数
+      itemMap.put("data", formatFileSizeMB(result.getLongDefaultZero(i, "dbsize")));
+      // 获取文件采集数据,并进行单位统一转换为 MB,并保留2位小数
+      itemMap.put("file", formatFileSizeMB(result.getLongDefaultZero(i, "filesize")));
+
+      resultMap.add(itemMap);
+    }
+    // 3: 将处理后的数据信息返回
+    return resultMap;
+  }
+
+  @Method(desc = "将bytes没转换为MB", logicStep = "1: 先算出MB的值. 2: 然后转换为保留2位数,并返回")
+  @Param(name = "size", desc = "文件的bytes", range = "不为空")
+  @Return(desc = "返回转换后的大小(MB)", range = "不为空")
+  private String formatFileSizeMB(long size) {
+    // 1: 先算出MB的值
+    double f = (double) size / (1024 * 1024);
+    // 2: 然后转换为保留2位数,并返回
+    return String.format("%.2f", f);
+  }
 }
