8d0783f0774974567e61750c411785ee0de3d5de
BY-HLL <78733419@qq.com>
2019-11-06 16:42:46 +0800

    提交部门管理类（修复已知问题）

--- hrds_A/src/main/java/hrds/a/biz/department/DepartmentAction.java
+++ hrds_A/src/main/java/hrds/a/biz/department/DepartmentAction.java
@@ -5,6 +5,7 @@ import fd.ng.core.annotation.Method;
 import fd.ng.core.annotation.Param;
 import fd.ng.core.annotation.Return;
 import fd.ng.core.utils.DateUtil;
+import fd.ng.core.utils.StringUtil;
 import fd.ng.db.jdbc.DefaultPageImpl;
 import fd.ng.db.jdbc.Page;
 import fd.ng.web.util.Dbo;
@@ -25,13 +26,13 @@ public class DepartmentAction extends BaseAction {
 	@Method(desc = "新增部门",
 			logicStep = "1.设置部门属性信息(非页面传入的值)" +
 					"2.新增部门信息")
-	@Param(name = "departmentInfo", desc = "Department_info的实体", range = "Department_info的实体", isBean = true)
-	@Return(desc = "void 无返回结果", range = "无")
+	@Param(name = "departmentInfo", desc = "Department_info的实体",
+			range = "Department_info的实体,其中dep_name不能为空", isBean = true)
 	public void addDepartmentInfo(Department_info departmentInfo) {
 		//数据权限校验：不做权限检查
 		//1.设置部门属性信息(非页面传入的值)
 		if (checkDepNameIsRepeat(departmentInfo.getDep_name())) {
-			throw new BusinessException("部门名称重复，dep_name=" + departmentInfo.getDep_name());
+			throw new BusinessException("部门名称重复!" + departmentInfo.getDep_name());
 		}
 		departmentInfo.setDep_id(PrimayKeyGener.getNextId());
 		departmentInfo.setCreate_date(DateUtil.getSysDate());
@@ -41,8 +42,7 @@ public class DepartmentAction extends BaseAction {
 	}
 
 	@Method(desc = "删除部门", logicStep = "1.检查待删除的部门是否存在，存在则根据 dep_id 删除")
-	@Param(name = "dep_id", desc = "部门id", range = "Integer类型，长度为10，此id唯一", example = "1000000001")
-	@Return(desc = "void 无返回结果", range = "无")
+	@Param(name = "dep_id", desc = "部门id", range = "long类型，长度为10，此id唯一", example = "1000000001")
 	public void deleteDepartmentInfo(long dep_id) {
 		//数据权限校验：不做权限检查
 		//1.检查待删除的部门是否存在，存在则根据 dep_id 删除
@@ -50,21 +50,27 @@ public class DepartmentAction extends BaseAction {
 			throw new BusinessException("部门下还存在用户!");
 		}
 		if (checkDepIdIsExist(dep_id)) {
-			DboExecute.deletesOrThrow("删除部门失败!，dep_id=" + dep_id,
+			DboExecute.deletesOrThrow("删除部门失败!" + dep_id,
 					"DELETE FROM " + Department_info.TableName + " WHERE dep_id = ? ", dep_id);
 		}
 	}
 
-	@Method(desc = "修改部门信息", logicStep = "1.检查待修改的的部门是否存在，存在则修改")
-	@Param(name = "departmentInfo", desc = "Department_info的实体", range = "Department_info的实体",
+	@Method(desc = "修改部门信息", logicStep = "1.校验数据合法性")
+	@Param(name = "departmentInfo", desc = "Department_info的实体", range = "Department_info的实体不能为null",
 			example = "departmentInfo", isBean = true)
-	@Return(desc = "void 无返回结果", range = "无")
 	public void updateDepartmentInfo(Department_info departmentInfo) {
 		//数据权限校验：不做权限检查
-		//1.检查待修改的的部门信息是否存在，存在则修改
-		if (checkDepIdIsExist(departmentInfo.getDep_id())) {
-			departmentInfo.update(Dbo.db());
+		//1.校验数据合法性
+		if (!checkDepIdIsExist(departmentInfo.getDep_id())) {
+			throw new BusinessException("修改的部门id不存在!");
 		}
+		if (StringUtil.isBlank(departmentInfo.getDep_name())) {
+			throw new BusinessException("部门名称不能为空!" + departmentInfo.getDep_name());
+		}
+		if (checkDepNameIsRepeat(departmentInfo.getDep_name())) {
+			throw new BusinessException("部门名称重复!" + departmentInfo.getDep_name());
+		}
+		departmentInfo.update(Dbo.db());
 	}
 
 	@Method(desc = "获取所有部门信息", logicStep = "获取所有部门信息")
@@ -90,7 +96,7 @@ public class DepartmentAction extends BaseAction {
 	public boolean checkDepIdIsExist(long dep_id) {
 		//数据权限校验：不做权限检查
 		//1.根据 dep_id 检查部门是否存在(1 : 表示存在, 其他为异常情况,因为根据主键只能查出一条记录信息)
-		return Dbo.queryNumber("SELECT COUNT(1) FROM " + Department_info.TableName + " WHERE dep_id = ?", dep_id)
+		return Dbo.queryNumber("SELECT COUNT(dep_id) FROM " + Department_info.TableName + " WHERE dep_id = ?", dep_id)
 				.orElseThrow(() -> new BusinessException("检查部门否存在的SQL编写错误")) == 1;
 	}
 
@@ -100,7 +106,7 @@ public class DepartmentAction extends BaseAction {
 	@Return(desc = "部门名称是否重复", range = "true：重复，false：不重复")
 	private boolean checkDepNameIsRepeat(String dep_name) {
 		//1.根据 dep_name 检查部门名称是否重复
-		return Dbo.queryNumber("select count(dep_name) count from " + Department_info.TableName + "" +
+		return Dbo.queryNumber("select count(dep_name) count from " + Department_info.TableName +
 						" WHERE dep_name =?",
 				dep_name).orElseThrow(() -> new BusinessException("检查部门名称否重复的SQL编写错误")) != 0;
 	}
@@ -113,7 +119,7 @@ public class DepartmentAction extends BaseAction {
 		//1.根据 data_mart_id 检查集市下是否存在数据表
 		return Dbo.queryNumber("select count(dep_id) count from " + Sys_user.TableName + " WHERE " +
 						"dep_id =?",
-				dep_id).orElseThrow(() -> new BusinessException("检查部门下是否存在用户的SQL编写错误")) != 0;
+				dep_id).orElseThrow(() -> new BusinessException("检查部门下是否存在用户的SQL编写错误")) > 0;
 	}
 
 }
