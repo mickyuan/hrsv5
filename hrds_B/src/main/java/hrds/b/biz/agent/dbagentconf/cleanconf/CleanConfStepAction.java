b1e72ac015d07e764441c2648c27be5ca3c68c69
wzc171500 <654951577@qq.com>
2019-11-14 15:36:32 +0800

    配合前端联调，修改代码及其测试用例

--- hrds_B/src/main/java/hrds/b/biz/agent/dbagentconf/cleanconf/CleanConfStepAction.java
+++ hrds_B/src/main/java/hrds/b/biz/agent/dbagentconf/cleanconf/CleanConfStepAction.java
@@ -243,10 +243,11 @@ public class CleanConfStepAction extends BaseAction{
 		//2、遍历replaceList
 		for(int i = 0; i < replaceList.size(); i++){
 			Table_clean tableClean = replaceList.get(i);
-			if(StringUtil.isBlank(tableClean.getField())){
+			//这里用isEmpty的目的是，有可能原字符串和替换字符串都是空格或者特殊字符
+			if(StringUtil.isEmpty(tableClean.getField())){
 				throw new BusinessException("保存表字符替换规则时，第"+ (i + 1) +"条数据缺少源字符串");
 			}
-			if(StringUtil.isBlank(tableClean.getReplace_feild())){
+			if(StringUtil.isEmpty(tableClean.getReplace_feild())){
 				throw new BusinessException("保存表字符替换规则时，第"+ (i + 1) +"条数据缺少替换字符串");
 			}
 			//2-1、为每一个Table_clean对象设置主键
@@ -280,10 +281,11 @@ public class CleanConfStepAction extends BaseAction{
 		//2、遍历replaceList
 		for(int i = 0; i < replaceList.size(); i++){
 			Column_clean columnClean = replaceList.get(i);
-			if(StringUtil.isBlank(columnClean.getField())){
+			//这里使用isEmpty的目的是，在保存字符替换规则的时候，源字符串和替换字符串可能都是空格
+			if(StringUtil.isEmpty(columnClean.getField())){
 				throw new BusinessException("保存列字符替换规则时，第"+ (i + 1) +"条数据缺少源字符串");
 			}
-			if(StringUtil.isBlank(columnClean.getReplace_feild())){
+			if(StringUtil.isEmpty(columnClean.getReplace_feild())){
 				throw new BusinessException("保存列字符替换规则时，第"+ (i + 1) +"条数据缺少替换字符串");
 			}
 			//2-1、为每一个Column_clean对象设置主键
@@ -467,13 +469,20 @@ public class CleanConfStepAction extends BaseAction{
 
 		//3、如果配置了字符替换
 		if(IsFlag.ofEnumByCode(replaceFlag) == IsFlag.Shi){
+			if(!(oriFieldArr.length > 0)){
+				throw new BusinessSystemException("保存所有表字符替换清洗设置时，缺失原字符");
+			}
+			if(!(replaceFeildArr.length > 0)){
+				throw new BusinessSystemException("保存所有表字符替换清洗设置时，缺失替换字符");
+			}
 			for(int i = 0; i < oriFieldArr.length; i++){
 				String oriField = oriFieldArr[i];
 				String replaceFeild = replaceFeildArr[i];
-				if(StringUtil.isBlank(oriField)){
+				//这里使用isEmpty的原因是，在保存字符替换的时候，原字符和替换字符都可能是空格
+				if(StringUtil.isEmpty(oriField)){
 					throw new BusinessSystemException("保存所有表字符替换清洗时，请填写第"+ (i + 1) +"条数据的原字符");
 				}
-				if(StringUtil.isBlank(replaceFeild)){
+				if(StringUtil.isEmpty(replaceFeild)){
 					throw new BusinessSystemException("保存所有表字符替换清洗时，请填写第"+ (i + 1) +"条数据的替换字符");
 				}
 				//3-1、构建Clean_parameter对象，设置原字段，替换后字段
