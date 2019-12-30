6870394a27f06a55e4cea37a3c3c5295f1a391aa
xiaozhongzhu <53812434+xiaozhongzhu@users.noreply.github.com>
2019-12-30 16:09:44 +0800

    修改参数传递

--- hrds_Agent/src/main/java/hrds/agent/job/biz/bean/DataStoreConfBean.java
+++ hrds_Agent/src/main/java/hrds/agent/job/biz/bean/DataStoreConfBean.java
@@ -16,11 +16,18 @@ public class DataStoreConfBean implements Serializable {
 	@DocBean(name = "store_type", value = "存储类型(store_type):1-关系型数据库<DATABASE> 2-Hbase<HBASE> " +
 			"3-solr<SOLR> 4-ElasticSearch<ElasticSearch> 5-mongodb<MONGODB> ", dataType = String.class, required = true)
 	private String store_type;
-	@DocBean(name = "data_store_layer_attr", value = "数据存储层配置属性:", dataType = Map.class, required = true)
-	private Map<String, String> data_store_layer_attr;
+	@DocBean(name = "data_store_connect_attr", value = "数据存储层连接配置属性:", dataType = Map.class, required = true)
+	private Map<String, String> data_store_connect_attr;
 	@DocBean(name = "additInfoField", value = "附加信息字段:", dataType = Map.class, required = true)
 	private Map<String, Map<Integer, String>> additInfoFieldMap;
-
+	@DocBean(name ="is_hadoopclient",value="是否有hadoop客户端(IsFlag):1-是<Shi> 0-否<Fou> ",dataType = String.class,required = true)
+	private String is_hadoopclient;
+	@DocBean(name ="dtcs_id",value="类型对照ID:",dataType = Long.class,required = false)
+	private Long dtcs_id;
+	@DocBean(name ="dlcs_id",value="长度对照表ID:",dataType = Long.class,required = false)
+	private Long dlcs_id;
+	@DocBean(name = "data_store_layer_file", value = "数据存储层配置文件属性:", dataType = Map.class, required = true)
+	private Map<String, String> data_store_layer_file;
 
 	public String getDsl_name() {
 		return dsl_name;
@@ -38,12 +45,44 @@ public class DataStoreConfBean implements Serializable {
 		this.store_type = store_type;
 	}
 
-	public Map<String, String> getData_store_layer_attr() {
-		return data_store_layer_attr;
+	public Map<String, String> getData_store_connect_attr() {
+		return data_store_connect_attr;
 	}
 
-	public void setData_store_layer_attr(Map<String, String> data_store_layer_attr) {
-		this.data_store_layer_attr = data_store_layer_attr;
+	public void setData_store_connect_attr(Map<String, String> data_store_connect_attr) {
+		this.data_store_connect_attr = data_store_connect_attr;
+	}
+
+	public String getIs_hadoopclient() {
+		return is_hadoopclient;
+	}
+
+	public void setIs_hadoopclient(String is_hadoopclient) {
+		this.is_hadoopclient = is_hadoopclient;
+	}
+
+	public Long getDtcs_id() {
+		return dtcs_id;
+	}
+
+	public void setDtcs_id(Long dtcs_id) {
+		this.dtcs_id = dtcs_id;
+	}
+
+	public Long getDlcs_id() {
+		return dlcs_id;
+	}
+
+	public void setDlcs_id(Long dlcs_id) {
+		this.dlcs_id = dlcs_id;
+	}
+
+	public Map<String, String> getData_store_layer_file() {
+		return data_store_layer_file;
+	}
+
+	public void setData_store_layer_file(Map<String, String> data_store_layer_file) {
+		this.data_store_layer_file = data_store_layer_file;
 	}
 
 	public Map<String, Map<Integer, String>> getAdditInfoFieldMap() {
