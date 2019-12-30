99ed38d9bd1780ad3052c0be4b4ce946522e8615
xiaozhongzhu <53812434+xiaozhongzhu@users.noreply.github.com>
2019-12-30 11:31:41 +0800

    提交sql算增量的代码

---
+++ hrds_Agent/src/main/java/hrds/agent/job/biz/core/dbstage/increasement/IncreasementByMpp.java
@@ -0,0 +1,233 @@
+package hrds.agent.job.biz.core.dbstage.increasement;
+
+import fd.ng.db.jdbc.DatabaseWrapper;
+import hrds.agent.job.biz.bean.TableBean;
+import hrds.commons.exception.AppSystemException;
+import hrds.commons.utils.Constant;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Mpp数据库通过sql方式来处理增量问题
+ */
+public class IncreasementByMpp extends JDBCIncreasement {
+	private static final Log logger = LogFactory.getLog(IncreasementByMpp.class);
+
+	public IncreasementByMpp(TableBean tableBean, String hbase_name, String sysDate, DatabaseWrapper db) {
+		super(tableBean, hbase_name, sysDate, db);
+	}
+
+	/**
+	 * 执行事务总方法（包括：创建表，比较增量结果入临时表，将结果入hbase
+	 */
+	public void calculateIncrement() {
+		//1.为了防止第一次执行，yesterdayTableName表不存在，创建空表
+		createTableIfNotExists(yesterdayTableName,db,columns,types,sqlList);
+		//2、创建增量临时表
+		getCreateDeltaSql();
+		//3、把今天的卸载数据映射成一个表，这里在上传数据的时候加载到了todayTableName这张表。
+		//4、为了可以重跑，这边需要把今天（如果今天有进数的话）的数据清除掉
+		restoreData();
+		//5.将比较之后的需要insert的结果插入到临时表中
+		getInsertDataSql();
+		//6.将比较之后的需要delete(拉链中的闭链)的结果插入到临时表中
+		getDeleteDataSql();
+		//7.执行所有sql语句
+		executeSql(sqlList, db);
+
+	}
+
+	private void restoreData() {
+		sqlList.add("delete from " + yesterdayTableName + " where " + Constant.SDATENAME + "='" + sysDate + "'");
+		sqlList.add("update " + yesterdayTableName + " set " + Constant.EDATENAME + " = "
+				+ Constant.MAXDATE + " where " + Constant.EDATENAME + "='" + sysDate + "'");
+	}
+
+	/**
+	 * 创建增量表
+	 */
+	private void getCreateDeltaSql() {
+		//1  创建增量表
+		StringBuilder sql = new StringBuilder(120); //拼接创表sql语句
+		sql.append("CREATE TABLE ");
+		sql.append(deltaTableName);
+		sql.append("(");
+		for (int i = 0; i < columns.size(); i++) {
+			sql.append(columns.get(i)).append(" ").append(types.get(i)).append(",");
+		}
+		sql.append(" action VARCHAR(2)");
+		sql.append(")");
+		//如果表已存在则删除
+		dropTableIfExists(deltaTableName, db, sqlList);
+		sqlList.add(sql.toString());
+	}
+
+	/**
+	 * 调用deleteData的sql，在增量表中插入增量数据
+	 */
+	private void getDeleteDataSql() {
+
+		StringBuilder deleteDatasql = new StringBuilder(120);
+		//拼接查找增量并插入增量表
+		deleteDatasql.append("INSERT INTO ");
+		deleteDatasql.append(deltaTableName);
+		deleteDatasql.append("(");
+		for (String col : columns) {
+			deleteDatasql.append(col).append(",");
+		}
+		deleteDatasql.append(" action ");
+		deleteDatasql.append(" ) ");
+		deleteDatasql.append(" select ");
+
+		for (String col : columns) {
+			if (col.equals(Constant.EDATENAME)) {
+				deleteDatasql.append(sysDate).append(" as ").append(col).append(",");
+			} else {
+				deleteDatasql.append(yesterdayTableName).append(".").append(col).append(",");
+			}
+		}
+		deleteDatasql.append("'UD' ");//修改action的值为D
+		deleteDatasql.append(" from ");
+		deleteDatasql.append(yesterdayTableName);
+		deleteDatasql.append(" WHERE NOT EXISTS ");
+		deleteDatasql.append(" ( select * from ");
+		deleteDatasql.append(todayTableName);
+		deleteDatasql.append(" where ");
+		deleteDatasql.append(yesterdayTableName).append(".").append(Constant.MD5NAME);
+		deleteDatasql.append(" = ");
+		deleteDatasql.append(todayTableName).append(".").append(Constant.MD5NAME);
+		deleteDatasql.append(" ) AND ").append(yesterdayTableName).append(".").append(Constant.EDATENAME);
+		deleteDatasql.append(" = '99991231'");
+		sqlList.add(deleteDatasql.toString());
+	}
+
+	/**
+	 * 调用insertData的sql，在增量表中插入增量数据
+	 */
+	private void getInsertDataSql() {
+		StringBuilder insertDataSql = new StringBuilder(120);
+		//拼接查找增量并插入增量表
+		insertDataSql.append("INSERT INTO ");
+		insertDataSql.append(deltaTableName);
+		insertDataSql.append("(");
+		for (String col : columns) {
+			insertDataSql.append(col).append(", ");
+		}
+		insertDataSql.append(" action ");
+		insertDataSql.append(" ) ");
+		insertDataSql.append(" select ");
+		for (String col : columns) {
+			insertDataSql.append(todayTableName).append(".").append(col).append(",");
+		}
+		//修改action的值为CU
+		insertDataSql.append("'CU' ");
+		insertDataSql.append(" from ");
+		insertDataSql.append(todayTableName);
+		insertDataSql.append(" WHERE NOT EXISTS ");
+		insertDataSql.append(" ( select * from ");
+		insertDataSql.append(yesterdayTableName);
+		insertDataSql.append(" where ");
+		insertDataSql.append(todayTableName).append(".").append(Constant.MD5NAME);
+		insertDataSql.append(" = ");
+		insertDataSql.append(yesterdayTableName).append(".").append(Constant.MD5NAME);
+		insertDataSql.append(" ) ");
+		sqlList.add(insertDataSql.toString());
+	}
+
+	/**
+	 * 根据临时增量表合并出新的增量表，删除以前的增量表
+	 */
+	@Override
+	public void mergeIncrement() {
+		List<String> list = new ArrayList<>();
+		String tmpDelTa = deltaTableName + "_delta";
+		try {
+			dropTableIfExists(tmpDelTa, db, list);
+			//创建临时表
+			list.add(createInvalidDataSql(tmpDelTa));
+			//插入有效数据
+			list.add(insertInvalidDataSql(tmpDelTa));
+			//插入所有数据
+			list.add(insertDeltaDataSql(tmpDelTa));
+			//删除原始表
+			dropTableIfExists(tableNameInHBase, db, list);
+			//重命名
+			list.add("ALTER TABLE  " + tmpDelTa + " RENAME TO  " + tableNameInHBase);
+			executeSql(list, db);
+		} catch (Exception e) {
+			logger.error("根据临时表对mpp表做增量操作时发生错误！！", e);
+			throw new AppSystemException("根据临时表对mpp表做增量操作时发生错误！！", e);
+		}
+	}
+
+	private String insertInvalidDataSql(String tmpDelTa) {
+		StringBuilder insertDataSql = new StringBuilder(120);
+		//拼接查找增量并插入增量表
+		insertDataSql.append("INSERT INTO ");
+		insertDataSql.append(tmpDelTa);
+		insertDataSql.append("(");
+		for (String col : columns) {
+			insertDataSql.append(col).append(",");
+		}
+		//将最后的逗号删除
+		insertDataSql.deleteCharAt(insertDataSql.length() - 1);
+		insertDataSql.append(" ) ");
+		insertDataSql.append(" select ");
+		for (String col : columns) {
+			insertDataSql.append(tableNameInHBase).append(".").append(col).append(",");
+		}
+		//将最后的逗号删除
+		insertDataSql.deleteCharAt(insertDataSql.length() - 1);
+		insertDataSql.append(" from ");
+		insertDataSql.append(tableNameInHBase);
+		insertDataSql.append(" WHERE NOT EXISTS ");
+		insertDataSql.append(" ( select * from ");
+		insertDataSql.append(deltaTableName);
+		insertDataSql.append(" where ");
+		insertDataSql.append(deltaTableName).append(".").append(Constant.MD5NAME);
+		insertDataSql.append(" = ");
+		insertDataSql.append(tableNameInHBase).append(".").append(Constant.MD5NAME);
+		insertDataSql.append(" ) ");
+		return insertDataSql.toString();
+
+	}
+
+	private String createInvalidDataSql(String tmpDelTa) {
+		//1  创建临时表
+		StringBuilder sql = new StringBuilder(120); //拼接创表sql语句
+		sql.append("CREATE TABLE ");
+		sql.append(tmpDelTa);
+		sql.append("(");
+		for (int i = 0; i < columns.size(); i++) {
+			sql.append(columns.get(i)).append(" ").append(types.get(i)).append(",");
+		}
+		sql.deleteCharAt(sql.length() - 1); //将最后的逗号删除
+		sql.append(")");
+		return sql.toString();
+	}
+
+	private String insertDeltaDataSql(String tmpDelTa) {
+		StringBuilder insertDataSql = new StringBuilder(120);
+		//拼接查找增量并插入增量表
+		insertDataSql.append("INSERT INTO ");
+		insertDataSql.append(tmpDelTa);
+		insertDataSql.append("(");
+		for (String col : columns) {
+			insertDataSql.append(col).append(",");
+		}
+		insertDataSql.deleteCharAt(insertDataSql.length() - 1); //将最后的逗号删除
+		insertDataSql.append(" ) ");
+		insertDataSql.append(" select ");
+		for (String col : columns) {
+			insertDataSql.append(deltaTableName).append(".").append(col).append(",");
+		}
+		insertDataSql.deleteCharAt(insertDataSql.length() - 1); //将最后的逗号删除
+		insertDataSql.append(" from ");
+		insertDataSql.append(deltaTableName);
+		return insertDataSql.toString();
+	}
+
+}
---
+++ hrds_Agent/src/main/java/hrds/agent/job/biz/core/dbstage/increasement/JDBCIncreasement.java
@@ -0,0 +1,191 @@
+package hrds.agent.job.biz.core.dbstage.increasement;
+
+import fd.ng.core.annotation.DocClass;
+import fd.ng.core.utils.StringUtil;
+import fd.ng.db.conf.Dbtype;
+import fd.ng.db.jdbc.DatabaseWrapper;
+import hrds.agent.job.biz.bean.TableBean;
+import hrds.agent.job.biz.core.dbstage.service.CollectTableHandleParse;
+import hrds.commons.exception.AppSystemException;
+import hrds.commons.utils.Constant;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.io.Closeable;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+@DocClass(desc = "通过jdbc使用sql算增量", author = "zxz", createdate = "2019/12/24 09:41")
+public abstract class JDBCIncreasement implements Closeable {
+	private static final Log logger = LogFactory.getLog(JDBCIncreasement.class);
+
+	List<String> columns;// csv中存有的字段
+	List<String> types;// csv中存有的字段类型
+	String sysDate;//任务跑批日期
+	String tableNameInHBase; //hbase的表名
+	String deltaTableName; //增量表的名字
+	List<String> sqlList = new ArrayList<>();
+	String yesterdayTableName;//上次的表
+	DatabaseWrapper db;
+	String todayTableName;
+
+	JDBCIncreasement(TableBean tableBean, String hbase_name, String sysDate, DatabaseWrapper db) {
+		this.columns = StringUtil.split(tableBean.getColumnMetaInfo(), CollectTableHandleParse.STRSPLIT);
+		this.types = StringUtil.split(tableBean.getColTypeMetaInfo(), CollectTableHandleParse.STRSPLIT);
+		this.sysDate = sysDate;
+		this.tableNameInHBase = hbase_name;
+		this.deltaTableName = hbase_name + "_tmp_hyren";
+		this.yesterdayTableName = hbase_name;
+		this.todayTableName = hbase_name + "_" + sysDate;
+		this.db = db;
+	}
+
+	/**
+	 * 比较出所有的增量数据入增量表
+	 */
+	public abstract void calculateIncrement() throws Exception;
+
+	/**
+	 * 根据临时增量表合并出新的增量表，删除以前的增量表
+	 */
+	public abstract void mergeIncrement() throws Exception;
+
+	/**
+	 * 表存在先删除该表，这里因为Oracle不支持DROP TABLE IF EXISTS
+	 */
+	void dropTableIfExists(String tableName, DatabaseWrapper db, List<String> sqlList) {
+		if (Dbtype.ORACLE.equals(db.getDbtype())) {
+			//如果有数据则表明该表存在，创建表
+			if (tableIsExistsOracle(tableName, db)) {
+				sqlList.add("DROP TABLE " + tableName);
+			}
+		} else {
+			sqlList.add("DROP TABLE IF EXISTS " + tableName);
+		}
+	}
+
+	/**
+	 * 创建表，如果表不存在
+	 */
+	void createTableIfNotExists(String tableName, DatabaseWrapper db, List<String> columns, List<String> types,
+	                            List<String> sqlList) {
+		StringBuilder create = new StringBuilder(1024);
+		if (Dbtype.ORACLE.equals(db.getDbtype())) {
+			//如果有数据则表明该表存在，创建表
+			if (!tableIsExistsOracle(tableName, db)) {
+				create.append("CREATE TABLE ");
+			}
+		} else {
+			create.append("CREATE TABLE IF NOT EXISTS ");
+		}
+		//当StringBuilder中有值
+		if (create.length() > 10) {
+			create.append(tableName);
+			create.append("(");
+			for (int i = 0; i < columns.size(); i++) {
+				create.append(columns.get(i)).append(" ").append(types.get(i)).append(",");
+			}
+			//将最后的逗号删除
+			create.deleteCharAt(create.length() - 1);
+			create.append(")");
+			sqlList.add(create.toString());
+		}
+	}
+
+	/**
+	 * 判断oracle数据库是否存在该表
+	 */
+	private boolean tableIsExistsOracle(String tableName, DatabaseWrapper db) {
+		ResultSet resultSet;
+		try {
+			resultSet = db.queryGetResultSet("SELECT * FROM user_objects where lower(object_name) = ? "
+					, tableName.toLowerCase());
+			//如果有数据则表明该表存在，创建表
+			if (resultSet.next()) {
+				return true;
+			}
+		} catch (Exception e) {
+			throw new AppSystemException("查询表名是否在oracle数据库中存在出现异常", e);
+		}
+		return false;
+	}
+
+	/**
+	 * 执行sql
+	 */
+	void executeSql(List<String> sqlList, DatabaseWrapper db) {
+		for (String sql : sqlList) {
+			logger.info("执行的sql为： " + sql);
+			db.execute(sql);
+		}
+	}
+
+	@Override
+	public void close() {
+		dropAllTmpTable();
+		if (db != null) {
+			db.close();
+		}
+	}
+
+	/**
+	 * 删除临时增量表
+	 */
+	private void dropAllTmpTable() {
+		List<String> deleteInfo = new ArrayList<>();
+		//删除临时增量表
+		dropTableIfExists(deltaTableName, db, deleteInfo);
+		//清空表数据
+		executeSql(deleteInfo, db);
+	}
+
+	/**
+	 * 追加
+	 */
+	public void append() {
+		//1.为了防止第一次执行，yesterdayTableName表不存在，创建空表
+		createTableIfNotExists(yesterdayTableName, db, columns, types, sqlList);
+		//2.恢复今天的数据，防止重跑
+		restoreAppendData();
+		//3.插入今天新增的数据
+		insertAppendData();
+		//4.执行sql
+		executeSql(sqlList, db);
+	}
+
+	private void insertAppendData() {
+		StringBuilder insertDataSql = new StringBuilder(120);
+		//拼接查找增量并插入增量表
+		insertDataSql.append("INSERT INTO ");
+		insertDataSql.append(yesterdayTableName);
+		insertDataSql.append("(");
+		for (String col : columns) {
+			insertDataSql.append(col).append(", ");
+		}
+		insertDataSql.deleteCharAt(insertDataSql.length() - 1);
+		insertDataSql.append(" ) ");
+		insertDataSql.append(" select ");
+		for (String col : columns) {
+			insertDataSql.append(todayTableName).append(".").append(col).append(",");
+		}
+		insertDataSql.deleteCharAt(insertDataSql.length() - 1);
+		insertDataSql.append(" from ");
+		insertDataSql.append(todayTableName);
+		sqlList.add(insertDataSql.toString());
+	}
+
+	private void restoreAppendData() {
+		sqlList.add("DELETE FROM " + yesterdayTableName + " WHERE " + Constant.SDATENAME + "='" + sysDate + "'");
+	}
+
+	/**
+	 * 替换
+	 */
+	public void replace() {
+		dropTableIfExists(yesterdayTableName, db, sqlList);
+		sqlList.add("CREATE TABLE " + yesterdayTableName + "AS SELECT * FROM " + todayTableName);
+		//执行sql
+		executeSql(sqlList, db);
+	}
+}
