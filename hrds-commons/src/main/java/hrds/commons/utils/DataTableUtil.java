71fc281f4840317a3177c2feee959aee9116bb00
BY-HLL <78733419@qq.com>
2019-12-31 16:23:48 +0800

    提交数据表检查工具类（注释非贴源层表检测方法）

--- hrds-commons/src/main/java/hrds/commons/utils/DataTableUtil.java
+++ hrds-commons/src/main/java/hrds/commons/utils/DataTableUtil.java
@@ -21,21 +21,21 @@ public class DataTableUtil {
 		if (tableIsExistInSourceFileAttribute(tableName)) {
 			throw new BusinessException("表在源文件表中已经存在!" + tableName);
 		}
-		if (tableIsExistInDatatableInfo(tableName)) {
-			throw new BusinessException("表在集市数据表中已经存在!" + tableName);
-		}
-		if (tableIsExistInEdwTable(tableName)) {
-			throw new BusinessException("表在数据仓库表中已经存在!" + tableName);
-		}
-		if (tableIsExistInSdmInnerTable(tableName)) {
-			throw new BusinessException("表在流数据内部消费信息登记表中已经存在!" + tableName);
-		}
-		if (tableIsExistInMlDatatableInfo(tableName)) {
-			throw new BusinessException("表在机器学习数据信息表中已经存在!" + tableName);
-		}
-		if (tableIsExistInSysTableInfo(tableName)) {
-			throw new BusinessException("表在系统表创建信息表中已经存在!" + tableName);
-		}
+//		if (tableIsExistInDatatableInfo(tableName)) {
+//			throw new BusinessException("表在集市数据表中已经存在!" + tableName);
+//		}
+//		if (tableIsExistInEdwTable(tableName)) {
+//			throw new BusinessException("表在数据仓库表中已经存在!" + tableName);
+//		}
+//		if (tableIsExistInSdmInnerTable(tableName)) {
+//			throw new BusinessException("表在流数据内部消费信息登记表中已经存在!" + tableName);
+//		}
+//		if (tableIsExistInMlDatatableInfo(tableName)) {
+//			throw new BusinessException("表在机器学习数据信息表中已经存在!" + tableName);
+//		}
+//		if (tableIsExistInSysTableInfo(tableName)) {
+//			throw new BusinessException("表在系统表创建信息表中已经存在!" + tableName);
+//		}
 		return false;
 	}
 
@@ -51,58 +51,58 @@ public class DataTableUtil {
 				-> new BusinessException("检查表名称否重复在源文件信息表的SQL编写错误")) != 0;
 	}
 
-	@Method(desc = "判断表是否在集市数据表存在",
-			logicStep = "1.判断表是否在集市数据表存在")
-	@Param(name = "tableName", desc = "表名", range = "String类型,不大于512字符")
-	@Return(desc = "boolean", range = "true: 存在 或者 false: 不存在")
-	private static boolean tableIsExistInDatatableInfo(String tableName) {
-		//1.判断表是否在集市数据表存在
-		return Dbo.queryNumber("SELECT count(1) count FROM " + Datatable_info.TableName +
-				" WHERE lower(datatable_en_name) = lower(?)", tableName.toLowerCase()).orElseThrow(()
-				-> new BusinessException("检查表名称否重复在集市数据表的SQL编写错误")) != 0;
-	}
-
-	@Method(desc = "判断表是否在数据仓库表存在",
-			logicStep = "1.判断表是否在数据仓库表存在")
-	@Param(name = "tableName", desc = "表名", range = "String类型,不大于512字符")
-	@Return(desc = "boolean", range = "true: 存在 或者 false: 不存在")
-	private static boolean tableIsExistInEdwTable(String tableName) {
-		//1.判断表是否在集市数据表存在
-		return Dbo.queryNumber("SELECT count(1) count FROM " + Edw_table.TableName +
-				" WHERE lower(tabname) = lower(?)", tableName.toLowerCase()).orElseThrow(()
-				-> new BusinessException("检查表名称否重复在数据仓库表的SQL编写错误")) != 0;
-	}
-
-	@Method(desc = "判断表是否在流数据内部消费信息登记表存在",
-			logicStep = "1.判断表是否在流数据内部消费信息登记表存在")
-	@Param(name = "tableName", desc = "表名", range = "String类型,不大于512字符")
-	@Return(desc = "boolean", range = "true: 存在 或者 false: 不存在")
-	private static boolean tableIsExistInSdmInnerTable(String tableName) {
-		//1.判断表是否在集市数据表存在
-		return Dbo.queryNumber("SELECT count(1) count FROM " + Sdm_inner_table.TableName +
-				" WHERE lower(table_en_name) = lower(?)", tableName.toLowerCase()).orElseThrow(()
-				-> new BusinessException("检查表名称否重复在流数据内部消费信息登记表的SQL编写错误")) != 0;
-	}
-
-	@Method(desc = "判断表是否在机器学习数据信息表存在",
-			logicStep = "1.判断表是否在机器学习数据信息表存在")
-	@Param(name = "tableName", desc = "表名", range = "String类型,不大于512字符")
-	@Return(desc = "boolean", range = "true: 存在 或者 false: 不存在")
-	private static boolean tableIsExistInMlDatatableInfo(String tableName) {
-		//1.判断表是否在集市数据表存在
-		return Dbo.queryNumber("SELECT count(1) count FROM " + Ml_datatable_info.TableName +
-				" WHERE lower(stable_en_name) = lower(?)", tableName.toLowerCase()).orElseThrow(()
-				-> new BusinessException("检查表名称否重复在机器学习数据信息表的SQL编写错误")) != 0;
-	}
-
-	@Method(desc = "判断表是否在系统表创建信息表存在",
-			logicStep = "1.判断表是否在系统表创建信息表存在")
-	@Param(name = "tableName", desc = "表名", range = "String类型,不大于512字符")
-	@Return(desc = "boolean", range = "true: 存在 或者 false: 不存在")
-	private static boolean tableIsExistInSysTableInfo(String tableName) {
-		//1.判断表是否在集市数据表存在
-		return Dbo.queryNumber("SELECT count(1) count FROM " + Sys_table_info.TableName +
-				" WHERE lower(table_name) = lower(?)", tableName.toLowerCase()).orElseThrow(()
-				-> new BusinessException("检查表名称否重复在系统表创建信息表的SQL编写错误")) != 0;
-	}
+//	@Method(desc = "判断表是否在集市数据表存在",
+//			logicStep = "1.判断表是否在集市数据表存在")
+//	@Param(name = "tableName", desc = "表名", range = "String类型,不大于512字符")
+//	@Return(desc = "boolean", range = "true: 存在 或者 false: 不存在")
+//	private static boolean tableIsExistInDatatableInfo(String tableName) {
+//		//1.判断表是否在集市数据表存在
+//		return Dbo.queryNumber("SELECT count(1) count FROM " + Datatable_info.TableName +
+//				" WHERE lower(datatable_en_name) = lower(?)", tableName.toLowerCase()).orElseThrow(()
+//				-> new BusinessException("检查表名称否重复在集市数据表的SQL编写错误")) != 0;
+//	}
+//
+//	@Method(desc = "判断表是否在数据仓库表存在",
+//			logicStep = "1.判断表是否在数据仓库表存在")
+//	@Param(name = "tableName", desc = "表名", range = "String类型,不大于512字符")
+//	@Return(desc = "boolean", range = "true: 存在 或者 false: 不存在")
+//	private static boolean tableIsExistInEdwTable(String tableName) {
+//		//1.判断表是否在集市数据表存在
+//		return Dbo.queryNumber("SELECT count(1) count FROM " + Edw_table.TableName +
+//				" WHERE lower(tabname) = lower(?)", tableName.toLowerCase()).orElseThrow(()
+//				-> new BusinessException("检查表名称否重复在数据仓库表的SQL编写错误")) != 0;
+//	}
+//
+//	@Method(desc = "判断表是否在流数据内部消费信息登记表存在",
+//			logicStep = "1.判断表是否在流数据内部消费信息登记表存在")
+//	@Param(name = "tableName", desc = "表名", range = "String类型,不大于512字符")
+//	@Return(desc = "boolean", range = "true: 存在 或者 false: 不存在")
+//	private static boolean tableIsExistInSdmInnerTable(String tableName) {
+//		//1.判断表是否在集市数据表存在
+//		return Dbo.queryNumber("SELECT count(1) count FROM " + Sdm_inner_table.TableName +
+//				" WHERE lower(table_en_name) = lower(?)", tableName.toLowerCase()).orElseThrow(()
+//				-> new BusinessException("检查表名称否重复在流数据内部消费信息登记表的SQL编写错误")) != 0;
+//	}
+//
+//	@Method(desc = "判断表是否在机器学习数据信息表存在",
+//			logicStep = "1.判断表是否在机器学习数据信息表存在")
+//	@Param(name = "tableName", desc = "表名", range = "String类型,不大于512字符")
+//	@Return(desc = "boolean", range = "true: 存在 或者 false: 不存在")
+//	private static boolean tableIsExistInMlDatatableInfo(String tableName) {
+//		//1.判断表是否在集市数据表存在
+//		return Dbo.queryNumber("SELECT count(1) count FROM " + Ml_datatable_info.TableName +
+//				" WHERE lower(stable_en_name) = lower(?)", tableName.toLowerCase()).orElseThrow(()
+//				-> new BusinessException("检查表名称否重复在机器学习数据信息表的SQL编写错误")) != 0;
+//	}
+//
+//	@Method(desc = "判断表是否在系统表创建信息表存在",
+//			logicStep = "1.判断表是否在系统表创建信息表存在")
+//	@Param(name = "tableName", desc = "表名", range = "String类型,不大于512字符")
+//	@Return(desc = "boolean", range = "true: 存在 或者 false: 不存在")
+//	private static boolean tableIsExistInSysTableInfo(String tableName) {
+//		//1.判断表是否在集市数据表存在
+//		return Dbo.queryNumber("SELECT count(1) count FROM " + Sys_table_info.TableName +
+//				" WHERE lower(table_name) = lower(?)", tableName.toLowerCase()).orElseThrow(()
+//				-> new BusinessException("检查表名称否重复在系统表创建信息表的SQL编写错误")) != 0;
+//	}
 }
