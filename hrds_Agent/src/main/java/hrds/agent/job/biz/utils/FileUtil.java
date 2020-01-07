024406a3101ebfd71c2b9b664effead7a0c2104e
wzc171500 <654951577@qq.com>
2020-01-07 10:49:43 +0800

    提交代码，修改写文件工具类

--- hrds_Agent/src/main/java/hrds/agent/job/biz/utils/FileUtil.java
+++ hrds_Agent/src/main/java/hrds/agent/job/biz/utils/FileUtil.java
@@ -129,11 +129,7 @@ public class FileUtil {
 	}
 
 	public static void writeString2File(File file, String context, String encoding) throws IOException{
-		if (file.exists() && file.isFile()) {
-			FileUtils.write(file, context, encoding);
-		} else {
-			throw new IllegalArgumentException(file.getName() + "：不是一个可读的文件");
-		}
+		FileUtils.write(file, context, encoding);
 	}
 
 	/**
