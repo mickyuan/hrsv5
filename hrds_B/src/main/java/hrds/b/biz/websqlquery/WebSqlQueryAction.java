59e631619101b879a245c6b3331356a64b10e42f
BY-HLL <78733419@qq.com>
2020-01-08 09:36:15 +0800

    提交树菜单查询工具类（完善后台查询）

--- hrds_B/src/main/java/hrds/b/biz/websqlquery/WebSqlQueryAction.java
+++ hrds_B/src/main/java/hrds/b/biz/websqlquery/WebSqlQueryAction.java
@@ -4,16 +4,11 @@ import fd.ng.core.annotation.DocClass;
 import fd.ng.core.annotation.Method;
 import fd.ng.core.annotation.Param;
 import fd.ng.core.annotation.Return;
-import fd.ng.core.utils.DateUtil;
 import fd.ng.db.jdbc.SqlOperator;
 import fd.ng.db.resultset.Result;
 import fd.ng.web.util.Dbo;
 import hrds.commons.base.BaseAction;
-import hrds.commons.codes.AgentType;
-import hrds.commons.codes.IsFlag;
-import hrds.commons.entity.Agent_info;
-import hrds.commons.entity.Data_source;
-import hrds.commons.zTree.ZTreeUtil;
+import hrds.commons.zTree.TreeUtil;
 import hrds.commons.zTree.bean.TreeDataInfo;
 
 import java.sql.ResultSet;
@@ -24,80 +19,6 @@ import java.util.Map;
 public class WebSqlQueryAction extends BaseAction {
 
 	private static final SqlOperator.Assembler asmSql = SqlOperator.Assembler.newInstance();
-
-	@Method(desc = "获取登录用户部门下的数据库采集和DB文件采集所有任务信息",
-			logicStep = "1.获取当前用户所在部门的所有数据源信息" +
-					"2.初始化采集任务信息" +
-					"3.获取数据源下采集任务信息" +
-					"3-1.获取DB文件采集任务信息" +
-					"3-2.获取数据库采集任务信息")
-	@Return(desc = "登录用户部门的数据源所有任务信息", range = "无限制")
-	public Result getCollectionTaskInfo() {
-		//数据权限验证: 根据登录用户所在部门进行数据验证
-		//1.获取当前用户所在部门的所有数据源信息
-		Result dataSourceRs = Dbo.queryResult("SELECT ds.SOURCE_ID, ds.DATASOURCE_NAME from" +
-				" source_relation_dep srd JOIN data_source ds on srd.SOURCE_ID = ds.SOURCE_ID" +
-				" where srd.dep_id = ?", getUser().getDepId());
-		//2.初始化采集任务信息
-		Result databaseCollectionTaskRs = new Result();
-		Result dbFileCollectionTaskRs = new Result();
-		//3.获取数据源下采集任务信息
-		//设置数据源实体
-		Data_source dataSource = new Data_source();
-		//设置Agent信息实体
-		Agent_info agentInfo = new Agent_info();
-		for (int i = 0; i < dataSourceRs.getRowCount(); i++) {
-			dataSource.setSource_id(dataSourceRs.getString(i, "source_id"));
-			agentInfo.setAgent_type(AgentType.ShuJuKu.getCode());
-			asmSql.clean();
-			asmSql.addSql("SELECT dbs.DATABASE_ID,dbs.TASK_NAME from data_source ds JOIN agent_info ai" +
-					" ON ds.source_id = ai.SOURCE_ID JOIN database_set dbs ON ai.AGENT_ID = dbs.AGENT_ID" +
-					" WHERE ds.SOURCE_ID = ? and ai.agent_type=? and dbs.is_sendok = ?");
-			asmSql.addParam(dataSource.getSource_id());
-			asmSql.addParam(agentInfo.getAgent_type());
-			asmSql.addParam(IsFlag.Shi.getCode());
-			databaseCollectionTaskRs = Dbo.queryResult(asmSql.sql(), asmSql.params());
-			dataSourceRs.setObject(i, "databaseCollectionTaskRs", databaseCollectionTaskRs.toList());
-			asmSql.cleanParams();
-			agentInfo.setAgent_type(AgentType.DBWenJian.getCode());
-			asmSql.addParam(dataSource.getSource_id());
-			asmSql.addParam(agentInfo.getAgent_type());
-			asmSql.addParam(IsFlag.Shi.getCode());
-			dbFileCollectionTaskRs = Dbo.queryResult(asmSql.sql(), asmSql.params());
-			dataSourceRs.setObject(i, "dbFileCollectionTaskRs", dbFileCollectionTaskRs.toList());
-		}
-		return dataSourceRs;
-	}
-
-	@Method(desc = "获取登录用户部门的集市表信息",
-			logicStep = "1.获取当前用户所在部门的所有集市源信息" +
-					"2.初始化集市信息" +
-					"3.获取集市表信息")
-	@Return(desc = "登录用户部门下所有集市数据表信息", range = "无限制")
-	public Result getTablesFromMarket() {
-		//1.获取当前用户所在部门的所有集市源信息
-		Result dataMarketInfoRs = Dbo.queryResult("SELECT data_mart_id from data_mart_info");
-		//2.初始化集市表信息
-		Result marketTables;
-		//3.获取集市表信息
-		for (int i = 0; i < dataMarketInfoRs.getRowCount(); i++) {
-			marketTables = Dbo.queryResult("SELECT * from datatable_info where data_mart_id = ?" +
-							" and datatable_due_date >=?",
-					dataMarketInfoRs.getLong(i, "data_mart_id"), DateUtil.getSysDate());
-			dataMarketInfoRs.setObject(i, "marketTables", marketTables.toList());
-		}
-		return dataMarketInfoRs;
-	}
-
-	@Method(desc = "根据数据库设置id获取表信息",
-			logicStep = "1.根据数据库设置id获取表信息")
-	@Param(name = "collect_set_id", desc = "数据库设置id", range = "Integer类型，长度为10")
-	@Return(desc = "集市数据表信息", range = "无限制")
-	public Result getTableInfoByCollectSetId(long collect_set_id) {
-		//1.根据数据库设置id获取表信息
-		return Dbo.queryResult("SELECT * FROM source_file_attribute WHERE COLLECT_SET_ID = ?",
-				collect_set_id);
-	}
 
 	@Method(desc = "根据表名获取采集数据，默认显示10条",
 			logicStep = "1.初始化查询" +
@@ -171,7 +92,7 @@ public class WebSqlQueryAction extends BaseAction {
 	                                           String database_type, String isFileCo, String tree_menu_from,
 	                                           String isPublicLayer, String isRootNode) {
 		//1.声明获取到 zTreeUtil 的对象
-		ZTreeUtil zTreeUtil = new ZTreeUtil();
+		TreeUtil treeUtil = new TreeUtil();
 		//2.设置树实体
 		TreeDataInfo treeDataInfo = new TreeDataInfo();
 		treeDataInfo.setAgent_layer(agent_layer);
@@ -194,7 +115,7 @@ public class WebSqlQueryAction extends BaseAction {
 		treeDataInfo.setIsShTable(isRootNode);
 		//3.调用ZTreeUtil的getTreeDataInfo获取树数据信息
 		Map<String, Object> treeSourcesMap = new HashMap<>();
-		treeSourcesMap.put("tree_sources", zTreeUtil.getTreeDataInfo(getUser(), treeDataInfo));
+		treeSourcesMap.put("tree_sources", treeUtil.getTreeDataInfo(getUser(), treeDataInfo));
 		return treeSourcesMap;
 	}
 
@@ -209,17 +130,17 @@ public class WebSqlQueryAction extends BaseAction {
 	@Return(desc = "树数据检索结果Map信息", range = "无限制")
 	public Map<String, Object> getTreeNodeSearchInfo(String tree_menu_from, String tableName, String isFileCo,
 	                                                 String isRootNode) {
-		//1.声明获取到 zTreeUtil 的对象
-		ZTreeUtil zTreeUtil = new ZTreeUtil();
+		//1.声明获取到 treeUtil 的对象
+		TreeUtil treeUtil = new TreeUtil();
 		//2.设置树实体
 		TreeDataInfo treeDataInfo = new TreeDataInfo();
 		treeDataInfo.setPage_from(tree_menu_from);
 		treeDataInfo.setTableName(tableName);
 		treeDataInfo.setIsFileCo(isFileCo);
 		treeDataInfo.setIsShTable(isRootNode);
-		//3.调用ZTreeUtil的getTreeNodeSearchInfo获取检索的结果的信息
+		//3.调用ZTreeUtil的getTreeNodeSearchInfo获取检索的数据信息
 		Map<String, Object> treeNodeSearchMap = new HashMap<>();
-		treeNodeSearchMap.put("search_nodes", zTreeUtil.getTreeNodeSearchInfo(getUser(), treeDataInfo));
+		treeNodeSearchMap.put("search_nodes", treeUtil.getTreeNodeSearchInfo(getUser(), treeDataInfo));
 		return treeNodeSearchMap;
 	}
 }
