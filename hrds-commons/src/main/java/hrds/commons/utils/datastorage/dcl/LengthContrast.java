6d73b692c05c028bf69a4e7af35211e27ae68364
Qiang-Lee <hliqiang1989@gmail.com>
2020-02-11 21:41:27 +0800

    增加系统参数,数据类型,数据类型长度yaml文件内容的查询

---
+++ hrds-commons/src/main/java/hrds/commons/utils/datastorage/dcl/LengthContrast.java
@@ -0,0 +1,60 @@
+package hrds.commons.utils.datastorage.dcl;
+
+import fd.ng.core.annotation.DocClass;
+import fd.ng.db.jdbc.DatabaseWrapper;
+import fd.ng.db.jdbc.SqlOperator;
+import hrds.commons.utils.datastorage.yamldata.YamlDataFormat;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@DocClass(desc = "储层数据类型长度对照表", author = "Mr.Lee", createdate = "2020-01-13 15:50")
+public class LengthContrast implements YamlDataFormat {
+
+  /** prefix : 前缀名称 */
+  public static final String PREFIX = "lengthcontrast";
+  /** name : 类型对照名称 */
+  public static final String NAME = "NAME";
+
+  @Override
+  public Map<String, List<Map<String, Object>>> yamlDataFormat() {
+    Map<String, Map<String, Object>> contrastMap = new LinkedHashMap<>();
+    getLengthContrast()
+        .forEach(
+            item -> {
+              String dtcs_name = ((String) item.get("dlcs_name")).toUpperCase();
+              String source_type = ((String) item.get("dlc_type")).toUpperCase();
+              Object target_type = item.get("dlc_length");
+
+              if (contrastMap.containsKey(dtcs_name)) {
+                contrastMap.get(dtcs_name).put(source_type, target_type);
+              } else {
+                Map<String, Object> map = new LinkedHashMap<>();
+                map.put(NAME, dtcs_name);
+                map.put(source_type, target_type);
+                contrastMap.put(dtcs_name, map);
+              }
+            });
+
+    List<Map<String, Object>> typeContrastAll = new ArrayList<>();
+    contrastMap.forEach(
+        (dtcs_name, typeContrast) -> {
+          typeContrastAll.add(typeContrast);
+        });
+
+    Map<String, List<Map<String, Object>>> map = new LinkedHashMap<>();
+    map.put(PREFIX, typeContrastAll);
+
+    return map;
+  }
+
+  private List<Map<String, Object>> getLengthContrast() {
+    try (DatabaseWrapper db = new DatabaseWrapper()) {
+      return SqlOperator.queryList(
+          db,
+          "SELECT t2.dlcs_name,t1.dlc_type,t1.dlc_length FROM length_contrast t1 LEFT JOIN "
+              + "length_contrast_sum t2 ON t1.dlcs_id = t2.dlcs_id");
+    }
+  }
+}
---
+++ hrds-commons/src/main/java/hrds/commons/utils/syspara/SysPara.java
@@ -0,0 +1,40 @@
+package hrds.commons.utils.syspara;
+
+import fd.ng.core.annotation.DocClass;
+import fd.ng.db.jdbc.DatabaseWrapper;
+import fd.ng.db.jdbc.SqlOperator;
+import hrds.commons.utils.datastorage.yamldata.YamlDataFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@DocClass(desc = "系统参数Yaml数据", author = "Mr.Lee", createdate = "2020-01-15 11:22")
+public class SysPara implements YamlDataFormat {
+  private static final String PREFIX = "param";
+
+  @Override
+  public Map<String, List<Map<String, Object>>> yamlDataFormat() {
+
+    Map<String, List<Map<String, Object>>> map = new LinkedHashMap<>();
+    Map<String, Object> sysMap = new HashMap<>();
+    getSysPara()
+        .forEach(
+            item -> {
+              sysMap.put(((String) item.get("para_name")), item.get("para_value"));
+            });
+
+    List<Map<String, Object>> list = new ArrayList<>();
+    list.add(sysMap);
+    map.put(PREFIX, list);
+    return map;
+  }
+
+  private List<Map<String, Object>> getSysPara() {
+
+    try (DatabaseWrapper db = new DatabaseWrapper()) {
+      return SqlOperator.queryList(db, "SELECT * FROM sys_para");
+    }
+  }
+}
