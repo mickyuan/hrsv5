468dde73f59c7ea4dff33112f926eefcf77637bd
mine <duhuawei@beyondsoft.com>
2020-03-23 15:20:46 +0800

    工具类修改

--- hrds_C/src/main/java/hrds/c/biz/util/redisconf/RedisParam.java
+++ hrds_C/src/main/java/hrds/c/biz/util/redisconf/RedisParam.java
@@ -1,4 +1,4 @@
-package hrds.commons.utils.datastorage.redisconf;
+package hrds.c.biz.util.redisconf;
 
 import fd.ng.core.annotation.DocClass;
 import fd.ng.core.utils.StringUtil;
@@ -14,13 +14,14 @@ public class RedisParam {
 
 	public static Map<String, Object> getRedisParam() {
 		Map<String, Object> redisMap = new HashMap<>();
-		Map<String, String> map = setRedisParam("", "");
+		Map<String, Object> map = setRedisParam("", "");
 		redisMap.put("redis", map);
 		return redisMap;
 	}
 
-	public static Map<String, String> setRedisParam(String redis_ip, String redis_port) {
-		Map<String, String> map = new HashMap<>();
+	public static Map<String, Object> setRedisParam(String redis_ip, String redis_port) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("timeout", 100);
 		if (StringUtil.isBlank(redis_ip) || StringUtil.isBlank(redis_port)) {
 			map.put("ip", PropertyParaValue.getString("redis_ip", "10.71.4.52"));
 			map.put("port", PropertyParaValue.getString("redis_port", "56379"));
