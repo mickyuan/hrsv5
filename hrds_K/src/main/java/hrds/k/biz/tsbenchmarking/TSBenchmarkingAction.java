4978e81a642a8274dcd0bb96b11241068559b8a8
BY-HLL <78733419@qq.com>
2020-03-25 15:06:04 +0800

    提交表结构对标(完成提交)

--- hrds_K/src/main/java/hrds/k/biz/tsbenchmarking/TSBenchmarkingAction.java
+++ hrds_K/src/main/java/hrds/k/biz/tsbenchmarking/TSBenchmarkingAction.java
@@ -24,14 +24,12 @@ import hrds.commons.utils.PropertyParaValue;
 import hrds.commons.utils.key.PrimayKeyGener;
 import hrds.commons.utils.tree.Node;
 import hrds.commons.utils.tree.NodeDataConvertedTreeList;
+import hrds.k.biz.dbmnormbasic.DbmNormbasicAction;
 import hrds.k.biz.tsbenchmarking.bean.TSBConf;
 import hrds.k.biz.tsbenchmarking.query.TSBQuery;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @DocClass(desc = "表结构对标", author = "BY-HLL", createdate = "2020/3/12 0012 上午 09:10")
 public class TSBenchmarkingAction extends BaseAction {
@@ -95,56 +93,12 @@ public class TSBenchmarkingAction extends BaseAction {
         return col_info_s;
     }
 
-    @Method(desc = "设置对标检测字段信息表",
-            logicStep = "设置对标检测字段信息表")
-    @Param(name = "col_info_s", desc = "字段信息", range = "String类型,格式" +
-            "[column_id: 1000369846" +
-            "tc_remark: 参数名" +
-            "is_primary_key: 0" +
-            "agent_id: 1000003138" +
-            "database_id: 1000369838" +
-            "column_name: para_name" +
-            "column_ch_name: 参数名" +
-            "source_id: 1000003137" +
-            "column_type: varchar(512)]")
-    @Return(desc = "检测字段信息表信息集合", range = "检测字段信息表信息集合")
-    public void setDbmDtcolInfo(String col_info_s) {
-        //设置 Dbm_dtcol_info 对象
-        Type type = new TypeReference<List<Map<String, String>>>() {
-        }.getType();
-        List<Map<String, String>> col_info_list = JsonUtil.toObject(col_info_s, type);
-        List<Dbm_dtcol_info> dbm_dtcol_info_list = new ArrayList<>();
-        col_info_list.forEach(col_info -> {
-            Dbm_dtcol_info dbm_dtcol_info = new Dbm_dtcol_info();
-            dbm_dtcol_info.setCol_id(PrimayKeyGener.getNextId());
-            dbm_dtcol_info.setCol_cname(col_info.get("column_ch_name"));
-            dbm_dtcol_info.setCol_ename(col_info.get("column_name"));
-            if (null == col_info.get("tc_remark")) {
-                dbm_dtcol_info.setCol_remark("");
-            } else {
-                dbm_dtcol_info.setCol_remark(col_info.get("tc_remark"));
-            }
-            Map<String, String> col_type_map = parsingFiledType(col_info.get("column_type"));
-            dbm_dtcol_info.setData_type(col_type_map.get("data_type"));
-            dbm_dtcol_info.setData_len(col_type_map.get("data_len"));
-            dbm_dtcol_info.setDecimal_point(col_type_map.get("decimal_point"));
-            dbm_dtcol_info.setIs_key(col_info.get("is_primary_key"));
-            dbm_dtcol_info.setIs_null(IsFlag.Fou.getCode());
-            dbm_dtcol_info.setDefault_value("");
-            dbm_dtcol_info.setDbm_tableid(tsbConf.getDbm_dtable_info().getDbm_tableid());
-            dbm_dtcol_info.setDetect_id(tsbConf.getDbm_normbm_detect().getDetect_id());
-            dbm_dtcol_info.setColumn_id(col_info.get("column_id"));
-            dbm_dtcol_info.setDatabase_id(col_info.get("database_id"));
-            dbm_dtcol_info.setAgent_id(col_info.get("agent_id"));
-            dbm_dtcol_info.setSource_id(col_info.get("source_id"));
-            dbm_dtcol_info_list.add(dbm_dtcol_info);
-            tsbConf.setDbm_dtcol_info_list(dbm_dtcol_info_list);
-        });
-    }
-
-    @Method(desc = "获取预测对标结果", logicStep = "获取预测对标结果")
+    @Method(desc = "预测对标结果", logicStep = "预测对标结果")
+    @Param(name = "col_info_s", desc = "字段信息", range = "String类型")
     @Return(desc = "预测对标结果", range = "预测对标结果")
-    public Map<String, Object> getPredictBenchmarkingResults() {
+    public void predictBenchmarking(String col_info_s) {
+        //设置待检测字段信息
+        setDbmDtcolInfo(col_info_s);
         //获取待检测字段信息
         List<Dbm_dtcol_info> dbm_dtcol_infos = tsbConf.getDbm_dtcol_info_list();
         //获取预测接口地址信息
@@ -156,7 +110,7 @@ public class TSBenchmarkingAction extends BaseAction {
             if (StringUtil.isBlank(dbm_dtcol_info.getCol_id().toString())) {
                 throw new BusinessException("检测字段主键为空!");
             }
-            map.put("id", dbm_dtcol_info.getCol_id().toString());
+            map.put("col_id", dbm_dtcol_info.getCol_id().toString());
             if (StringUtil.isBlank(dbm_dtcol_info.getCol_cname())) {
                 throw new BusinessException("检测字段中文名为空!");
             }
@@ -170,25 +124,137 @@ public class TSBenchmarkingAction extends BaseAction {
                 .addData("content", JsonUtil.toJson(params)).post(predict_address).getBodyString();
         ActionResult ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class).orElseThrow(() ->
                 new BusinessException("URL请求失败!" + predict_address));
-        //设置返回结果
-        Map<String, Object> predictDataMap = new HashMap<>();
-        predictDataMap.put("predictData", JsonUtil.toObjectSafety(ar.getData().toString(), List.class));
-        return predictDataMap;
+        //初始化结果数据List
+        List<Map<String, String>> dbm_normbmd_info_list = new ArrayList<>();
+        //设置预测结果集合数据类型为 List<Map<String, Object>>
+        Type type = new TypeReference<List<Map<String, Object>>>() {
+        }.getType();
+        List<Map<String, Object>> predictDataList = JsonUtil.toObject(ar.getData().toString(), type);
+        //处理每个字段的预测结果
+        predictDataList.forEach(predictData -> {
+            Type lo = new TypeReference<List<Object>>() {
+            }.getType();
+            List<Object> predicts = JsonUtil.toObject(predictData.get("predict").toString(), lo);
+            String col_id = predictData.get("col_id").toString();
+            //处理每个字段预测结果的匹配度
+            predicts.forEach(predict -> {
+                List<Object> pMap = JsonUtil.toObject(predict.toString(), lo);
+                Type mss = new TypeReference<Map<String, String>>() {
+                }.getType();
+                Map<String, String> predictMap = JsonUtil.toObject(pMap.get(0).toString(), mss);
+                String standard_id = predictMap.get("standard_id");
+                String col_similarity = pMap.get(1).toString();
+                //设置预测结果集信息
+                Map<String, String> map = new HashMap<>();
+                map.put("col_id", col_id);
+                map.put("standard_id", standard_id);
+                map.put("col_similarity", col_similarity);
+                dbm_normbmd_info_list.add(map);
+            });
+        });
+        //设置 Dbm_normbmd_result
+        setDbmNormbmdResult(dbm_normbmd_info_list);
+    }
+
+    @Method(desc = "获取预测结果信息", logicStep = "获取预测结果信息")
+    @Return(desc = "预测结果信息", range = "预测结果信息")
+    public List<Map<String, Object>> getPredictResult() {
+        List<Map<String, Object>> predictResult = new ArrayList<>();
+        //获取对标结果字段信息
+        List<Dbm_dtcol_info> dbm_dtcol_info_s = tsbConf.getDbm_dtcol_info_list();
+        System.out.println(dbm_dtcol_info_s);
+        if (null == dbm_dtcol_info_s || dbm_dtcol_info_s.isEmpty()) {
+            throw new BusinessException("对标信息已失效,请重新选择字段进行对标!");
+        }
+        dbm_dtcol_info_s.forEach(dbm_dtcol_info -> {
+            Map<String, Object> map = new HashMap<>();
+            map.put("col_id", dbm_dtcol_info.getCol_id());
+            map.put("col_cname", dbm_dtcol_info.getCol_cname());
+            map.put("col_ename", dbm_dtcol_info.getCol_ename());
+            //获取预测结果信息
+            List<Map<String, String>> pList = new ArrayList<>();
+            List<Dbm_normbmd_result> dbm_normbmd_result_s = tsbConf.getDbm_normbmd_result_list();
+            dbm_normbmd_result_s.forEach(dbm_normbmd_result -> {
+                if (dbm_dtcol_info.getCol_id().equals(dbm_normbmd_result.getCol_id())) {
+                    Map<String, String> pMap = new HashMap<>();
+                    //通过标准的basic_id获取标准信息
+                    DbmNormbasicAction dbmNormbasicAction = new DbmNormbasicAction();
+                    Optional<Dbm_normbasic> dbmNormbasic =
+                            dbmNormbasicAction.getDbmNormbasicInfoById(dbm_normbmd_result.getBasic_id());
+                    if (dbmNormbasic.isPresent()) {
+                        pMap.put("result_id", dbm_normbmd_result.getResult_id());
+                        pMap.put("norm_ename", dbmNormbasic.get().getNorm_ename());
+                        pMap.put("norm_cname", dbmNormbasic.get().getNorm_cname());
+                    }
+                    pMap.put("col_similarity", dbm_normbmd_result.getCol_similarity().toString());
+                    pList.add(pMap);
+                }
+            });
+            map.put("predict", pList);
+            predictResult.add(map);
+        });
+        return predictResult;
     }
 
     @Method(desc = "保存表结构对标数据", logicStep = "保存表结构对标数据")
     @Param(name = "dbm_normbmd_info_s", desc = "对标结果数据信息",
-            range = "String类型,JsonArray格式,[{col_id:'',col_similarity:'',standard_id:'',is_artificial:''}]" +
+            range = "String类型,JsonArray格式,[{col_id:'',result_id:'',is_artificial:''}]" +
                     "col_id: 字段id,唯一性" +
-                    "col_similarity: 字段相似度" +
-                    "standard_id: 标准编号(唯一性),如: IP600012" +
+                    "result_id: 对标最终选择结果id" +
                     "is_artificial: 是否人工对标,IsFlag标识,1:是,0:否")
     public void saveTSBConfData(String dbm_normbmd_info_s) {
-        //设置 Dbm_normbmd_result
+        //字符串类型内容转换成List<Map<String, String>>
         Type type = new TypeReference<List<Map<String, String>>>() {
         }.getType();
         List<Map<String, String>> dbm_normbmd_info_list = JsonUtil.toObject(dbm_normbmd_info_s, type);
-        setDbmNormbmdResult(dbm_normbmd_info_list);
+        //数据校验
+        if (null == dbm_normbmd_info_list || dbm_normbmd_info_list.isEmpty()) {
+            throw new BusinessException("待保存的信息已失效,请重新选择字段并进行对标结果定义后再保存!");
+        }
+        dbm_normbmd_info_list.forEach(dbm_normbmd_info -> {
+            if (StringUtil.isBlank(dbm_normbmd_info.get("col_id"))) {
+                throw new BusinessException("字段:" + dbm_normbmd_info.get("col_ename") + "对标字段id为空!");
+            }
+            if (StringUtil.isBlank(dbm_normbmd_info.get("is_artificial"))) {
+                throw new BusinessException("字段:" + dbm_normbmd_info.get("col_ename") + "对标结果为空,请选择对标结果!");
+            }
+            if (StringUtil.isBlank(dbm_normbmd_info.get("result_id"))) {
+                throw new BusinessException("字段:" + dbm_normbmd_info.get("col_ename") + "对标最终选择结果为空!");
+            }
+        });
+        //更新 Dbm_normbmd_result
+        dbm_normbmd_info_list.forEach(dbm_normbmd_info -> {
+            //如果不是人工对标,标记选中的对标结果为最终结果
+            if (dbm_normbmd_info.get("is_artificial").equals(IsFlag.Fou.getCode())) {
+                List<Dbm_normbmd_result> dbm_normbmd_result_s = tsbConf.getDbm_normbmd_result_list();
+                if (null == dbm_normbmd_result_s || dbm_normbmd_result_s.isEmpty()) {
+                    throw new BusinessException("待标记为最终结果的对标结果已失效,请重新选择字段并进行对标后再保存!");
+                }
+                //修改保存结果集中选中的结果为最终结果
+                dbm_normbmd_result_s.forEach(dbm_normbmd_result -> {
+                    if (dbm_normbmd_info.get("result_id").equals(dbm_normbmd_result.getResult_id())) {
+                        dbm_normbmd_result.setIs_tag(IsFlag.Shi.getCode());
+                    }
+                });
+            }
+            //如果是人工对标则保存人工对标结果
+            else if (dbm_normbmd_info.get("is_artificial").equals(IsFlag.Shi.getCode())) {
+                Dbm_normbmd_result dbm_normbmd_result = new Dbm_normbmd_result();
+                dbm_normbmd_result.setResult_id(PrimayKeyGener.getNextId());
+                //如果是人工对标,字段相识度和描述相似度都为1
+                dbm_normbmd_result.setCol_similarity(IsFlag.Shi.getCode());
+                dbm_normbmd_result.setRemark_similarity(IsFlag.Shi.getCode());
+                dbm_normbmd_result.setDetect_id(tsbConf.getDbm_normbm_detect().getDetect_id());
+                dbm_normbmd_result.setCol_id(dbm_normbmd_info.get("col_id"));
+                //如果是人工对标,result_id是页面选中标准的basic_id
+                dbm_normbmd_result.setBasic_id(dbm_normbmd_info.get("result_id"));
+                dbm_normbmd_result.setIs_artificial(dbm_normbmd_info.get("is_artificial"));
+                dbm_normbmd_result.setIs_tag(IsFlag.Shi.getCode());
+                tsbConf.getDbm_normbmd_result_list().add(dbm_normbmd_result);
+            } else {
+                throw new BusinessException("对标方式为空或者对标方式不正确!" + dbm_normbmd_info.get("is_artificial"));
+            }
+        });
         //结果存入数据库
         try (DatabaseWrapper db = new DatabaseWrapper()) {
             //设置检测结束时间
@@ -206,6 +272,11 @@ public class TSBenchmarkingAction extends BaseAction {
             tsbConf.getDbm_normbmd_result_list().forEach(dbm_normbmd_result -> dbm_normbmd_result.add(db));
             //提交所有数据库执行操作
             SqlOperator.commitTransaction(db);
+            //数据保存完成后,清理本次操作数据
+            tsbConf.setDbm_normbm_detect(new Dbm_normbm_detect());
+            tsbConf.setDbm_dtable_info(new Dbm_dtable_info());
+            tsbConf.getDbm_dtcol_info_list().clear();
+            tsbConf.getDbm_normbmd_result_list().clear();
         }
     }
 
@@ -258,6 +329,71 @@ public class TSBenchmarkingAction extends BaseAction {
         tsbConf.setDbm_dtable_info(dbm_dtable_info);
     }
 
+    @Method(desc = "设置对标检测字段信息表",
+            logicStep = "设置对标检测字段信息表")
+    @Param(name = "col_info_s", desc = "字段信息", range = "String类型")
+    @Param(name = "dbmColInfos", desc = "字段信息", range = "String类型")
+    @Return(desc = "检测字段信息表信息集合", range = "检测字段信息表信息集合")
+    private void setDbmDtcolInfo(String col_info_s) {
+        //设置 Dbm_dtcol_info 对象
+        Type type = new TypeReference<List<Map<String, String>>>() {
+        }.getType();
+        List<Map<String, String>> col_info_list = JsonUtil.toObject(col_info_s, type);
+        List<Dbm_dtcol_info> dbm_dtcol_info_list = new ArrayList<>();
+//        for (DbmColInfo col_info : dbmColInfos) {
+//            Dbm_dtcol_info dbm_dtcol_info = new Dbm_dtcol_info();
+//            dbm_dtcol_info.setCol_id(PrimayKeyGener.getNextId());
+//            dbm_dtcol_info.setCol_cname(col_info.getColumn_ch_name());
+//            dbm_dtcol_info.setCol_ename(col_info.getColumn_name());
+//            if (null == col_info.getTc_remark()) {
+//                dbm_dtcol_info.setCol_remark("");
+//            } else {
+//                dbm_dtcol_info.setCol_remark(col_info.getTc_remark());
+//            }
+//            Map<String, String> col_type_map = parsingFiledType(col_info.getColumn_type());
+//            dbm_dtcol_info.setData_type(col_type_map.get("data_type"));
+//            dbm_dtcol_info.setData_len(col_type_map.get("data_len"));
+//            dbm_dtcol_info.setDecimal_point(col_type_map.get("decimal_point"));
+//            dbm_dtcol_info.setIs_key(col_info.getIs_primary_key());
+//            dbm_dtcol_info.setIs_null(IsFlag.Fou.getCode());
+//            dbm_dtcol_info.setDefault_value("");
+//            dbm_dtcol_info.setDbm_tableid(tsbConf.getDbm_dtable_info().getDbm_tableid());
+//            dbm_dtcol_info.setDetect_id(tsbConf.getDbm_normbm_detect().getDetect_id());
+//            dbm_dtcol_info.setColumn_id(col_info.getColumn_id());
+//            dbm_dtcol_info.setDatabase_id(col_info.getDatabase_id());
+//            dbm_dtcol_info.setAgent_id(col_info.getAgent_id());
+//            dbm_dtcol_info.setSource_id(col_info.getSource_id());
+//            dbm_dtcol_info_list.add(dbm_dtcol_info);
+//            tsbConf.setDbm_dtcol_info_list(dbm_dtcol_info_list);
+//        }
+        col_info_list.forEach(col_info -> {
+            Dbm_dtcol_info dbm_dtcol_info = new Dbm_dtcol_info();
+            dbm_dtcol_info.setCol_id(PrimayKeyGener.getNextId());
+            dbm_dtcol_info.setCol_cname(col_info.get("column_ch_name"));
+            dbm_dtcol_info.setCol_ename(col_info.get("column_name"));
+            if (null == col_info.get("tc_remark")) {
+                dbm_dtcol_info.setCol_remark("");
+            } else {
+                dbm_dtcol_info.setCol_remark(col_info.get("tc_remark"));
+            }
+            Map<String, String> col_type_map = parsingFiledType(col_info.get("column_type"));
+            dbm_dtcol_info.setData_type(col_type_map.get("data_type"));
+            dbm_dtcol_info.setData_len(col_type_map.get("data_len"));
+            dbm_dtcol_info.setDecimal_point(col_type_map.get("decimal_point"));
+            dbm_dtcol_info.setIs_key(col_info.get("is_primary_key"));
+            dbm_dtcol_info.setIs_null(IsFlag.Fou.getCode());
+            dbm_dtcol_info.setDefault_value("");
+            dbm_dtcol_info.setDbm_tableid(tsbConf.getDbm_dtable_info().getDbm_tableid());
+            dbm_dtcol_info.setDetect_id(tsbConf.getDbm_normbm_detect().getDetect_id());
+            dbm_dtcol_info.setColumn_id(col_info.get("column_id"));
+            dbm_dtcol_info.setDatabase_id(col_info.get("database_id"));
+            dbm_dtcol_info.setAgent_id(col_info.get("agent_id"));
+            dbm_dtcol_info.setSource_id(col_info.get("source_id"));
+            dbm_dtcol_info_list.add(dbm_dtcol_info);
+            tsbConf.setDbm_dtcol_info_list(dbm_dtcol_info_list);
+        });
+    }
+
     @Method(desc = "设置对标检测结果表",
             logicStep = "逻辑说明")
     @Param(name = "dbm_normbmd_info_list", desc = "对标结果集合",
@@ -274,7 +410,8 @@ public class TSBenchmarkingAction extends BaseAction {
             dbm_normbmd_result.setCol_id(dbm_normbmd_info.get("col_id"));
             //根据 standard_id 获取对应标准元id
             dbm_normbmd_result.setBasic_id(getDbmNormbasicInfoById(dbm_normbmd_info.get("standard_id")));
-            dbm_normbmd_result.setIs_artificial(dbm_normbmd_info.get("is_artificial"));
+            dbm_normbmd_result.setIs_artificial(IsFlag.Fou.getCode());
+            dbm_normbmd_result.setIs_tag(IsFlag.Fou.getCode());
             dbm_normbmd_results_list.add(dbm_normbmd_result);
             tsbConf.setDbm_normbmd_result_list(dbm_normbmd_results_list);
         });
