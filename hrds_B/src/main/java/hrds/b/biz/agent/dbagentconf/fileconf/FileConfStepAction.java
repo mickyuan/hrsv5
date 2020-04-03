42182599f9217e289a8b89a0d0ea05d7039c4162
Qiang-Lee <hliqiang1989@gmail.com>
2020-04-03 12:08:24 +0800

    修改数据库卸数配置逻辑

--- hrds_B/src/main/java/hrds/b/biz/agent/dbagentconf/fileconf/FileConfStepAction.java
+++ hrds_B/src/main/java/hrds/b/biz/agent/dbagentconf/fileconf/FileConfStepAction.java
@@ -17,9 +17,15 @@ import hrds.commons.entity.Table_info;
 import hrds.commons.exception.BusinessException;
 import hrds.commons.utils.key.PrimayKeyGener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import org.apache.avro.generic.GenericData.Array;
 
 @DocClass(desc = "定义卸数文件配置", author = "WangZhengcheng")
 public class FileConfStepAction extends BaseAction {
@@ -27,32 +33,47 @@ public class FileConfStepAction extends BaseAction {
   @Method(desc = "根据数据库设置ID获得定义卸数文件页面初始信息", logicStep = "" + "1、根据数据库设置ID去数据库中查询与数据抽取相关的信息")
   @Param(name = "colSetId", desc = "数据库设置ID，源系统数据库设置表主键，数据库对应表外键", range = "不为空")
   @Return(desc = "查询结果集", range = "不为空")
-  public Result getInitInfo(long colSetId) {
-    Result result =
-        Dbo.queryResult(
-            " select * "
-                + " from "
+  public List<Map<String, Object>> getInitInfo(long colSetId) {
+
+    List<Map<String, Object>> table_infos =
+        Dbo.queryList(
+            "SELECT table_id,table_name,table_ch_name FROM "
                 + Table_info.TableName
-                + " ti left join "
-                + Data_extraction_def.TableName
-                + " ded "
-                + " on ti.table_id = ded.table_id where ti.database_id = ?",
+                + " WHERE database_id = ?",
             colSetId);
-    if (!result.isEmpty()) {
-      for (int i = 0; i < result.getRowCount(); i++) {
-        if (StringUtil.isNotBlank(result.getString(i, "row_separator"))) {
-          result.setValue(
-              i, "row_separator", StringUtil.unicode2String(result.getString(i, "row_separator")));
-        }
-        if (StringUtil.isNotBlank(result.getString(i, "database_separatorr"))) {
-          result.setValue(
-              i,
-              "database_separatorr",
-              StringUtil.unicode2String(result.getString(i, "database_separatorr")));
-        }
-      }
-    }
-    return result;
+
+    table_infos.forEach(
+        tableInfo -> {
+          List<Map<String, Object>> list =
+              Dbo.queryList(
+                  " select * "
+                      + " from "
+                      + Table_info.TableName
+                      + " ti left join "
+                      + Data_extraction_def.TableName
+                      + " ded "
+                      + " on ti.table_id = ded.table_id where ti.database_id = ? AND ti.table_id = ? ",
+                  colSetId,
+                  (long) tableInfo.get("table_id"));
+          list.forEach(
+              item -> {
+                /*
+                把unicode转换为字符串
+                1: 行分隔符  2: 数据分隔符
+                 */
+                //            1: 行分隔符
+                item.put(
+                    "row_separator",
+                    StringUtil.unicode2String(((String) item.get("row_separator"))));
+                //            2: 数据分隔符
+                item.put(
+                    "database_separatorr",
+                    StringUtil.unicode2String(((String) item.get("database_separatorr"))));
+              });
+          tableInfo.put("tableData", list);
+        });
+
+    return table_infos;
   }
 
   @Method(
@@ -91,12 +112,7 @@ public class FileConfStepAction extends BaseAction {
               + "4、保存数据")
   @Param(
       name = "extractionDefString",
-      desc =
-          "存有待保存信息的Data_extraction_def实体数组格式字符串"
-              + "注意："
-              //							+ "(1)、数据抽取方式请从DataExtractType代码项取值"
-              + "(1)、数据字符集请从DataBaseCode代码项取值"
-              + "(2)、抽取数据存储方式请从FileFormat代码项取值",
+      desc = "1: 检查当前传入的数据是否为空" + "2: 删除上次任务的卸数配置信息" + "3: 循环卸数配置信息并进行保存",
       range =
           "每张表的抽取方式(dbfile_format)为多种, 抽取方式为"
               + "非定长,定长,CSV : 请传递数数据 换行符(row_separator),数据分隔符(database_separatorr),数据字符集(database_code),落地目录(plane_url);"
@@ -109,24 +125,29 @@ public class FileConfStepAction extends BaseAction {
   @Param(name = "colSetId", desc = "数据库采集设置表ID", range = "不为空")
   @Return(desc = "返回数据库设置ID，方便下一个页面能够通过这个参数加载初始化设置", range = "不为空")
   public long saveFileConf(Data_extraction_def[] extractionDefString, long colSetId) {
-    // 1、将传入的json格式的字符串转换为List<Data_extraction_def>集合
-    //		List<Data_extraction_def> dataExtractionDefs = JSONArray.parseArray(extractionDefString,
-    //				Data_extraction_def.class);
 
-    if (extractionDefString == null || extractionDefString.length != 0) {
+//    1: 检查当前传入的数据是否为空
+    if (extractionDefString == null || extractionDefString.length == 0) {
       throw new BusinessException("未获取到卸数文件信息");
     }
 
-    // 2、遍历集合，对集合中的内容调用方法进行校验
-    verifySeqConf(extractionDefString);
+    // 2: 删除上次任务的卸数配置信息
+    Dbo.execute(
+        "delete from "
+            + Data_extraction_def.TableName
+            + " where table_id in (select table_id from table_info where database_id = ?)",
+        colSetId);
+
+//    3: 循环卸数配置信息并进行保存
+    //    verifySeqConf(extractionDefString);
     for (int i = 0; i < extractionDefString.length; i++) {
 
       Data_extraction_def def = extractionDefString[i];
 
-      // 3、根据table_id去data_extraction_def表中删除尝试删除该表曾经的卸数文件配置，不关心删除数目
-      Dbo.execute(
-          "delete from " + Data_extraction_def.TableName + " where table_id = ?",
-          def.getTable_id());
+      // 校验保存数据必须关联表
+      if (def.getTable_id() == null) {
+        throw new BusinessException("保存卸数文件配置，第" + (i + 1) + "数据必须关联表ID");
+      }
       def.setDed_id(PrimayKeyGener.getNextId());
       // 将卸数分隔符(行，列)转换为unicode码
       if (StringUtil.isBlank(def.getDbfile_format())) {
@@ -138,19 +159,30 @@ public class FileConfStepAction extends BaseAction {
           || fileFormat == FileFormat.FeiDingChang
           || fileFormat == FileFormat.CSV) {
         // 检查行分隔符不能为空
-        if (StringUtil.isBlank(def.getRow_separator())) {
+        String row_separator = def.getRow_separator();
+        if (StringUtil.isBlank(row_separator)) {
           throw new BusinessException("第 " + (i + 1) + " 条的数据行分割符不能为空!");
         } else {
           // 如果不为空则使用Unicode进行转码入库
-          def.setRow_separator(StringUtil.string2Unicode(def.getRow_separator()));
+          def.setRow_separator(StringUtil.string2Unicode(row_separator));
         }
         // 检查数据分隔符不能为空
-        if (StringUtil.isBlank(def.getDatabase_separatorr())) {
+        String database_separatorr = def.getDatabase_separatorr();
+        if (StringUtil.isBlank(database_separatorr)) {
           throw new BusinessException("第 " + (i + 1) + " 条的数据分隔符不能为空!");
         } else {
           // 如果不为空则使用Unicode进行转码入库
-          def.setRow_separator(StringUtil.string2Unicode(def.getRow_separator()));
+          def.setDatabase_separatorr(StringUtil.string2Unicode(database_separatorr));
         }
+      }
+      // 如果是一下文件格式,不需要数据的分隔符和数据的行分隔符...这里卸数的时候使用的标准的文件格式
+      else if (fileFormat == FileFormat.ORC
+          || fileFormat == FileFormat.SEQUENCEFILE
+          || fileFormat == FileFormat.PARQUET) {
+        def.setDatabase_separatorr("");
+        def.setRow_separator("");
+      } else {
+        throw new BusinessException("传递的数据文件格式不存在在代码项中,此次获取的数据文件格式是: " + def.getDbfile_format());
       }
 
       if (StringUtil.isBlank(def.getDatabase_code())) {
@@ -163,6 +195,7 @@ public class FileConfStepAction extends BaseAction {
 
       // TODO data_extraction_def表is_header字段设置默认值为"是"，后期可能会修改
       def.setIs_header(IsFlag.Shi.getCode());
+      def.setData_extract_type(DataExtractType.ShuJuKuChouQuLuoDi.getCode());
       def.add(Dbo.db());
     }
     // 4、保存数据
@@ -212,33 +245,35 @@ public class FileConfStepAction extends BaseAction {
         // 2-2、文件格式如果是定长/CSV，那么行分隔符和列分隔符，用户可以填，可以不填
       }
       // 3、校验采集的方式如果是抽取并入库
-//      if (extractType == DataExtractType.ShuJuChouQuJiRuKu) {
-//        // 如果数据抽取方式是抽取及入库，那么校验存储方式不能是定长
-//        if (fileFormat == FileFormat.DingChang) {
-//          throw new BusinessException("抽取并入库操作，不能保存为定长文件");
-//        }
-//        // 3-1、如果是ORC/PARQUET/SEQUENCEFILE，不允许用户填写行分隔符和列分隔符
-//        if (fileFormat == FileFormat.ORC
-//            || fileFormat == FileFormat.PARQUET
-//            || fileFormat == FileFormat.SEQUENCEFILE) {
-//          if (StringUtil.isNotEmpty(def.getRow_separator())) {
-//            throw new BusinessException("数据抽取并入库，保存格式为ORC/PARQUET/SEQUENCEFILE，" + "不能指定行分隔符");
-//          }
-//          if (StringUtil.isNotEmpty(def.getDatabase_separatorr())) {
-//            throw new BusinessException("数据抽取并入库，保存格式为ORC/PARQUET/SEQUENCEFILE，" + "不能指定列分隔符");
-//          }
-//        }
-//        // 3-2、如果是非定长，则校验，用户必须填写行分隔符和列分隔符
-//        if (fileFormat == FileFormat.FeiDingChang) {
-//          if (StringUtil.isEmpty(def.getRow_separator())) {
-//            throw new BusinessException("数据抽取并入库，保存格式为非定长，请指定行分隔符");
-//          }
-//          if (StringUtil.isEmpty(def.getDatabase_separatorr())) {
-//            throw new BusinessException("数据抽取并入库，保存格式为非定长，请指定列分隔符");
-//          }
-//        }
-//        // 3-3、如果是CSV，则不进行校验，即如果用户不填写，就卸成标准CSV，否则，按照用户指定的列分隔符写文件
-//      }
+      //      if (extractType == DataExtractType.ShuJuChouQuJiRuKu) {
+      //        // 如果数据抽取方式是抽取及入库，那么校验存储方式不能是定长
+      //        if (fileFormat == FileFormat.DingChang) {
+      //          throw new BusinessException("抽取并入库操作，不能保存为定长文件");
+      //        }
+      //        // 3-1、如果是ORC/PARQUET/SEQUENCEFILE，不允许用户填写行分隔符和列分隔符
+      //        if (fileFormat == FileFormat.ORC
+      //            || fileFormat == FileFormat.PARQUET
+      //            || fileFormat == FileFormat.SEQUENCEFILE) {
+      //          if (StringUtil.isNotEmpty(def.getRow_separator())) {
+      //            throw new BusinessException("数据抽取并入库，保存格式为ORC/PARQUET/SEQUENCEFILE，" +
+      // "不能指定行分隔符");
+      //          }
+      //          if (StringUtil.isNotEmpty(def.getDatabase_separatorr())) {
+      //            throw new BusinessException("数据抽取并入库，保存格式为ORC/PARQUET/SEQUENCEFILE，" +
+      // "不能指定列分隔符");
+      //          }
+      //        }
+      //        // 3-2、如果是非定长，则校验，用户必须填写行分隔符和列分隔符
+      //        if (fileFormat == FileFormat.FeiDingChang) {
+      //          if (StringUtil.isEmpty(def.getRow_separator())) {
+      //            throw new BusinessException("数据抽取并入库，保存格式为非定长，请指定行分隔符");
+      //          }
+      //          if (StringUtil.isEmpty(def.getDatabase_separatorr())) {
+      //            throw new BusinessException("数据抽取并入库，保存格式为非定长，请指定列分隔符");
+      //          }
+      //        }
+      //        // 3-3、如果是CSV，则不进行校验，即如果用户不填写，就卸成标准CSV，否则，按照用户指定的列分隔符写文件
+      //      }
     }
   }
 }
