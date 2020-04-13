7667d39a4a9e555df800a95420d1fe362cbbfcb1
Qiang-Lee <hliqiang1989@gmail.com>
2020-04-13 15:27:01 +0800

    新增数据文件采集

---
+++ hrds_B/src/main/java/hrds/b/biz/agent/datafileconf/fileconf/CollectFIleAction.java
@@ -0,0 +1,119 @@
+package hrds.b.biz.agent.datafileconf.fileconf;
+
+import fd.ng.core.annotation.DocClass;
+import fd.ng.core.annotation.Method;
+import fd.ng.core.annotation.Param;
+import fd.ng.core.annotation.Return;
+import fd.ng.core.utils.StringUtil;
+import fd.ng.web.util.Dbo;
+import hrds.b.biz.agent.datafileconf.CheckParam;
+import hrds.commons.base.BaseAction;
+import hrds.commons.codes.AgentType;
+import hrds.commons.codes.IsFlag;
+import hrds.commons.entity.Agent_info;
+import hrds.commons.entity.Collect_job_classify;
+import hrds.commons.entity.Database_set;
+import hrds.commons.exception.BusinessException;
+import hrds.commons.utils.key.PrimayKeyGener;
+import java.util.Map;
+
+@DocClass(desc = "数据文件采集配置管理", author = "Mr.Lee", createdate = "2020-04-10 16:08")
+public class CollectFIleAction extends BaseAction {
+
+  @Method(desc = "编辑数据文件采集", logicStep = "1: 检查当前任务是否存在 2: 返回编辑的数据信息")
+  @Param(name = "colSetId", desc = "数据采集任务ID(database_id)", range = "不可为空")
+  @Return(desc = "返回编辑时需要的数据文件采集信息", range = "不可为空")
+  public Map<String, Object> getInitDataFileData(long colSetId) {
+    //    1: 检查当前任务是否存在
+    long countNum =
+        Dbo.queryNumber(
+                "SELECT COUNT(1) FROM  " + Database_set.TableName + " WHERE database_id = ?",
+                colSetId)
+            .orElseThrow(() -> new BusinessException("SQL查询错误"));
+
+    if (countNum == 0) {
+      CheckParam.throwErrorMsg("任务( %s )不存在!!!", colSetId);
+    }
+
+    //    2: 返回编辑的数据信息
+    return Dbo.queryOneObject(
+        "SELECT t1.*,t2.classify_num,t2.classify_name FROM "
+            + Database_set.TableName
+            + " t1 JOIN "
+            + Collect_job_classify.TableName
+            + " t2 ON t1.classify_id = t2.classify_id WHERE t2.user_id = ? AND t1.is_sendok = ? AND t1.database_id = ?",
+        getUserId(),
+        IsFlag.Shi.getCode(),
+        colSetId);
+  }
+
+  @Method(desc = "新增数据文件采集", logicStep = "根据用户信息查询数据文件上次为配置完成的数据信息")
+  @Return(desc = "返回为配置的数据采集信息", range = "可以为空,为空表示不存在上次为配置完成的数据信息")
+  public Map<String, Object> addDataFileData(long source_id) {
+    return Dbo.queryOneObject(
+        "SELECT t1.*,t2.classify_num,t2.classify_name FROM "
+            + Database_set.TableName
+            + " t1 JOIN "
+            + Collect_job_classify.TableName
+            + " t2 ON t1.classify_id = t2.classify_id join "
+            + Agent_info.TableName
+            + " ai on t1.agent_id = ai.agent_id "
+            + " where t1.is_sendok = ? AND ai.agent_type = ? AND ai.user_id = ? AND ai.source_id = ?",
+        IsFlag.Fou.getCode(),
+        AgentType.DBWenJian.getCode(),
+        getUserId(),
+        source_id);
+  }
+
+  @Method(desc = "保存数据文件的配置信息", logicStep = "1: 检查数据编号是否存在" + "2: 设置此次任务的ID" + "3: 返回生成的采集任务ID给前端")
+  @Param(name = "database_set", desc = "数据文件的配置实体", range = "不可为空", isBean = true)
+  @Return(desc = "返回此次任务的ID", range = "不会为空")
+  public String saveDataFile(Database_set database_set) {
+    CheckParam.checkData("采集任务编号不能为空", database_set.getDatabase_number());
+    CheckParam.checkData("采集数据文件路径不能为空", database_set.getPlane_url());
+    CheckParam.checkData("分类编号不能为空", String.valueOf(database_set.getClassify_id()));
+    //    1: 检查数据编号是否存在
+    long countNum =
+        Dbo.queryNumber(
+                "SELECT COUNT(1) FROM " + Database_set.TableName + " WHERE database_number = ? ",
+                database_set.getDatabase_number())
+            .orElseThrow(() -> new BusinessException("SQL查询异常"));
+    if (countNum != 0) {
+      CheckParam.throwErrorMsg("当前任务编号( %s ),已经存在", database_set.getDatabase_number());
+    }
+    //    2: 设置此次任务的ID
+    String database_id = PrimayKeyGener.getNextId();
+    database_set.setDatabase_id(database_id);
+    database_set.add(Dbo.db());
+    //    3: 返回生成的采集任务ID给前端
+    return database_id;
+  }
+
+  @Method(
+      desc = "更新数据文件的配置信息",
+      logicStep = "1: 检查数据文件采集信息是否存在" + "2: 更新数据文件采集信息" + "3: 返回生成的采集任务ID给前端")
+  @Param(name = "database_set", desc = "数据文件的配置实体数据信息", range = "不可为空", isBean = true)
+  @Return(desc = "返回此次任务的ID", range = "不会为空")
+  public String updateDataFile(Database_set database_set) {
+    CheckParam.checkData("任务ID不能为空", database_set.getDatabase_id().toString());
+    CheckParam.checkData("任务编号不能为空", database_set.getDatabase_number());
+    CheckParam.checkData("采集数据文件路径不能为空", database_set.getPlane_url());
+    CheckParam.checkData("分类编号不能为空", String.valueOf(database_set.getClassify_id()));
+    //    1: 检查数据文件采集信息是否存在
+    long countNum =
+        Dbo.queryNumber(
+                "SELECT COUNT(1) FROM "
+                    + Database_set.TableName
+                    + " WHERE database_number = ? AND database_id = ?",
+                database_set.getDatabase_number(),
+                database_set.getDatabase_id())
+            .orElseThrow(() -> new BusinessException("SQL查询异常"));
+    if (countNum == 1) {
+      CheckParam.throwErrorMsg("当前任务编号( %s ),已经不存在", database_set.getDatabase_number());
+    }
+    //    2: 更新数据文件采集信息
+    database_set.update(Dbo.db());
+    //    3: 返回生成的采集任务ID给前端
+    return database_set.getDatabase_id().toString();
+  }
+}
---
+++ hrds_B/src/main/java/hrds/b/biz/agent/datafileconf/tableconf/DictionaryTableAction.java
@@ -0,0 +1,84 @@
+package hrds.b.biz.agent.datafileconf.tableconf;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import fd.ng.core.annotation.DocClass;
+import fd.ng.core.annotation.Method;
+import fd.ng.core.annotation.Param;
+import fd.ng.core.annotation.Return;
+import fd.ng.web.util.Dbo;
+import hrds.b.biz.agent.datafileconf.CheckParam;
+import hrds.b.biz.agent.tools.SendMsgUtil;
+import hrds.commons.base.BaseAction;
+import hrds.commons.entity.Agent_info;
+import hrds.commons.entity.Database_set;
+import hrds.commons.entity.Table_info;
+import hrds.commons.exception.BusinessException;
+import hrds.commons.utils.AgentActionUtil;
+import java.util.List;
+import java.util.Map;
+
+@DocClass(desc = "数据文件采集数据字典的表配置", author = "Mr.Lee", createdate = "2020-04-13 14:41")
+public class DictionaryTableAction extends BaseAction {
+
+  public void dictionTableData(long colSetId) {
+    long countNum =
+        Dbo.queryNumber(
+                "SELECT count(1) FROM " + Database_set.TableName + " WHERE database_id = ?",
+                colSetId)
+            .orElseThrow(() -> new BusinessException("SQL查询异常"));
+    if (countNum == 0) {
+      CheckParam.throwErrorMsg("采集任务( %s ),不存在!!!", colSetId);
+    }
+    // 获取数据库上次已存在的表
+    List<Map<String, Object>> list =
+        Dbo.queryList(
+            Table_info.TableName,
+            "SELECT * FROM " + Table_info.TableName + " WHERE database_id = ?",
+            colSetId);
+    // 获取数据字典目录下的数据表
+    // 1、根据colSetId去数据库中获取数据库设置相关信息
+    Map<String, Object> databaseInfo = getDatabaseSetInfo(colSetId);
+    if (databaseInfo.size() > 0) {
+      throw new BusinessException("未找到数据库采集任务");
+    }
+    // 2、和Agent端进行交互，得到Agent返回的数据
+    long agentId = (long) databaseInfo.get("agent_id");
+    String respMsg =
+        SendMsgUtil.getAllTableName(
+            agentId, getUserId(), databaseInfo, AgentActionUtil.GETDATABASETABLE);
+    // 3、对获取到的数据进行处理，根据表名和colSetId获取界面需要显示的信息并返回
+    List<String> tableNames = JSON.parseObject(respMsg, new TypeReference<List<String>>() {});
+    // 找出已在数据字典中存在表
+
+  }
+
+  @Method(desc = "根据colSetId去数据库中查出DB连接信息", logicStep = "1、根据colSetId和userId去数据库中查出DB连接信息")
+  @Param(name = "colSetId", desc = "数据库设置ID，源系统数据库设置表主键，数据库对应表外键", range = "不为空")
+  @Param(name = "userId", desc = "当前登录用户ID，sys_user表主键", range = "不为空")
+  @Return(desc = "查询结果集", range = "不为空")
+  private Map<String, Object> getDatabaseSetInfo(long colSetId) {
+
+    long databaseNum =
+        Dbo.queryNumber(
+                "SELECT COUNT(1) FROM " + Database_set.TableName + " WHERE database_id = ?",
+                colSetId)
+            .orElseThrow(() -> new BusinessException("SQL查询异常"));
+    if (databaseNum == 0) {
+      throw new BusinessException("任务(" + colSetId + ")不存在!!!");
+    }
+    // 1、根据colSetId和userId去数据库中查出DB连接信息
+    return Dbo.queryOneObject(
+        " select t1.database_type, t1.database_ip, t1.database_port, t1.database_name, "
+            + " t1.database_pad, t1.user_name, t1.database_drive, t1.jdbc_url, t1.agent_id, t1.db_agent, t1.plane_url"
+            + " from "
+            + Database_set.TableName
+            + " t1"
+            + " left join "
+            + Agent_info.TableName
+            + " ai on ai.agent_id = t1.agent_id"
+            + " where t1.database_id = ? and ai.user_id = ? ",
+        colSetId,
+        getUserId());
+  }
+}
