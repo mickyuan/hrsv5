6cdb25b383f9dc1958483bd8d33b56ba475d08fd
Qiang-Lee <hliqiang1989@gmail.com>
2020-04-16 14:57:56 +0800

    修改数据文件采集问题

--- hrds_B/src/main/java/hrds/b/biz/agent/datafileconf/fileconf/CollectFIleAction.java
+++ hrds_B/src/main/java/hrds/b/biz/agent/datafileconf/fileconf/CollectFIleAction.java
@@ -86,6 +86,7 @@ public class CollectFIleAction extends BaseAction {
     // 设置是否为DB文件为是
     database_set.setDb_agent(IsFlag.Shi.getCode());
     database_set.setDatabase_id(database_id);
+    database_set.setIs_sendok(IsFlag.Fou.getCode());
     database_set.add(Dbo.db());
     //    3: 返回生成的采集任务ID给前端
     return database_id;
@@ -104,16 +105,14 @@ public class CollectFIleAction extends BaseAction {
     //    1: 检查数据文件采集信息是否存在
     long countNum =
         Dbo.queryNumber(
-                "SELECT COUNT(1) FROM "
-                    + Database_set.TableName
-                    + " WHERE database_number = ? AND database_id = ?",
-                database_set.getDatabase_number(),
+                "SELECT COUNT(1) FROM " + Database_set.TableName + " WHERE  database_id = ?",
                 database_set.getDatabase_id())
             .orElseThrow(() -> new BusinessException("SQL查询异常"));
-    if (countNum == 1) {
+    if (countNum == 0) {
       CheckParam.throwErrorMsg("当前任务编号( %s ),已经不存在", database_set.getDatabase_number());
     }
     //    2: 更新数据文件采集信息
+    database_set.setIs_sendok(IsFlag.Fou.getCode());
     database_set.update(Dbo.db());
     //    3: 返回生成的采集任务ID给前端
     return database_set.getDatabase_id().toString();
