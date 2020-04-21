d0a1fa90db2b425f594bdce6ac7794368f310739
BY-HLL <78733419@qq.com>
2020-04-21 14:32:56 +0800

    提交数据管控-规则详情配置(已知问题修改)

--- hrds_K/src/main/java/hrds/k/biz/dm/ruleconfig/commons/DqcExecution.java
+++ hrds_K/src/main/java/hrds/k/biz/dm/ruleconfig/commons/DqcExecution.java
@@ -30,8 +30,8 @@ public class DqcExecution {
     @Param(name = "beans", desc = "List<SysVarCheckBean>", range = "自定义bean类型")
     @Param(name = "exec_method", desc = "执行方式（DqcExecMode代码项）", range = "DqcExecMode代码项")
     @Return(desc = "执行任务id", range = "执行任务id")
-    public static long executionRule(Dq_definition dq_definition, String verify_date,
-                                     Set<SysVarCheckBean> beans, String exec_method) {
+    public static long executionRule(Dq_definition dq_definition, String verify_date, Set<SysVarCheckBean> beans,
+                                     String exec_method) {
         //数据校验
         if (StringUtil.isBlank(dq_definition.getReg_num().toString())) {
             throw new BusinessException("执行规则时,规则编号为空!");
@@ -46,118 +46,119 @@ public class DqcExecution {
         //初始化检测记录日志信息
         Dq_result dq_result = new Dq_result();
         //初始化操作db
-        DatabaseWrapper db = new DatabaseWrapper();
-        try {
-            //获取定义的数据质量规则级别
-            dq_rule_level = dq_definition.getFlags();
-            System.out.println(dq_definition.toString());
-            //将原始通用属性从原始bean中复制到目标bean中
-            BeanUtils.copyProperties(dq_definition, dq_result);
-            dq_result.setTask_id(PrimayKeyGener.getNextId());
-            dq_result.setVerify_date(verify_date);
-            dq_result.setStart_date(DateUtil.getSysDate());
-            dq_result.setStart_time(DateUtil.getSysTime());
-            dq_result.setExec_mode(exec_method);
-            dq_result.setVerify_result(DqcVerifyResult.ZhengChang.getCode());
-            dq_result.setVerify_sql(dq_definition.getSpecify_sql());
-            dq_result.setErr_dtl_sql(dq_definition.getErr_data_sql());
-            dq_result.setDl_stat(DqcDlStat.ZhengChang.getCode());
-            //执行开始时间
-            long execution_start_time = System.currentTimeMillis();
-            //获取指定的验证sql
-            String[] verify_sql = dq_result.getVerify_sql().split(";");
-            //不在范围内的记录数
-            String index1_sql;
-            //检查总记录数
-            String index2_sql = "";
-            if (verify_sql.length == 1) {
-                index1_sql = verify_sql[0].trim();
-            } else if (verify_sql.length == 2) {
-                index1_sql = verify_sql[0].trim();
-                index2_sql = verify_sql[1].trim();
-            } else {
-                throw new BusinessException("请填写检查sql!");
-            }
-            //问题明细sql
-            String index3_sql = dq_result.getErr_dtl_sql().trim();
-            //替换系统变量
-            for (SysVarCheckBean bean : beans) {
-                index1_sql = index1_sql.replace(bean.getName(), bean.getValue());
-                index2_sql = index2_sql.replace(bean.getName(), bean.getValue());
-                index3_sql = index3_sql.replace(bean.getName(), bean.getValue());
-            }
-            //运行sql:不在范围内的记录数
-            Map<String, Object> map_result_1 = new HashMap<>();
-            if (StringUtil.isNotBlank(index1_sql) && IsFlag.Shi.getCode().equals(dq_definition.getIs_saveindex1())) {
-                new ProcessingData() {
+        try (DatabaseWrapper db = new DatabaseWrapper()) {
+            //设置检查结果
+            try {
+                //获取定义的数据质量规则级别
+                dq_rule_level = dq_definition.getFlags();
+                System.out.println(dq_definition.toString());
+                //将原始通用属性从原始bean中复制到目标bean中
+                BeanUtils.copyProperties(dq_definition, dq_result);
+                dq_result.setTask_id(PrimayKeyGener.getNextId());
+                dq_result.setVerify_date(verify_date);
+                dq_result.setStart_date(DateUtil.getSysDate());
+                dq_result.setStart_time(DateUtil.getSysTime());
+                dq_result.setExec_mode(exec_method);
+                dq_result.setVerify_result(DqcVerifyResult.ZhengChang.getCode());
+                dq_result.setVerify_sql(dq_definition.getSpecify_sql());
+                dq_result.setErr_dtl_sql(dq_definition.getErr_data_sql());
+                dq_result.setDl_stat(DqcDlStat.ZhengChang.getCode());
+                //执行开始时间
+                long execution_start_time = System.currentTimeMillis();
+                //获取指定的验证sql
+                String[] verify_sql = dq_result.getVerify_sql().split(";");
+                //不在范围内的记录数
+                String index1_sql;
+                //检查总记录数
+                String index2_sql = "";
+                if (verify_sql.length == 1) {
+                    index1_sql = verify_sql[0].trim();
+                } else if (verify_sql.length == 2) {
+                    index1_sql = verify_sql[0].trim();
+                    index2_sql = verify_sql[1].trim();
+                } else {
+                    throw new BusinessException("请填写检查sql!");
+                }
+                //问题明细sql
+                String index3_sql = dq_result.getErr_dtl_sql().trim();
+                //替换系统变量
+                for (SysVarCheckBean bean : beans) {
+                    index1_sql = index1_sql.replace(bean.getName(), bean.getValue());
+                    index2_sql = index2_sql.replace(bean.getName(), bean.getValue());
+                    index3_sql = index3_sql.replace(bean.getName(), bean.getValue());
+                }
+                //运行sql:不在范围内的记录数
+                Map<String, Object> map_result_1 = new HashMap<>();
+                if (StringUtil.isNotBlank(index1_sql) && IsFlag.Shi.getCode().equals(dq_definition.getIs_saveindex1())) {
+                    new ProcessingData() {
 
-                    @Override
-                    public void dealLine(Map<String, Object> map) {
-                        map_result_1.putAll(map);
-                    }
-                }.getDataLayer(index1_sql, db);
-            }
-            //运行sql:检查总记录数
-            Map<String, Object> map_result_2 = new HashMap<>();
-            if (StringUtil.isNotBlank(index2_sql) && IsFlag.Shi.getCode().equals(dq_definition.getIs_saveindex2())) {
-                new ProcessingData() {
+                        @Override
+                        public void dealLine(Map<String, Object> map) {
+                            map_result_1.putAll(map);
+                        }
+                    }.getDataLayer(index1_sql, db);
+                }
+                //运行sql:检查总记录数
+                Map<String, Object> map_result_2 = new HashMap<>();
+                if (StringUtil.isNotBlank(index2_sql) && IsFlag.Shi.getCode().equals(dq_definition.getIs_saveindex2())) {
+                    new ProcessingData() {
 
-                    @Override
-                    public void dealLine(Map<String, Object> map) {
-                        map_result_2.putAll(map);
-                    }
-                }.getDataLayer(index2_sql, db);
-            }
-            //运行sql:问题明细sql
-            List<Map<String, Object>> map_result_3 = new ArrayList<>();
-            if (StringUtil.isNotBlank(index3_sql) && IsFlag.Shi.getCode().equals(dq_definition.getIs_saveindex3())) {
-                new ProcessingData() {
+                        @Override
+                        public void dealLine(Map<String, Object> map) {
+                            map_result_2.putAll(map);
+                        }
+                    }.getDataLayer(index2_sql, db);
+                }
+                //运行sql:问题明细sql
+                List<Map<String, Object>> map_result_3 = new ArrayList<>();
+                if (StringUtil.isNotBlank(index3_sql) && IsFlag.Shi.getCode().equals(dq_definition.getIs_saveindex3())) {
+                    new ProcessingData() {
 
-                    @Override
-                    public void dealLine(Map<String, Object> map) {
-                        map_result_3.add(map);
-                    }
-                }.getDataLayer(index3_sql, db);
+                        @Override
+                        public void dealLine(Map<String, Object> map) {
+                            map_result_3.add(map);
+                        }
+                    }.getDataLayer(index3_sql, db);
+                }
+                //设置运行结束日期,时间
+                dq_result.setEnd_date(DateUtil.getSysDate());
+                dq_result.setEnd_time(DateUtil.getSysTime());
+                //设置耗时
+                long execution_end_time = System.currentTimeMillis();
+                dq_result.setElapsed_ms((int) (execution_end_time - execution_start_time));
+                //处理sql运行结果
+                dq_result.setCheck_index1(map_result_1.get("index1").toString());
+                dq_result.setCheck_index2(map_result_2.get("index2").toString());
+                dq_result.setCheck_index3(map_result_3.toString());
+            } catch (Exception e) {
+                has_exception = Boolean.TRUE;
+                //规则级别为严重,且发生了异常
+                dq_result.setVerify_result(DqcVerifyResult.YiChang.getCode());
+                dq_result.setDl_stat(DqcDlStat.DengDaiChuLi.getCode());
+                dq_result.setErrno(e.getMessage());
+                //如果规则级别为'严重'，则规则结果为'执行失败'
+                if (EdRuleLevel.YanZhong.getCode().equals(dq_rule_level)) {
+                    dq_result.setVerify_result(DqcVerifyResult.ZhiXingShiBai.getCode());
+                }
             }
-            //设置运行结束日期,时间
-            dq_result.setEnd_date(DateUtil.getSysDate());
-            dq_result.setEnd_time(DateUtil.getSysTime());
-            //设置耗时
-            long execution_end_time = System.currentTimeMillis();
-            dq_result.setElapsed_ms((int) (execution_end_time - execution_start_time));
-            //处理sql运行结果
-            dq_result.setCheck_index1(map_result_1.get("index1").toString());
-            dq_result.setCheck_index2(map_result_2.get("index2").toString());
-            dq_result.setCheck_index3(map_result_3.toString());
-        } catch (Exception e) {
-            has_exception = Boolean.TRUE;
-            //规则级别为严重,且发生了异常
-            dq_result.setVerify_result(DqcVerifyResult.YiChang.getCode());
-            dq_result.setDl_stat(DqcDlStat.DengDaiChuLi.getCode());
-            dq_result.setErrno(e.getMessage());
-            //如果规则级别为'严重'，则规则结果为'执行失败'
-            if (EdRuleLevel.YanZhong.getCode().equals(dq_rule_level)) {
-                dq_result.setVerify_result(DqcVerifyResult.ZhiXingShiBai.getCode());
-            }
-        } finally {
             //保存检查结果
             dq_result.add(db);
             db.commit();
-        }
-        //如果规则级别是严重且发生了异常，则记录异常信息并异常退出
-        if (EdRuleLevel.YanZhong.getCode().equals(dq_rule_level) && has_exception) {
-            //如果是手工执行抛异常
-            if (DqcExecMode.ShouGong.getCode().equals(exec_method)) {
-                throw new BusinessException("规则级别为严重，且发生了异常");
-            } else {
-                //作业调度执行,规则级别为严重，且发生了异常，退出
-                System.exit(-1);
+            //如果规则级别是严重且发生了异常，则在记录异常信息后异常退出
+            if (EdRuleLevel.YanZhong.getCode().equals(dq_rule_level) && has_exception) {
+                //如果是手工执行抛异常
+                if (DqcExecMode.ShouGong.getCode().equals(exec_method)) {
+                    throw new BusinessException("规则级别为严重，且发生了异常");
+                } else {
+                    //作业调度执行,规则级别为严重，且发生了异常，退出
+                    System.exit(-1);
+                }
             }
-        }
-        //执行到此处则代表程序正常运行结束，开始记录指标3的数据
-        if (IsFlag.Shi.getCode().equals(dq_result.getIs_saveindex3())
-                && StringUtil.isNotBlank(dq_result.getErr_dtl_sql())) {
-            recordIndicator3Data(dq_result, beans);
+            //执行到此处则代表程序正常运行结束，开始记录指标3的数据
+            if (IsFlag.Shi.getCode().equals(dq_result.getIs_saveindex3())
+                    && StringUtil.isNotBlank(dq_result.getErr_dtl_sql())) {
+                recordIndicator3Data(dq_result, beans);
+            }
         }
         return dq_result.getTask_id();
     }
@@ -199,7 +200,7 @@ public class DqcExecution {
     @Param(name = "db", desc = "DatabaseWrapper连接信息", range = "DatabaseWrapper类型")
     @Param(name = "dq_definition", desc = "Dq_definition实体", range = "Dq_definition实体")
     @Return(desc = "有效依赖作业List", range = "有效依赖作业List")
-    public static List<Map<String, Object>> getEffDepJobs(DatabaseWrapper db, Dq_definition dq_definition) {
+    public static List<Map<String, Object>> getEffDepJobs(Dq_definition dq_definition) {
         if (StringUtil.isBlank(dq_definition.getReg_num().toString())) {
             throw new BusinessException("规则编号为空!");
         }
@@ -217,7 +218,7 @@ public class DqcExecution {
         asmSql.addSql("SELECT etl_sys_cd,etl_job,job_eff_flag,job_disp_status FROM " + Etl_job_disp_his.TableName +
                 " WHERE job_eff_flag = ?").addParam(Job_Effective_Flag.YES.getCode());
         asmSql.addLikeParam("etl_job", dq_definition.getReg_num().toString());
-        return Dbo.queryList(db, asmSql.sql(), asmSql.params());
+        return Dbo.queryList(asmSql.sql(), asmSql.params());
     }
 
 
@@ -249,9 +250,8 @@ public class DqcExecution {
         if (StringUtil.isBlank(dq_result.getTask_id().toString())) {
             throw new BusinessException("在记录指标3的数据时，传入的任务编号为空!");
         }
-        //初始化操作db
-        DatabaseWrapper db = new DatabaseWrapper();
-        try {
+        //保存指标3的数据到对应数据表的存储层下
+        try (DatabaseWrapper db = new DatabaseWrapper()) {
             //指标3的sql
             String sql = dq_result.getErr_dtl_sql();
             //替换系统变量
