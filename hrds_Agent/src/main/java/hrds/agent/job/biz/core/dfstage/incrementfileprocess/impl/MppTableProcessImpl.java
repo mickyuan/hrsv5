0da316a918729d26c0325ba98e98d547660d0818
xiaozhongzhu <53812434+xiaozhongzhu@users.noreply.github.com>
2020-05-15 10:20:20 +0800

    修改增量情况下更新表添加事务

--- hrds_Agent/src/main/java/hrds/agent/job/biz/core/dfstage/incrementfileprocess/impl/MppTableProcessImpl.java
+++ hrds_Agent/src/main/java/hrds/agent/job/biz/core/dfstage/incrementfileprocess/impl/MppTableProcessImpl.java
@@ -46,6 +46,7 @@ public class MppTableProcessImpl extends TableProcessAbstract {
 		this.updateSql = getBatchUpdateSql();
 		//数据库的连接
 		this.db = ConnectionTool.getDBWrapper(dataStoreConfBean.getData_store_connect_attr());
+		this.db.beginTrans();
 		//获取需要更新的数据
 		this.setColumnList = getSetColumnList();
 		//获取需要跟新的数据的判断条件
@@ -59,88 +60,100 @@ public class MppTableProcessImpl extends TableProcessAbstract {
 
 	@Override
 	public void dealData(Map<String, Map<String, Object>> valueList) {
-		for (String operate : valueList.keySet()) {
-			if ("insert".equals(operate)) {
-				Object[] object = new Object[insertColumnList.size()];
-				for (int i = 0; i < insertColumnList.size(); i++) {
-					//加类型转换
-					object[i] = valueList.get(operate).get(insertColumnList.get(i));
+		try {
+			for (String operate : valueList.keySet()) {
+				if ("insert".equals(operate)) {
+					Object[] object = new Object[insertColumnList.size()];
+					for (int i = 0; i < insertColumnList.size(); i++) {
+						//加类型转换
+						object[i] = valueList.get(operate).get(insertColumnList.get(i));
+					}
+					addParamsPool.add(object);
+				} else if ("update".equals(operate)) {
+					Object[] object = new Object[updateColumnList.size()];
+					for (int i = 0; i < setColumnList.size(); i++) {
+						//加类型转换
+						object[i] = valueList.get(operate).get(setColumnList.get(i));
+					}
+					for (int i = 0; i < whereColumnList.size(); i++) {
+						//加类型转换
+						object[setColumnList.size() + i] = valueList.get(operate).get(whereColumnList.get(i));
+					}
+					updateParamsPool.add(object);
+				} else if ("delete".equals(operate)) {
+					deleteNum++;
+					deleteSql.append("(");
+					for (String column : deleteColumnList) {
+						deleteSql.append(getDeleteValue(valueList.get(operate).get(column))).append(",");
+					}
+					deleteSql.delete(deleteSql.length() - 1, deleteSql.length());
+					deleteSql.append(")").append(",");
+				} else {
+					throw new AppSystemException("增量数据采集不自持" + operate + "操作");
 				}
-				addParamsPool.add(object);
-			} else if ("update".equals(operate)) {
-				Object[] object = new Object[updateColumnList.size()];
-				for (int i = 0; i < setColumnList.size(); i++) {
-					//加类型转换
-					object[i] = valueList.get(operate).get(setColumnList.get(i));
-				}
-				for (int i = 0; i < whereColumnList.size(); i++) {
-					//加类型转换
-					object[setColumnList.size() + i] = valueList.get(operate).get(whereColumnList.get(i));
-				}
-				updateParamsPool.add(object);
-			} else if ("delete".equals(operate)) {
-				deleteNum++;
-				deleteSql.append("(");
-				for (String column : deleteColumnList) {
-					deleteSql.append(getDeleteValue(valueList.get(operate).get(column))).append(",");
-				}
-				deleteSql.delete(deleteSql.length() - 1, deleteSql.length());
-				deleteSql.append(")").append(",");
-			} else {
-				throw new AppSystemException("增量数据采集不自持" + operate + "操作");
 			}
-		}
-		//先执行删除，再执行更新, 再执行新增
-		if (deleteNum % 900 == 0) {
-			deleteSql.delete(deleteSql.length() - 1, deleteSql.length()).append(")");
-			//每900条删除一次
-			db.execute(deleteSql.toString());
-			deleteSql.delete(0, deleteSql.length());
-			deleteSql.append("DELETE FROM ").append(collectTableBean.getHbase_name()).append(" WHERE ").append("(");
-			for (String column : deleteColumnList) {
-				deleteSql.append(column).append(",");
-			}
-			deleteSql.delete(deleteSql.length() - 1, deleteSql.length()).append(") IN (");
-			deleteNum = 0;
-		}
-		if (updateParamsPool.size() != 0 && updateParamsPool.size() % 5000 == 0) {
-			//如果更新的有数据，说明delete的值全部读完，这时候判断如果deleteNum不等于0则立即执行剩余的删除操作
-			if (deleteNum > 0) {
+			//先执行删除，再执行更新, 再执行新增
+			if (deleteNum % 900 == 0) {
 				deleteSql.delete(deleteSql.length() - 1, deleteSql.length()).append(")");
 				//每900条删除一次
 				db.execute(deleteSql.toString());
+				deleteSql.delete(0, deleteSql.length());
+				deleteSql.append("DELETE FROM ").append(collectTableBean.getHbase_name()).append(" WHERE ").append("(");
+				for (String column : deleteColumnList) {
+					deleteSql.append(column).append(",");
+				}
+				deleteSql.delete(deleteSql.length() - 1, deleteSql.length()).append(") IN (");
 				deleteNum = 0;
 			}
-			//每5000条batch提交一次
-			db.execBatch(updateSql, updateParamsPool);
-			updateParamsPool.clear();
-		}
-		if (addParamsPool.size() != 0 && addParamsPool.size() % 5000 == 0) {
-			//如果新增的有数据,说明update的值全部读完，判断如果updateParamsPool.size()大于0则立即执行剩余的更新操作
-			if (updateParamsPool.size() > 0) {
+			if (updateParamsPool.size() != 0 && updateParamsPool.size() % 5000 == 0) {
+				//如果更新的有数据，说明delete的值全部读完，这时候判断如果deleteNum不等于0则立即执行剩余的删除操作
+				if (deleteNum > 0) {
+					deleteSql.delete(deleteSql.length() - 1, deleteSql.length()).append(")");
+					//每900条删除一次
+					db.execute(deleteSql.toString());
+					deleteNum = 0;
+				}
+				//每5000条batch提交一次
 				db.execBatch(updateSql, updateParamsPool);
 				updateParamsPool.clear();
 			}
-			//每5000条batch提交一次
-			db.execBatch(insertSql, addParamsPool);
-			addParamsPool.clear();
+			if (addParamsPool.size() != 0 && addParamsPool.size() % 5000 == 0) {
+				//如果新增的有数据,说明update的值全部读完，判断如果updateParamsPool.size()大于0则立即执行剩余的更新操作
+				if (updateParamsPool.size() > 0) {
+					db.execBatch(updateSql, updateParamsPool);
+					updateParamsPool.clear();
+				}
+				//每5000条batch提交一次
+				db.execBatch(insertSql, addParamsPool);
+				addParamsPool.clear();
+			}
+		} catch (Exception e) {
+			if (db != null)
+				db.rollback();
+			throw new AppSystemException("Mpp数据库增量模式直接更新库失败", e);
 		}
 	}
 
 	@Override
 	public void excute() {
 		//最后执行一次提交,如果删除的没有过900，更新和新增的都没有过5000则第一次执行就到这里
-		if (deleteNum > 0) {
-			deleteSql.delete(deleteSql.length() - 1, deleteSql.length()).append(")");
-			//每900条删除一次
-			db.execute(deleteSql.toString());
-			deleteNum = 0;
-		}
-		if (updateParamsPool.size() > 0) {
-			db.execBatch(updateSql, updateParamsPool);
-		}
-		if (addParamsPool.size() > 0) {
-			db.execBatch(insertSql, addParamsPool);
+		try {
+			if (deleteNum > 0) {
+				deleteSql.delete(deleteSql.length() - 1, deleteSql.length()).append(")");
+				//每900条删除一次
+				db.execute(deleteSql.toString());
+				deleteNum = 0;
+			}
+			if (updateParamsPool.size() > 0) {
+				db.execBatch(updateSql, updateParamsPool);
+			}
+			if (addParamsPool.size() > 0) {
+				db.execBatch(insertSql, addParamsPool);
+			}
+		} catch (Exception e) {
+			if (db != null)
+				db.rollback();
+			throw new AppSystemException("Mpp数据库增量模式直接更新库失败", e);
 		}
 	}
 
@@ -187,6 +200,7 @@ public class MppTableProcessImpl extends TableProcessAbstract {
 
 	@Override
 	public void close() {
+		db.commit();
 		db.close();
 	}
 
