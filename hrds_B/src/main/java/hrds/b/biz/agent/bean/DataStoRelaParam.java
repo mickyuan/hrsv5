8806f2931a5df18006ef770777675ea79ac04087
Qiang-Lee <hliqiang1989@gmail.com>
2020-05-18 14:39:33 +0800

    实体增加数据落地的字段 hyren_name

--- hrds_B/src/main/java/hrds/b/biz/agent/bean/DataStoRelaParam.java
+++ hrds_B/src/main/java/hrds/b/biz/agent/bean/DataStoRelaParam.java
@@ -10,32 +10,40 @@ import java.util.Arrays;
  * */
 public class DataStoRelaParam {
 
-	//表ID
-	private Long tableId;
-	//数据存储层配置表ID，由于一张抽取并入库的表可以选择多个存储目的地，所以这里用数组表示
-	private long[] dslIds;
+  // 表ID
+  private Long tableId;
+  // 数据存储层配置表ID，由于一张抽取并入库的表可以选择多个存储目的地，所以这里用数组表示
+  private long[] dslIds;
 
-	public Long getTableId() {
-		return tableId;
-	}
+  /** 最终的落地数据表名称*/
+  private String hyren_name;
 
-	public void setTableId(Long tableId) {
-		this.tableId = tableId;
-	}
+  public Long getTableId() {
+    return tableId;
+  }
 
-	public long[] getDslIds() {
-		return dslIds;
-	}
+  public void setTableId(Long tableId) {
+    this.tableId = tableId;
+  }
 
-	public void setDslIds(long[] dslIds) {
-		this.dslIds = dslIds;
-	}
+  public long[] getDslIds() {
+    return dslIds;
+  }
 
-	@Override
-	public String toString() {
-		return "DataStoRelaParam{" +
-				"tableId=" + tableId +
-				", dslIds=" + Arrays.toString(dslIds) +
-				'}';
-	}
+  public void setDslIds(long[] dslIds) {
+    this.dslIds = dslIds;
+  }
+
+  public void setHyren_name(String hyren_name) {
+    this.hyren_name = hyren_name;
+  }
+
+  public String getHyren_name() {
+    return hyren_name;
+  }
+
+  @Override
+  public String toString() {
+    return "DataStoRelaParam{" + "tableId=" + tableId + ", dslIds=" + Arrays.toString(dslIds) + '}';
+  }
 }
