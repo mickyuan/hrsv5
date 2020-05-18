eb0e712a473f238f312eb2469c9bc4a93a82b229
mickyuan <mickyuan@foxmail.com>
2020-05-18 10:22:49 +0800

    添加当作业主作业或后置作业出现异常时，执行回滚操作，回滚操作出现异常忽略

--- hrds_H/src/main/java/hrds/h/biz/service/BusinessForStorageType.java
+++ hrds_H/src/main/java/hrds/h/biz/service/BusinessForStorageType.java
@@ -24,37 +24,55 @@ public final class BusinessForStorageType extends AbstractBusiness {
         if (conf.isFirstLoad()) {
             logger.info("======================= 首次执行 =======================");
             logger.info("======================= 主作业执行 =======================");
-            loader.firstLoad();
-            logger.info("======================= 后置作业执行 =======================");
-            loader.finalWork();
+            try {
+                loader.firstLoad();
+                logger.info("======================= 后置作业执行 =======================");
+                loader.finalWork();
+            } catch (Exception e) {
+                try {
+                    logger.warn("作业执行失败，执行回滚操作。");
+                    loader.restore();
+                } catch (Exception ignored) {
+                }
+                throw e;
+            }
         } else {
             logger.info("======================= 非首次执行 =======================");
             logger.info("======================= 前置作业执行 =======================");
             logger.info("======================= 主作业执行 =======================");
             //该变量表示追加，替换还是增量
             String storageType = conf.getDmDatatable().getStorage_type();
-            if (StorageType.TiHuan.getCode().equals(storageType)) {
-                logger.info("======================= 替换 =======================");
-                loader.replace();
-            } else if (StorageType.ZhuiJia.getCode().equals(storageType)) {
-                logger.info("======================= 追加 =======================");
-                /**
-                 * 支持追加重跑，前提是该loader实现了{@link NonFirstLoad#restore()}
-                 */
-                restore(loaderName);
-                loader.append();
-            } else if (StorageType.ZengLiang.getCode().equals(storageType)) {
-                logger.info("======================= 增量 =======================");
-                /**
-                 * 支持增量重跑，前提是该loader实现了{@link NonFirstLoad#restore()}
-                 */
-                restore(loaderName);
-                loader.increment();
-            } else {
-                throw new AppSystemException("无效的进数方式: " + storageType);
+            try {
+                if (StorageType.TiHuan.getCode().equals(storageType)) {
+                    logger.info("======================= 替换 =======================");
+                    loader.replace();
+                } else if (StorageType.ZhuiJia.getCode().equals(storageType)) {
+                    logger.info("======================= 追加 =======================");
+                    /**
+                     * 支持追加重跑，前提是该loader实现了{@link NonFirstLoad#restore()}
+                     */
+                    restore(loaderName);
+                    loader.append();
+                } else if (StorageType.ZengLiang.getCode().equals(storageType)) {
+                    logger.info("======================= 增量 =======================");
+                    /**
+                     * 支持增量重跑，前提是该loader实现了{@link NonFirstLoad#restore()}
+                     */
+                    restore(loaderName);
+                    loader.increment();
+                } else {
+                    throw new AppSystemException("无效的进数方式: " + storageType);
+                }
+                logger.info("======================= 后置作业执行 =======================");
+                loader.finalWork();
+            } catch (Exception e) {
+                try {
+                    logger.warn("作业执行失败，执行回滚操作。");
+                    loader.restore();
+                } catch (Exception ignored) {
+                }
+                throw e;
             }
-            logger.info("======================= 后置作业执行 =======================");
-            loader.finalWork();
         }
 
     }
