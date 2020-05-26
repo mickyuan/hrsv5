0d0f1fa61f79edceada9f8b1b226b81df4336307
Qiang-Lee <hliqiang1989@gmail.com>
2020-05-26 16:44:43 +0800

    修改Agent类型转换和长度转换问题(contrast.conf)

--- hrds-commons/src/main/java/hrds/commons/utils/datastorage/dcl/LengthContrast.java
+++ hrds-commons/src/main/java/hrds/commons/utils/datastorage/dcl/LengthContrast.java
@@ -3,6 +3,7 @@ package hrds.commons.utils.datastorage.dcl;
 import fd.ng.core.annotation.DocClass;
 import fd.ng.db.jdbc.DatabaseWrapper;
 import fd.ng.db.jdbc.SqlOperator;
+import hrds.commons.entity.Data_store_layer;
 import hrds.commons.entity.Length_contrast;
 import hrds.commons.entity.Length_contrast_sum;
 import hrds.commons.utils.datastorage.yamldata.YamlDataFormat;
@@ -14,52 +15,68 @@ import java.util.Map;
 @DocClass(desc = "储层数据类型长度对照表", author = "Mr.Lee", createdate = "2020-01-13 15:50")
 public class LengthContrast implements YamlDataFormat {
 
-  /** prefix : 前缀名称 */
+  /**
+   * prefix : 前缀名称
+   */
   public static final String PREFIX = "lengthcontrast";
-  /** name : 类型对照名称 */
+  /**
+   * name : 类型对照名称
+   */
   public static final String NAME = "NAME";
 
   @Override
   public Map<String, List<Map<String, Object>>> yamlDataFormat() {
-    Map<String, Map<String, Object>> contrastMap = new LinkedHashMap<>();
-    getLengthContrast()
-        .forEach(
-            item -> {
-              String dtcs_name = ((String) item.get("dlcs_name")).toUpperCase();
-              String source_type = ((String) item.get("dlc_type")).toUpperCase();
-              Object target_type = item.get("dlc_length");
+	Map<String, Map<String, Object>> contrastMap = new LinkedHashMap<>();
+	List<Map<String, Object>> lengthContrast = getLengthContrast();
+	lengthContrast.addAll(getDefault());
+	lengthContrast.forEach(
+		item -> {
+		  String dtcs_name = ((String) item.get("dsl_name"));
+		  String source_type = ((String) item.get("dlc_type")).toUpperCase();
+		  Object target_type = item.get("dlc_length");
 
-              if (contrastMap.containsKey(dtcs_name)) {
-                contrastMap.get(dtcs_name).put(source_type, target_type);
-              } else {
-                Map<String, Object> map = new LinkedHashMap<>();
-                map.put(NAME, dtcs_name);
-                map.put(source_type, target_type);
-                contrastMap.put(dtcs_name, map);
-              }
-            });
+		  if (contrastMap.containsKey(dtcs_name)) {
+			contrastMap.get(dtcs_name).put(source_type, target_type);
+		  } else {
+			Map<String, Object> map = new LinkedHashMap<>();
+			map.put(NAME, dtcs_name);
+			map.put(source_type, target_type);
+			contrastMap.put(dtcs_name, map);
+		  }
+		});
 
-    List<Map<String, Object>> typeContrastAll = new ArrayList<>();
-    contrastMap.forEach(
-        (dtcs_name, typeContrast) -> {
-          typeContrastAll.add(typeContrast);
-        });
+	List<Map<String, Object>> typeContrastAll = new ArrayList<>();
+	contrastMap.forEach(
+		(dtcs_name, typeContrast) -> {
+		  typeContrastAll.add(typeContrast);
+		});
 
-    Map<String, List<Map<String, Object>>> map = new LinkedHashMap<>();
-    map.put(PREFIX, typeContrastAll);
+	Map<String, List<Map<String, Object>>> map = new LinkedHashMap<>();
+	map.put(PREFIX, typeContrastAll);
 
-    return map;
+	return map;
   }
 
   private List<Map<String, Object>> getLengthContrast() {
-    try (DatabaseWrapper db = new DatabaseWrapper()) {
-      return SqlOperator.queryList(
-          db,
-          "SELECT t2.dlcs_name,t1.dlc_type,t1.dlc_length FROM "
-              + Length_contrast.TableName
-              + " t1 LEFT JOIN "
-              + Length_contrast_sum.TableName
-              + " t2 ON t1.dlcs_id = t2.dlcs_id");
-    }
+	try (DatabaseWrapper db = new DatabaseWrapper()) {
+	  return SqlOperator.queryList(
+		  db,
+		  "select t1.dsl_name,t3.dlc_type,t3.dlc_length from " + Data_store_layer.TableName
+			  + " t1 left join " + Length_contrast_sum.TableName + " t2 on t1.dlcs_id = t2.dlcs_id left join "
+			  + Length_contrast.TableName + " t3 on t1.dlcs_id = t3.dlcs_id order by t1.dsl_name");
+	}
+  }
+
+  //TODO 默认的类型长度，基于卸数时不知道最终目的地的原因,页面需配置一个名称为DEFAULT的长度对照关系
+  private List<Map<String, Object>> getDefault() {
+	try (DatabaseWrapper db = new DatabaseWrapper()) {
+	  return SqlOperator.queryList(
+		  db,
+		  "SELECT t2.dlcs_name as dsl_name,t1.dlc_type,t1.dlc_length FROM "
+			  + Length_contrast.TableName
+			  + " t1 LEFT JOIN "
+			  + Length_contrast_sum.TableName
+			  + " t2 ON t1.dlcs_id = t2.dlcs_id where dlcs_name = 'DEFAULT'");
+	}
   }
 }
--- hrds-commons/src/main/java/hrds/commons/utils/datastorage/dcl/TypeContrast.java
+++ hrds-commons/src/main/java/hrds/commons/utils/datastorage/dcl/TypeContrast.java
@@ -3,6 +3,7 @@ package hrds.commons.utils.datastorage.dcl;
 import fd.ng.core.annotation.DocClass;
 import fd.ng.db.jdbc.DatabaseWrapper;
 import fd.ng.db.jdbc.SqlOperator;
+import hrds.commons.entity.Data_store_layer;
 import hrds.commons.entity.Type_contrast;
 import hrds.commons.entity.Type_contrast_sum;
 import hrds.commons.utils.datastorage.yamldata.YamlDataFormat;
@@ -14,54 +15,56 @@ import java.util.Map;
 @DocClass(desc = "存储层数据类型对照实体", author = "Mr.Lee", createdate = "2020-01-13 15:45")
 public class TypeContrast implements YamlDataFormat {
 
-  /** prefix : 前缀名称 */
+  /**
+   * prefix : 前缀名称
+   */
   public static final String PREFIX = "typecontrast";
-  /** name : 类型对照名称 */
+  /**
+   * name : 类型对照名称
+   */
   public static final String NAME = "NAME";
 
   @Override
   public Map<String, List<Map<String, Object>>> yamlDataFormat() {
 
-    Map<String, Map<String, Object>> contrastMap = new LinkedHashMap<>();
-    getTypeContrast()
-        .forEach(
-            item -> {
-              String dtcs_name = ((String) item.get("dtcs_name")).toUpperCase();
-              String source_type = ((String) item.get("source_type")).toUpperCase();
-              Object target_type = ((String) item.get("target_type")).toUpperCase();
+	Map<String, Map<String, Object>> contrastMap = new LinkedHashMap<>();
+	getTypeContrast()
+		.forEach(
+			item -> {
+			  String dtcs_name = ((String) item.get("dsl_name"));
+			  String source_type = ((String) item.get("source_type")).toUpperCase();
+			  Object target_type = ((String) item.get("target_type")).toUpperCase();
 
-              if (contrastMap.containsKey(dtcs_name)) {
-                contrastMap.get(dtcs_name).put(source_type, target_type);
-              } else {
-                Map<String, Object> map = new LinkedHashMap<>();
-                map.put(NAME, dtcs_name);
-                map.put(source_type, target_type);
-                contrastMap.put(dtcs_name, map);
-              }
-            });
+			  if (contrastMap.containsKey(dtcs_name)) {
+				contrastMap.get(dtcs_name).put(source_type, target_type);
+			  } else {
+				Map<String, Object> map = new LinkedHashMap<>();
+				map.put(NAME, dtcs_name);
+				map.put(source_type, target_type);
+				contrastMap.put(dtcs_name, map);
+			  }
+			});
 
-    List<Map<String, Object>> typeContrastAll = new ArrayList<>();
-    contrastMap.forEach(
-        (dtcs_name, typeContrast) -> {
-          typeContrastAll.add(typeContrast);
-        });
+	List<Map<String, Object>> typeContrastAll = new ArrayList<>();
+	contrastMap.forEach(
+		(dtcs_name, typeContrast) -> {
+		  typeContrastAll.add(typeContrast);
+		});
 
-    Map<String, List<Map<String, Object>>> map = new LinkedHashMap<>();
-    map.put(TypeContrast.PREFIX, typeContrastAll);
+	Map<String, List<Map<String, Object>>> map = new LinkedHashMap<>();
+	map.put(TypeContrast.PREFIX, typeContrastAll);
 
-    return map;
+	return map;
   }
 
   private List<Map<String, Object>> getTypeContrast() {
-    try (DatabaseWrapper db = new DatabaseWrapper()) {
-      return SqlOperator.queryList(
-          db,
-          "SELECT t2.dtcs_name,t1.source_type,t1.target_type FROM "
-              + Type_contrast.TableName
-              + " t1 "
-              + "LEFT JOIN "
-              + Type_contrast_sum.TableName
-              + " t2 ON t1.dtcs_id = t2.dtcs_id");
-    }
+	try (DatabaseWrapper db = new DatabaseWrapper()) {
+	  return SqlOperator.queryList(
+		  db,
+		  "select t1.dsl_name,t3.source_type,t3.target_type from " + Data_store_layer.TableName
+			  + " t1 left join " + Type_contrast_sum.TableName
+			  + " t2 on t1.dtcs_id = t2.dtcs_id left join " + Type_contrast.TableName
+			  + " t3 on t1.dtcs_id = t3.dtcs_id order by t1.dsl_name");
+	}
   }
 }
