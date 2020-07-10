6a8405c3e88b86d6723bc6efbb601aef1207be53
dhw <duhuawei@beyondsoft.com>
2020-07-10 14:44:44 +0800

    修改半结构化采集启动方式配置一致问题

--- hrds_B/src/main/java/hrds/b/biz/agent/bean/JobStartConf.java
+++ hrds_B/src/main/java/hrds/b/biz/agent/bean/JobStartConf.java
@@ -3,7 +3,6 @@ package hrds.b.biz.agent.bean;
 import fd.ng.core.annotation.DocBean;
 import fd.ng.core.annotation.DocClass;
 import fd.ng.db.entity.anno.Table;
-import hrds.commons.entity.Obj_relation_etl;
 import hrds.commons.entity.fdentity.ProjectTableEntity;
 
 @DocClass(desc = "", author = "dhw", createdate = "2020/6/16 16:32")
@@ -18,9 +17,46 @@ public class JobStartConf extends ProjectTableEntity {
 	private String pro_dic;
 	@DocBean(name = "log_dic", value = "日志目录", dataType = String.class, required = false)
 	private String log_dic;
-	@DocBean(name = "obj_relation_etl", value = "对象作业关系实体", dataType = Obj_relation_etl.class,
-			required = false)
-	private Obj_relation_etl obj_relation_etl;
+	@DocBean(name = "etl_sys_cd", value = "工程代码:", dataType = String.class, required = false)
+	private String etl_sys_cd;
+	@DocBean(name = "etl_job", value = "作业名:", dataType = String.class, required = false)
+	private String etl_job;
+	@DocBean(name = "sub_sys_cd", value = "子系统代码:", dataType = String.class, required = false)
+	private String sub_sys_cd;
+	@DocBean(name = "ocs_id", value = "对象采集任务编号:", dataType = Long.class, required = false)
+	private Long ocs_id;
+
+	public String getEtl_sys_cd() {
+		return etl_sys_cd;
+	}
+
+	public void setEtl_sys_cd(String etl_sys_cd) {
+		this.etl_sys_cd = etl_sys_cd;
+	}
+
+	public String getEtl_job() {
+		return etl_job;
+	}
+
+	public void setEtl_job(String etl_job) {
+		this.etl_job = etl_job;
+	}
+
+	public String getSub_sys_cd() {
+		return sub_sys_cd;
+	}
+
+	public void setSub_sys_cd(String sub_sys_cd) {
+		this.sub_sys_cd = sub_sys_cd;
+	}
+
+	public Long getOcs_id() {
+		return ocs_id;
+	}
+
+	public void setOcs_id(Long ocs_id) {
+		this.ocs_id = ocs_id;
+	}
 
 	public String getPro_dic() {
 		return pro_dic;
@@ -36,14 +72,6 @@ public class JobStartConf extends ProjectTableEntity {
 
 	public void setLog_dic(String log_dic) {
 		this.log_dic = log_dic;
-	}
-
-	public Obj_relation_etl getObj_relation_etl() {
-		return obj_relation_etl;
-	}
-
-	public void setObj_relation_etl(Obj_relation_etl obj_relation_etl) {
-		this.obj_relation_etl = obj_relation_etl;
 	}
 
 	public String[] getPre_etl_job() {
