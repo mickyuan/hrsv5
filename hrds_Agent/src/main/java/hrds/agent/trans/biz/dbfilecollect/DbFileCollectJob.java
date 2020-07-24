039ee34c1f3c8ee354ae7fe6843423b15e7c044c
xiaozhongzhu <zhuxiaozhong@beyondsoft.com>
2020-07-24 11:46:13 +0800

    提交agent立即执行后台接口

--- hrds_Agent/src/main/java/hrds/agent/trans/biz/dbfilecollect/DbFileCollectJob.java
+++ hrds_Agent/src/main/java/hrds/agent/trans/biz/dbfilecollect/DbFileCollectJob.java
@@ -7,13 +7,16 @@ import fd.ng.core.annotation.Param;
 import hrds.agent.job.biz.bean.CollectTableBean;
 import hrds.agent.job.biz.bean.JobStatusInfo;
 import hrds.agent.job.biz.bean.SourceDataConfBean;
+import hrds.agent.job.biz.constant.JobConstant;
 import hrds.agent.job.biz.core.DataFileJobImpl;
 import hrds.agent.job.biz.utils.FileUtil;
 import hrds.agent.job.biz.utils.JobStatusInfoUtil;
 import hrds.commons.base.AgentBaseAction;
-import hrds.commons.exception.AppSystemException;
+import hrds.commons.exception.BusinessException;
 import hrds.commons.utils.Constant;
 import hrds.commons.utils.PackUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,46 +26,66 @@ import java.util.concurrent.Future;
 
 @DocClass(desc = "db文件采集agent接受发送任务的接口", author = "zxz", createdate = "2020/3/26 9:56")
 public class DbFileCollectJob extends AgentBaseAction {
-	@Method(desc = "文件采集和前端交互的接口",
-			logicStep = "1.获取json数组转成File_source的集合" +
-					"2.校验对象的值是否正确" +
-					"3.使用JobFactory工厂类调用后台方法")
+	//打印日志
+	private static final Log log = LogFactory.getLog(DbFileCollectJob.class);
+
+	@Method(desc = "Db数据文件采集和前端交互生成配置文件的接口",
+			logicStep = "1.对配置信息解压缩并反序列化为SourceDataConfBean对象" +
+					"2.将页面传递过来的压缩信息解压写文件")
+	@Param(name = "taskInfo", desc = "数据库采集需要的参数实体bean的json对象字符串",
+			range = "所有这张表不能为空的字段的值必须有，为空则会抛异常，" +
+					"collectTableBeanArray对应的表CollectTableBean这个实体不能为空的字段的值必须有，为空则会抛异常")
+	public void execute(String taskInfo) {
+		//1.对配置信息解压缩并反序列化为SourceDataConfBean对象
+		SourceDataConfBean sourceDataConfBean =
+				JSONObject.parseObject(PackUtil.unpackMsg(taskInfo).get("msg"), SourceDataConfBean.class);
+		//2.将页面传递过来的压缩信息解压写文件
+		FileUtil.createFile(Constant.MESSAGEFILE + sourceDataConfBean.getDatabase_id(),
+				PackUtil.unpackMsg(taskInfo).get("msg"));
+	}
+
+	@Method(desc = "Db数据文件采集和前端交互立即执行的接口",
+			logicStep = "1.对配置信息解压缩并反序列化为SourceDataConfBean对象" +
+					"2.将页面传递过来的压缩信息解压写文件" +
+					"3.获取json数组转成CollectTableBean的集合" +
+					"4.遍历，设置跑批日期，多线程执行任务" +
+					"5.打印每个线程执行情况")
 	@Param(name = "etlDate", desc = "跑批日期", range = "不能为空")
 	@Param(name = "taskInfo", desc = "数据库采集需要的参数实体bean的json对象字符串",
 			range = "所有这张表不能为空的字段的值必须有，为空则会抛异常，" +
 					"collectTableBeanArray对应的表CollectTableBean这个实体不能为空的字段的值必须有，为空则会抛异常")
-	public void execute(String etlDate, String taskInfo) {
-		//对配置信息解压缩并反序列化为SourceDataConfBean对象
+	public void executeImmediately(String etlDate, String taskInfo) {
+		//1.对配置信息解压缩并反序列化为SourceDataConfBean对象
 		SourceDataConfBean sourceDataConfBean =
 				JSONObject.parseObject(PackUtil.unpackMsg(taskInfo).get("msg"), SourceDataConfBean.class);
-		//将页面传递过来的压缩信息解压写文件
+		//2.将页面传递过来的压缩信息解压写文件
 		FileUtil.createFile(Constant.MESSAGEFILE + sourceDataConfBean.getDatabase_id(),
 				PackUtil.unpackMsg(taskInfo).get("msg"));
-//		ExecutorService executor = null;
-//		try {
-//			//1.获取json数组转成File_source的集合
-//			List<CollectTableBean> collectTableBeanList = sourceDataConfBean.getCollectTableBeanArray();
-//			//此处不会有海量的任务需要执行，不会出现队列中等待的任务对象过多的OOM事件。
-//			executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-//			List<Future<JobStatusInfo>> list = new ArrayList<>();
-//			//2.校验对象的值是否正确
-//			for (CollectTableBean collectTableBean : collectTableBeanList) {
-//				//设置跑批日期
-//				collectTableBean.setEtlDate(etlDate);
-//				//为了确保多个线程之间的值不互相干涉，复制对象的值。
-//				SourceDataConfBean sourceDataConfBean1 = JSONObject.parseObject(
-//						JSONObject.toJSONString(sourceDataConfBean), SourceDataConfBean.class);
-//				DataFileJobImpl fileCollectJob = new DataFileJobImpl(sourceDataConfBean1, collectTableBean);
-//				Future<JobStatusInfo> submit = executor.submit(fileCollectJob);
-//				list.add(submit);
-//			}
-//			//3.打印每个线程执行情况
-//			JobStatusInfoUtil.printJobStatusInfo(list);
-//		} catch (Exception e) {
-//			throw new AppSystemException("DB文件采集失败!", e);
-//		} finally {
-//			if (executor != null)
-//				executor.shutdown();
-//		}
+		ExecutorService executor = null;
+		try {
+			//3.获取json数组转成CollectTableBean的集合
+			List<CollectTableBean> collectTableBeanList = sourceDataConfBean.getCollectTableBeanArray();
+			executor = Executors.newFixedThreadPool(JobConstant.AVAILABLEPROCESSORS);
+			List<Future<JobStatusInfo>> list = new ArrayList<>();
+			//4.遍历，设置跑批日期，多线程执行任务
+			for (CollectTableBean collectTableBean : collectTableBeanList) {
+				//设置跑批日期
+				collectTableBean.setEtlDate(etlDate);
+				//为了确保多个线程之间的值不互相干涉，复制对象的值。
+				SourceDataConfBean sourceDataConfBean1 = JSONObject.parseObject(
+						JSONObject.toJSONString(sourceDataConfBean), SourceDataConfBean.class);
+				DataFileJobImpl fileCollectJob = new DataFileJobImpl(sourceDataConfBean1, collectTableBean);
+				Future<JobStatusInfo> submit = executor.submit(fileCollectJob);
+				list.add(submit);
+			}
+			//5.打印每个线程执行情况
+			JobStatusInfoUtil.printJobStatusInfo(list);
+		} catch (Exception e) {
+			log.error(e);
+			throw new BusinessException("执行db文件采集入库任务失败:" + e.getMessage());
+		} finally {
+			if (executor != null)
+				executor.shutdown();
+		}
 	}
 }
--- hrds_Agent/src/main/java/hrds/agent/trans/biz/jdbccollect/JdbcCollectJob.java
+++ hrds_Agent/src/main/java/hrds/agent/trans/biz/jdbccollect/JdbcCollectJob.java
@@ -8,16 +8,19 @@ import hrds.agent.job.biz.bean.CollectTableBean;
 import hrds.agent.job.biz.bean.JobStatusInfo;
 import hrds.agent.job.biz.bean.SourceDataConfBean;
 import hrds.agent.job.biz.bean.TableBean;
+import hrds.agent.job.biz.constant.JobConstant;
 import hrds.agent.job.biz.core.DataBaseJobImpl;
 import hrds.agent.job.biz.core.metaparse.CollectTableHandleFactory;
 import hrds.agent.job.biz.utils.DataExtractUtil;
 import hrds.agent.job.biz.utils.FileUtil;
 import hrds.agent.job.biz.utils.JobStatusInfoUtil;
 import hrds.commons.base.AgentBaseAction;
-import hrds.commons.codes.FileFormat;
-import hrds.commons.exception.AppSystemException;
+import hrds.commons.entity.Data_extraction_def;
+import hrds.commons.exception.BusinessException;
 import hrds.commons.utils.Constant;
 import hrds.commons.utils.PackUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,54 +30,78 @@ import java.util.concurrent.Future;
 
 @DocClass(desc = "数据库采集接收消息接口", author = "zxz", createdate = "2019/12/2 10:35")
 public class JdbcCollectJob extends AgentBaseAction {
+	//打印日志
+	private static final Log log = LogFactory.getLog(JdbcCollectJob.class);
 
 	@Method(desc = "数据库抽取和前端交互生成页面配置到agent所在目录的接口",
 			logicStep = "1.对配置信息解压缩并反序列化为SourceDataConfBean对象" +
 					"2.将页面传递过来的压缩信息解压写文件")
-	@Param(name = "etlDate", desc = "跑批日期", range = "不能为空")
 	@Param(name = "taskInfo", desc = "数据库采集需要的参数实体bean的json对象字符串",
 			range = "所有sourceDataConfBean表不能为空的字段的值必须有，为空则会抛异常，" +
 					"collectTableBeanArray对应的表CollectTableBean这个实体不能为空的字段的值必须有，为空则会抛异常")
-	public void execute(String etlDate, String taskInfo) {
+	public void execute(String taskInfo) {
 		//1.对配置信息解压缩并反序列化为SourceDataConfBean对象
 		SourceDataConfBean sourceDataConfBean =
 				JSONObject.parseObject(PackUtil.unpackMsg(taskInfo).get("msg"), SourceDataConfBean.class);
 		//2.将页面传递过来的压缩信息解压写文件
 		FileUtil.createFile(Constant.MESSAGEFILE + sourceDataConfBean.getDatabase_id(),
 				PackUtil.unpackMsg(taskInfo).get("msg"));
-//		ExecutorService executor = null;
-//		try {
-//			//初始化当前任务需要保存的文件的根目录
-//			String[] paths = {Constant.JOBINFOPATH + sourceDataConfBean.getDatabase_id(),
-//					Constant.DICTIONARY + sourceDataConfBean.getDatabase_id()};
-//			FileUtil.initPath(paths);
-//			//1.获取json数组转成File_source的集合
-//			List<CollectTableBean> collectTableBeanList = sourceDataConfBean.getCollectTableBeanArray();
-//			//此处不会有海量的任务需要执行，不会出现队列中等待的任务对象过多的OOM事件。
-//			//TODO Runtime.getRuntime().availableProcessors()此处不能用这个,因为可能同时又多个数据库采集同时进行
-//			executor = Executors.newFixedThreadPool(5);
-//			List<Future<JobStatusInfo>> list = new ArrayList<>();
-//			//2.校验对象的值是否正确
-//			for (CollectTableBean collectTableBean : collectTableBeanList) {
-//				collectTableBean.setSelectFileFormat(FileFormat.FeiDingChang.getCode());
-//				//设置跑批日期
-//				collectTableBean.setEtlDate(etlDate);
-//				//为了确保多个线程之间的值不互相干涉，复制对象的值。
-//				SourceDataConfBean sourceDataConfBean1 = JSONObject.parseObject(
-//						JSONObject.toJSONString(sourceDataConfBean), SourceDataConfBean.class);
-//				//XXX 多线程执行
-//				DataBaseJobImpl fileCollectJob = new DataBaseJobImpl(sourceDataConfBean1, collectTableBean);
-//				Future<JobStatusInfo> submit = executor.submit(fileCollectJob);
-//				list.add(submit);
-//			}
-//			//3.打印每个线程执行情况
-//			JobStatusInfoUtil.printJobStatusInfo(list);
-//		} catch (Exception e) {
-//			throw new AppSystemException("数据库文件抽取采集失败!", e);
-//		} finally {
-//			if (executor != null)
-//				executor.shutdown();
-//		}
+	}
+
+	@Method(desc = "数据库抽取和前端交互立即执行的接口",
+			logicStep = "1.对配置信息解压缩并反序列化为SourceDataConfBean对象" +
+					"2.将页面传递过来的压缩信息解压写文件" +
+					"3.初始化当前任务需要保存的文件的根目录" +
+					"4.获取json数组转成CollectTableBean的集合" +
+					"5.遍历CollectTableBean的集合，遍历落地文件格式，设置跑批日期，当前线程采集的文件格式" +
+					"6.打印每个线程执行情况")
+	@Param(name = "etlDate", desc = "跑批日期", range = "不能为空")
+	@Param(name = "taskInfo", desc = "数据库采集需要的参数实体bean的json对象字符串",
+			range = "所有sourceDataConfBean表不能为空的字段的值必须有，为空则会抛异常，" +
+					"collectTableBeanArray对应的表CollectTableBean这个实体不能为空的字段的值必须有，为空则会抛异常")
+	public void executeImmediately(String etlDate, String taskInfo) {
+		//1.对配置信息解压缩并反序列化为SourceDataConfBean对象
+		SourceDataConfBean sourceDataConfBean =
+				JSONObject.parseObject(PackUtil.unpackMsg(taskInfo).get("msg"), SourceDataConfBean.class);
+		//2.将页面传递过来的压缩信息解压写文件
+		FileUtil.createFile(Constant.MESSAGEFILE + sourceDataConfBean.getDatabase_id(),
+				PackUtil.unpackMsg(taskInfo).get("msg"));
+		ExecutorService executor = null;
+		try {
+			//3.初始化当前任务需要保存的文件的根目录
+			String[] paths = {Constant.DICTIONARY + sourceDataConfBean.getDatabase_id()};
+			FileUtil.initPath(paths);
+			//4.获取json数组转成CollectTableBean的集合
+			List<CollectTableBean> collectTableBeanList = sourceDataConfBean.getCollectTableBeanArray();
+			//此处不会有海量的任务需要执行，不会出现队列中等待的任务对象过多的OOM事件。
+			executor = Executors.newFixedThreadPool(JobConstant.AVAILABLEPROCESSORS);
+			List<Future<JobStatusInfo>> list = new ArrayList<>();
+			//5.遍历CollectTableBean的集合，遍历落地文件格式，设置跑批日期，当前线程采集的文件格式
+			for (CollectTableBean collectTableBean : collectTableBeanList) {
+				List<Data_extraction_def> data_extraction_def_list = collectTableBean.getData_extraction_def_list();
+				for (Data_extraction_def data_extraction_def : data_extraction_def_list) {
+					collectTableBean.setEtlDate(etlDate);
+					collectTableBean.setSelectFileFormat(data_extraction_def.getDbfile_format());
+					//为了确保多个线程之间的值不互相干涉，复制对象的值。
+					SourceDataConfBean sourceDataConfBean1 = JSONObject.parseObject(
+							JSONObject.toJSONString(sourceDataConfBean), SourceDataConfBean.class);
+					CollectTableBean collectTableBean1 = JSONObject.parseObject(
+							JSONObject.toJSONString(collectTableBean), CollectTableBean.class);
+					//多线程执行
+					DataBaseJobImpl fileCollectJob = new DataBaseJobImpl(sourceDataConfBean1, collectTableBean1);
+					Future<JobStatusInfo> submit = executor.submit(fileCollectJob);
+					list.add(submit);
+				}
+			}
+			//6.打印每个线程执行情况
+			JobStatusInfoUtil.printJobStatusInfo(list);
+		} catch (Exception e) {
+			log.error(e);
+			throw new BusinessException("执行数据库抽取任务失败:" + e.getMessage());
+		} finally {
+			if (executor != null)
+				executor.shutdown();
+		}
 	}
 
 	@Method(desc = "数据库抽取和前端交互生成数据字典的接口",
