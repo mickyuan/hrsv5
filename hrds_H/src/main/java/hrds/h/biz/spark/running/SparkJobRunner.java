a12f58755304e60a80141538547613ec9a3a2ba4
xiaozhongzhu <zhuxiaozhong@beyondsoft.com>
2020-07-31 22:21:07 +0800

    提交集市跑spark在服务器上依赖的路径修改

--- hrds_H/src/main/java/hrds/h/biz/spark/running/SparkJobRunner.java
+++ hrds_H/src/main/java/hrds/h/biz/spark/running/SparkJobRunner.java
@@ -13,11 +13,11 @@ public class SparkJobRunner {
 
 	private static final String SPARK_MAIN_CLASS =
 			"hrds.h.biz.spark.running.MarketSparkMain";
+	//	private static final String SPARK_CLASSPATH =
+//			"hrds_H/build/libs/hrds_H-5.0.jar;lib/*;hrds_H/src/main/resources/;spark/jars/*;" +
+//					"libs/runtime/*;hrds-commons/build/libs/hrds-commons-5.0.jar";
 	private static final String SPARK_CLASSPATH =
-			"hrds_H/build/libs/hrds_H-5.0.jar;lib/*;hrds_H/src/main/resources/;spark/jars/*;" +
-					"libs/runtime/*;hrds-commons/build/libs/hrds-commons-5.0.jar";
-//    private static final String SPARK_CLASSPATH =
-//            ".:hrds_H-5.0.jar:resources/:" +
+			".:hrds_H-5.0.jar:resources/:../spark/jars/*:../lib/*";
 //                    "../spark/jars/*:../lib/fd-*:../lib/hrds-commons-5.0.jar";
 
 	private static final long SPARK_JOB_TIMEOUT_SECONDS =
