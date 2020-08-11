bc35e7b68a50c9ced48e78444d63a7f281da2621
hulili930715 <78733419@qq.com>
2020-08-11 10:47:19 +0800

    数据管控(规则校验,作业调度执行依赖脚本)

--- bin/startEngineBatchTrigger.sh
+++ bin/startEngineBatchTrigger.sh
@@ -9,32 +9,32 @@ SH_EXEC_DIR=$(cd $(dirname $0); pwd)
 # Get system bits
 OS_BIT=`getconf LONG_BIT`
 
-ROOT_DIR=./
+ROOT_DIR=${SH_EXEC_DIR}
 YEAR=`date -d now +%Y`
 MONTH=`date -d now +%Y%m`
 DAY=`date -d now +%Y%m%d`
 
-LOG_YEAR_DIR=$ROOT_DIR$YEAR/
-LOG_DIR=$LOG_YEAR_DIR$MONTH/
+LOG_YEAR_DIR=${ROOT_DIR}"/"${YEAR}
+LOG_DIR=${LOG_YEAR_DIR}"/"${MONTH}
 
 #标准输出日志
-LOG_OUT_FILE=$LOG_DIR$DAY"TriggerOut".log
-echo "标准输出日志"$LOG_OUT_FILE
+LOG_OUT_FILE=${LOG_DIR}"/"${DAY}"_TriggerOut".log
+echo "标准输出日志: "${LOG_OUT_FILE}
 
 #标准输出错误日志
-LOG_ERR_FILE=$LOG_DIR$DAY"TriggerErr".log
-echo "标准输出错误日志"$LOG_ERR_FILE
+LOG_ERR_FILE=${LOG_DIR}"/"${DAY}"_TriggerErr".log
+echo "标准输出错误日志: "${LOG_ERR_FILE}
 
-if [ ! -d $LOG_YEAR_DIR ]; then
- mkdir -m 755 $LOG_YEAR_DIR  
-fi
-
-if [ ! -d $LOG_DIR ]; then
- mkdir -m 755 $LOG_DIR  
+if [[ ! -d ${LOG_DIR} ]]; then
+ mkdir -p -m 755 ${LOG_DIR}
 fi
 
 cd ${JRE_DIRECTORY}
 chmod -R 755 jre
-nohup ${JRE_DIRECTORY}/jre/linux/${OS_BIT}/jre/bin/java -Dorg.eclipse.jetty.server.Request.maxFormContentSize=99900000 -Dproject.name=hrds_Trigger -Dproject.dir=${SH_EXEC_DIR} -jar hrds_Trigger-5.0.jar  sys.code=$1 1>>$LOG_OUT_FILE 2>>$LOG_ERR_FILE &
+nohup ${JRE_DIRECTORY}/jre/linux/${OS_BIT}/jre/bin/java \
+    -Dorg.eclipse.jetty.server.Request.maxFormContentSize=99900000 \
+    -Dproject.name=hrds_Trigger \
+    -Dproject.dir=${SH_EXEC_DIR} \
+    -jar hrds_Trigger-5.0.jar  \
+    sys.code=$1 1>>${LOG_OUT_FILE} 2>>${LOG_ERR_FILE} &
 exit 0
-
