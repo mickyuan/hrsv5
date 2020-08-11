35b363a1d9788a0831d9d6e081d8916e8a666c40
hulili930715 <78733419@qq.com>
2020-08-11 14:29:15 +0800

    数据管控(规则校验,作业调度执行依赖脚本)

--- bin/quality_manage.sh
+++ bin/quality_manage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Jre directory
-JRE_DIRECTORY=$(dirname $(pwd))
 # Shell script execution directory
 SH_EXEC_DIR=$(cd $(dirname $0); pwd)
+# Jre directory
+JRE_DIRECTORY=$(cd "${SH_EXEC_DIR}/../"; pwd)
 # MainClass
 MainClass="hrds.k.biz.RuleCMDExecute"
 # Depend on jar package
@@ -18,11 +18,15 @@ verify_date="${2}"
 ## 参数1  规则编号 10000001
 ## 参数2  验证日期 20200202
 ## 使用方式 sh quality_manage.sh 10000001 20200202
-## 返回状态码说明 {0: 执行成功; 1: 参数不合法; -1: 执行失败; 2: 根据规则编号没有找到对应的规则}
+## 返回状态码说明 {0: 执行成功; 1: 参数不合法; -1: 执行失败; 2: 根据规则编号没有找到对应的规则; 3: jre目录不存在}
 main(){
     # if no parameter is passed to script then show how to use.
     if [[ $# -eq 0 ]]; then usage;fi
     if [[ $# -ne 2 ]]; then usage; fi
+    # Enter the script directory
+    cd ${SH_EXEC_DIR}
+    # Check jre environment
+    if [[ ! -d "${JRE_DIRECTORY}/jre" ]]; then echo "jre does not exist, please check the jre env." ; exit 3; fi
     # Get system bits
     OS_BIT=`getconf LONG_BIT`
     # execute script
