879a29ce9cd1266de86d256126a4a146f176f52f
Qiang-Lee <hliqiang1989@gmail.com>
2020-08-21 10:20:44 +0800

    删除无用类

--- hrds_B/src/main/java/hrds/b/biz/agent/loadintostorage/startmethod/StartMethodAction.java
+++
@@ -1,8 +0,0 @@
-package hrds.b.biz.agent.loadintostorage.startmethod;
-
-import fd.ng.core.annotation.DocClass;
-
-@DocClass(desc = "配置启动方式", author = "Mr.Lee", createdate = "2020-08-17 16:09")
-public class StartMethodAction {
-
-}
