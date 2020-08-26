a7a82fbd296001a235d2434f4d5dfc7ad7378ddb
hulili930715 <78733419@qq.com>
2020-08-26 16:50:57 +0800

    DMLDataQuery(加工数据查询添加根据任务执行状态查询表的方法)

--- hrds-commons/src/main/java/hrds/commons/tree/background/query/DMLDataQuery.java
+++ hrds-commons/src/main/java/hrds/commons/tree/background/query/DMLDataQuery.java
@@ -18,61 +18,77 @@ import java.util.Map;
 @DocClass(desc = "加工层(DML)层数据信息查询类", author = "BY-HLL", createdate = "2020/1/7 0007 上午 11:17")
 public class DMLDataQuery {
 
-    @Method(desc = "获取加工信息", logicStep = "1.如果加工名称不为空,模糊查询获取加工信息")
-    @Param(name = "user", desc = "User", range = "登录用户信息")
-    @Return(desc = "加工信息列表", range = "无限制")
-    public static List<Map<String, Object>> getDMLDataInfos(User user) {
-        //初始化查询sql
-        SqlOperator.Assembler asmSql = SqlOperator.Assembler.newInstance();
-        asmSql.clean();
-        asmSql.addSql("SELECT distinct t1.* from " + Dm_info.TableName + " t1 left join " + Sys_user.TableName + " t2 on t1.create_id = t2.user_id ");
-        //1.获取数据源下分类信息,如果是系统管理员,则不过滤部门
-        if (!UserType.XiTongGuanLiYuan.getCode().equals(user.getUserType())) {
-            asmSql.addSql(" where t2.dep_id = ?");
-            asmSql.addParam(user.getDepId());
-        }
-        return Dbo.queryList(asmSql.sql(), asmSql.params());
-    }
+	@Method(desc = "获取加工信息", logicStep = "1.如果加工名称不为空,模糊查询获取加工信息")
+	@Param(name = "user", desc = "User", range = "登录用户信息")
+	@Return(desc = "加工信息列表", range = "无限制")
+	public static List<Map<String, Object>> getDMLDataInfos(User user) {
+		//初始化查询sql
+		SqlOperator.Assembler asmSql = SqlOperator.Assembler.newInstance();
+		asmSql.clean();
+		asmSql.addSql("SELECT distinct t1.* from " + Dm_info.TableName + " t1 left join " + Sys_user.TableName + " t2 on t1.create_id = t2.user_id ");
+		//1.获取数据源下分类信息,如果是系统管理员,则不过滤部门
+		if (!UserType.XiTongGuanLiYuan.getCode().equals(user.getUserType())) {
+			asmSql.addSql(" where t2.dep_id = ?");
+			asmSql.addParam(user.getDepId());
+		}
+		return Dbo.queryList(asmSql.sql(), asmSql.params());
+	}
 
-    @Method(desc = "获取加工信息", logicStep = "1.如果加工名称不为空,模糊查询获取加工信息")
-    @Param(name = "user", desc = "User", range = "登录用户信息")
-    @Return(desc = "加工信息列表", range = "无限制")
-    public static List<Map<String, Object>> getDMLCategoryInfos(long data_mart_id) {
-        return Dbo.queryList("select * from " + Dm_category.TableName + " where data_mart_id=?", data_mart_id);
-    }
+	@Method(desc = "获取加工信息", logicStep = "1.如果加工名称不为空,模糊查询获取加工信息")
+	@Param(name = "user", desc = "User", range = "登录用户信息")
+	@Return(desc = "加工信息列表", range = "无限制")
+	public static List<Map<String, Object>> getDMLCategoryInfos(long data_mart_id) {
+		return Dbo.queryList("select * from " + Dm_category.TableName + " where data_mart_id=?", data_mart_id);
+	}
 
-    @Method(desc = "获取加工下表信息列表", logicStep = "获取加工下表信息列表")
-    @Param(name = "category_id", desc = "加工分类id,该值唯一", range = "long类型")
-    @Return(desc = "返回值说明", range = "返回值取值范围")
-    public static List<Map<String, Object>> getDMLTableInfos(long category_id) {
-        return Dbo.queryList("select * from " + Dm_datatable.TableName + " dm" +
-                " join " + Dtab_relation_store.TableName + " dtab_rs on dm.datatable_id=dtab_rs.tab_id" +
-                " where dm.category_id=? and dtab_rs.is_successful=?", category_id, JobExecuteState.WanCheng.getCode());
-    }
+	@Method(desc = "获取加工下表信息列表", logicStep = "获取加工下表信息列表")
+	@Param(name = "category_id", desc = "加工分类id,该值唯一", range = "long类型")
+	@Return(desc = "返回值说明", range = "返回值取值范围")
+	public static List<Map<String, Object>> getDMLTableInfos(long category_id) {
+		//默认只查询执行完成并且登记成功的表
+		return getDMLTableInfos(category_id, JobExecuteState.WanCheng);
+	}
 
-    @Method(desc = "获取加工下表信息", logicStep = "获取加工下表信息")
-    @Param(name = "datatable_id", desc = "加工表id,唯一", range = "String类型")
-    @Return(desc = "返回值说明", range = "返回值取值范围")
-    public static Dm_datatable getDMLTableInfo(String datatable_id) {
-        //设置加工表对象
-        Dm_datatable dm_datatable = new Dm_datatable();
-        dm_datatable.setDatatable_id(datatable_id);
-        //查询加工表信息
-        return Dbo.queryOneObject(Dm_datatable.class, "select * from " + Dm_datatable.TableName + " where datatable_id" +
-                "=?", dm_datatable.getDatatable_id()).orElseThrow(() -> new BusinessException("获取加工数据表信息的sql失败!"));
-    }
+	@Method(desc = "获取加工下表信息列表", logicStep = "获取加工下表信息列表")
+	@Param(name = "category_id", desc = "加工分类id,该值唯一", range = "long类型")
+	@Param(name = "jobExecuteState", desc = "JobExecuteState作业执行状态", range = "如果执行状态不为null,查询指定状态的表,如果为null,查询所有表")
+	@Return(desc = "返回值说明", range = "返回值取值范围")
+	public static List<Map<String, Object>> getDMLTableInfos(long category_id, JobExecuteState jobExecuteState) {
+		//初始化查询sql
+		SqlOperator.Assembler asmSql = SqlOperator.Assembler.newInstance();
+		asmSql.clean();
+		asmSql.addSql("select * from " + Dm_datatable.TableName + " dm" +
+				" join " + Dtab_relation_store.TableName + " dtab_rs on dm.datatable_id=dtab_rs.tab_id" +
+				" where dm.category_id=?").addParam(category_id);
+		if (null != jobExecuteState) {
+			asmSql.addSql(" and dtab_rs.is_successful=?").addParam(jobExecuteState.getCode());
+		}
+		return Dbo.queryList(asmSql.sql(), asmSql.params());
+	}
 
-    @Method(desc = "获取加工下表的字段信息", logicStep = "获取加工下表的字段信息")
-    @Param(name = "datatable_id", desc = "加工表id,唯一", range = "String类型")
-    @Return(desc = "返回值说明", range = "返回值取值范围")
-    public static List<Map<String, Object>> getDMLTableColumns(String datatable_id) {
-        //设置加工表对象
-        Dm_datatable dm_datatable = new Dm_datatable();
-        dm_datatable.setDatatable_id(datatable_id);
-        //查询表字段信息
-        return Dbo.queryList("select datatable_field_id AS column_id,field_en_name as column_name," +
-                        " field_cn_name as column_ch_name,concat(field_type,'(',field_length,')') AS column_type," +
-                        " '0' AS is_primary_key FROM " + Datatable_field_info.TableName + " WHERE datatable_id= ?",
-                dm_datatable.getDatatable_id());
-    }
+	@Method(desc = "获取加工下表信息", logicStep = "获取加工下表信息")
+	@Param(name = "datatable_id", desc = "加工表id,唯一", range = "String类型")
+	@Return(desc = "返回值说明", range = "返回值取值范围")
+	public static Dm_datatable getDMLTableInfo(String datatable_id) {
+		//设置加工表对象
+		Dm_datatable dm_datatable = new Dm_datatable();
+		dm_datatable.setDatatable_id(datatable_id);
+		//查询加工表信息
+		return Dbo.queryOneObject(Dm_datatable.class, "select * from " + Dm_datatable.TableName + " where datatable_id" +
+				"=?", dm_datatable.getDatatable_id()).orElseThrow(() -> new BusinessException("获取加工数据表信息的sql失败!"));
+	}
+
+	@Method(desc = "获取加工下表的字段信息", logicStep = "获取加工下表的字段信息")
+	@Param(name = "datatable_id", desc = "加工表id,唯一", range = "String类型")
+	@Return(desc = "返回值说明", range = "返回值取值范围")
+	public static List<Map<String, Object>> getDMLTableColumns(String datatable_id) {
+		//设置加工表对象
+		Dm_datatable dm_datatable = new Dm_datatable();
+		dm_datatable.setDatatable_id(datatable_id);
+		//查询表字段信息
+		return Dbo.queryList("select datatable_field_id AS column_id,field_en_name as column_name," +
+						" field_cn_name as column_ch_name,concat(field_type,'(',field_length,')') AS column_type," +
+						" '0' AS is_primary_key FROM " + Datatable_field_info.TableName + " WHERE datatable_id= ?",
+				dm_datatable.getDatatable_id());
+	}
 }
