0b0f34033aab0de1aec2647eed8fa7a1b609721a
hulili930715 <78733419@qq.com>
2020-08-27 11:09:52 +0800

    MarketVersionManageAction(删除无用代码)

--- hrds_H/src/main/java/hrds/h/biz/manage/version/MarketVersionManageAction.java
+++ hrds_H/src/main/java/hrds/h/biz/manage/version/MarketVersionManageAction.java
@@ -4,7 +4,6 @@ import fd.ng.core.annotation.DocClass;
 import fd.ng.core.annotation.Method;
 import fd.ng.core.annotation.Param;
 import fd.ng.core.annotation.Return;
-import fd.ng.core.utils.JsonUtil;
 import fd.ng.core.utils.Validator;
 import fd.ng.db.jdbc.SqlOperator;
 import fd.ng.web.util.Dbo;
