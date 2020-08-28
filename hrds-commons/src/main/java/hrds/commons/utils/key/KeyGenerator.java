257b8a9874ee739445e8fc167ff0bf579211cf0e
hulili930715 <78733419@qq.com>
2020-08-28 16:55:46 +0800

    KeyGenerator(语法优化)

--- hrds-commons/src/main/java/hrds/commons/utils/key/KeyGenerator.java
+++ hrds-commons/src/main/java/hrds/commons/utils/key/KeyGenerator.java
@@ -4,27 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class KeyGenerator {
-    private static final KeyGenerator m_instance = new KeyGenerator();
-    private static final int POOL_SIZE = 5;
-    private Map keyList;
+	private static final KeyGenerator m_instance = new KeyGenerator();
+	private static final int POOL_SIZE = 5;
+	private Map<String, KeyDBPool> keyList;
 
-    public synchronized long getNextKey(String keyName) {
-        KeyDBPool key_pool = null;
-        if (keyList.containsKey(keyName)) {
-            key_pool = (KeyDBPool) keyList.get(keyName);
-        } else {
-            key_pool = new KeyDBPool(5, keyName);
-            keyList.put(keyName, key_pool);
-        }
-        long key = key_pool.getNextKey();
-        return key;
-    }
+	public synchronized long getNextKey(String keyName) {
+		KeyDBPool key_pool;
+		if (keyList.containsKey(keyName)) {
+			key_pool = keyList.get(keyName);
+		} else {
+			key_pool = new KeyDBPool(5, keyName);
+			keyList.put(keyName, key_pool);
+		}
+		return key_pool.getNextKey();
+	}
 
-    private KeyGenerator() {
-        keyList = new HashMap(10);
-    }
+	private KeyGenerator() {
+		keyList = new HashMap<>(10);
+	}
 
-    public static KeyGenerator getInstance() {
-        return m_instance;
-    }
+	public static KeyGenerator getInstance() {
+		return m_instance;
+	}
 }
