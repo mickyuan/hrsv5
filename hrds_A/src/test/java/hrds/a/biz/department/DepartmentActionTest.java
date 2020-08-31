321a7b4a1ba4113c2194084bb31cadcab3fe5ddd
hulili930715 <78733419@qq.com>
2020-08-31 11:20:51 +0800

    DepartmentActionTest(已知问题修改,用例写法问题)

--- hrds_A/src/test/java/hrds/a/biz/department/DepartmentActionTest.java
+++ hrds_A/src/test/java/hrds/a/biz/department/DepartmentActionTest.java
@@ -11,12 +11,11 @@ import fd.ng.netclient.http.HttpClient;
 import fd.ng.web.action.ActionResult;
 import hrds.commons.entity.Department_info;
 import hrds.commons.entity.Sys_user;
+import hrds.commons.exception.BusinessException;
 import hrds.testbase.WebBaseTestCase;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-
-import java.util.OptionalLong;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -29,9 +28,6 @@ public class DepartmentActionTest extends WebBaseTestCase {
 	private static final String USER_NAME = "超级管理员init-hll";
 	//测试数据用户密码
 	private static final String USER_PASSWORD = "111111";
-
-	private static String bodyString;
-	private static ActionResult ar;
 
 	@Method(desc = "初始化测试用例依赖表数据",
 			logicStep = "1.初始化数据" +
@@ -47,8 +43,8 @@ public class DepartmentActionTest extends WebBaseTestCase {
 					"* 1.sys_user 表中有3条测试数据 user_id:-1000 user_password:111111," +
 					" user_id:-1000 user_password:111111," +
 					" user_id:-1000 user_password:111111 ")
-	@BeforeClass
-	public static void before() {
+	@Before
+	public void before() {
 		try (DatabaseWrapper db = new DatabaseWrapper()) {
 			//1.初始化数据
 			//1-1.初始化 Sys_user 表数据 (初始化一个超级管理员)
@@ -106,11 +102,12 @@ public class DepartmentActionTest extends WebBaseTestCase {
 			//2.提交所有数据库执行操作
 			SqlOperator.commitTransaction(db);
 			//3.用户模拟登陆
-			bodyString = new HttpClient()
+			String bodyString = new HttpClient()
 					.addData("user_id", USER_ID)
 					.addData("password", "111111")
-					.post("http://127.0.0.1:8099/A/action/hrds/a/biz/login/login").getBodyString();
-			ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class).get();
+					.post("http://127.0.0.1:8888/A/action/hrds/a/biz/login/login").getBodyString();
+			ActionResult ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class)
+					.orElseThrow(() -> new BusinessException("模拟登录失败"));
 			assertThat(ar.isSuccess(), is(true));
 		}
 	}
@@ -119,8 +116,8 @@ public class DepartmentActionTest extends WebBaseTestCase {
 			logicStep = "1.删除测试数据" +
 					"1-1.删除 sys_user 表测试数据" +
 					"1-2.删除 Department_info 表测试数据")
-	@AfterClass
-	public static void after() {
+	@After
+	public void after() {
 		try (DatabaseWrapper db = new DatabaseWrapper()) {
 			//删除测试数据
 			//1-1.删除 sys_user 表测试数据
@@ -169,6 +166,7 @@ public class DepartmentActionTest extends WebBaseTestCase {
 					"select count(1) from " + Department_info.TableName + " where dep_name=?",
 					"测试添加部门init-hll"
 			).orElseThrow(() -> new RuntimeException("count fail!"));
+			assertThat("Department_info 表此条数据删除后,记录数应该为0", deptDataNum, is(0L));
 			deptDataNum = SqlOperator.queryNumber(db,
 					"select count(1) from " + Department_info.TableName + " where dep_name=?",
 					"测试修改部门init-hll"
@@ -177,25 +175,26 @@ public class DepartmentActionTest extends WebBaseTestCase {
 		}
 	}
 
-	@Method(desc = "新增部门测试方法",
-			logicStep = "1.正确数据访问" +
-					"1-1.新增部门")
+	@Method(desc = "新增部门测试方法", logicStep = "1.正确数据访问" +
+			"1-1.新增部门")
 	@Test
 	public void addDepartmentInfo() {
 		//1.正确数据访问
 		//1-1.新增部门，部门名称不重复
-		bodyString = new HttpClient()
+		String bodyString = new HttpClient()
 				.addData("dep_name", "测试添加部门init-hll")
 				.addData("dep_remark", "测试添加部门init-hll")
 				.post(getActionUrl("addDepartmentInfo")).getBodyString();
-		ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class).get();
+		ActionResult ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class)
+				.orElseThrow(() -> new RuntimeException("获取ActionResult失败!"));
 		assertThat(ar.isSuccess(), is(true));
 		try (DatabaseWrapper db = new DatabaseWrapper()) {
 			//1-1-1.检查 Department_info 部门是否新增成功
-			OptionalLong number = SqlOperator.queryNumber(db, "select count(*) from " + Department_info.TableName +
-					" where dep_name=?", "测试添加部门init-hll");
+			long number = SqlOperator.queryNumber(db, "select count(*) from " + Department_info.TableName +
+					" where dep_name=?", "测试添加部门init-hll")
+					.orElseThrow(() -> new RuntimeException("统计测试数据结果的SQL执行失败!"));
 			assertThat("检查 Department_info 表数据，表 Department_info 数据新增成功",
-					number.getAsLong(), is(1L));
+					number, is(1L));
 		}
 		//2.错误数据访问
 		//2-1.部门名称重复
@@ -203,63 +202,66 @@ public class DepartmentActionTest extends WebBaseTestCase {
 				.addData("dep_name", "测试添加部门init-hll")
 				.addData("dep_remark", "测试添加部门init-hll")
 				.post(getActionUrl("addDepartmentInfo")).getBodyString();
-		ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class).get();
+		ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class)
+				.orElseThrow(() -> new RuntimeException("获取ActionResult失败!"));
 		assertThat(ar.isSuccess(), is(false));
-
 	}
 
-	@Method(desc = "删除部门信息测试类",
-			logicStep = "1.正确数据访问" +
-					"1-1.等待删除的数据源id存在" +
-					"2.错误数据访问" +
-					"2-1.待删除的数据源id不存在")
+	@Method(desc = "删除部门信息测试类", logicStep = "1.正确数据访问" +
+			"1-1.等待删除的数据源id存在" +
+			"2.错误数据访问" +
+			"2-1.待删除的数据源id不存在")
 	@Test
 	public void deleteDepartmentInfo() {
 		//1.正确数据访问
 		//1-1.等待删除的数据源id存在
-		bodyString = new HttpClient()
+		String bodyString = new HttpClient()
 				.addData("dep_id", -1000000001L)
 				.post(getActionUrl("deleteDepartmentInfo")).getBodyString();
-		ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class).get();
+		ActionResult ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class)
+				.orElseThrow(() -> new RuntimeException("获取ActionResult失败!"));
 		assertThat(ar.isSuccess(), is(true));
 		try (DatabaseWrapper db = new DatabaseWrapper()) {
 			//1-1-1.检查 Department_info 部门是否新增成功
-			OptionalLong number = SqlOperator.queryNumber(db, "select count(*) from " + Department_info.TableName +
-					" where dep_id=?", -1000000001L);
+			long number = SqlOperator.queryNumber(db, "select count(*) from " + Department_info.TableName +
+					" where dep_id=?", -1000000001L)
+					.orElseThrow(() -> new RuntimeException("统计测试数据结果的SQL执行失败!"));
 			assertThat("检查 Department_info 表数据，表 Department_info 数据删除成功",
-					number.getAsLong(), is(0L));
+					number, is(0L));
 		}
 		//2.错误数据访问
 		//2-1.待删除的数据源id不存在
 		bodyString = new HttpClient()
 				.addData("dep_id", 9000000001L)
 				.post(getActionUrl("deleteDepartmentInfo")).getBodyString();
-		ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class).get();
+		ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class)
+				.orElseThrow(() -> new RuntimeException("获取ActionResult失败!"));
 		assertThat(ar.isSuccess(), is(true));
 
 	}
 
-	@Method(desc = "修改部门信息测试类",
-			logicStep = "1.正确数据访问" +
-					"1-1.部门id存在" +
-					"2.错误数据访问" +
-					"2-1.部门id不存在")
+	@Method(desc = "修改部门信息测试类", logicStep = "1.正确数据访问" +
+			"1-1.部门id存在" +
+			"2.错误数据访问" +
+			"2-1.部门id不存在")
 	@Test
 	public void updateDepartmentInfo() {
 		//1.正确数据访问
 		//1-1.数据源id存在
-		bodyString = new HttpClient()
+		String bodyString = new HttpClient()
 				.addData("dep_id", -1000000002L)
 				.addData("dep_name", "修改+测试修改部门init-hll")
 				.addData("dep_remark", "修改+测试修改部门init-hll")
 				.post(getActionUrl("updateDepartmentInfo")).getBodyString();
-		ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class).get();
+		ActionResult ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class)
+				.orElseThrow(() -> new RuntimeException("获取ActionResult失败!"));
 		assertThat(ar.isSuccess(), is(true));
 		try (DatabaseWrapper db = new DatabaseWrapper()) {
 			//1-1-1.检查 Sys_para 参数是否修改成功
-			OptionalLong number = SqlOperator.queryNumber(db, "select count(*) from " + Department_info.TableName +
-					" where dep_id=?", -1000000002L);
-			assertThat("检查表 DepartmentInfo 数据修改成功", number.getAsLong(), is(1L));
+			long number = SqlOperator.queryNumber(db, "select count(*) from " + Department_info.TableName +
+					" where dep_id=?", -1000000002L)
+					.orElseThrow(() -> new RuntimeException("统计测试数据结果的SQL执行失败!"));
+			assertThat("检查表 DepartmentInfo 数据修改成功", number, is(1L));
 			//1-1-2.检查数据更新是否正确
 			Result rs = SqlOperator.queryResult(db, "select * from " + Department_info.TableName +
 					" where dep_id=?", -1000000002L);
@@ -273,19 +275,19 @@ public class DepartmentActionTest extends WebBaseTestCase {
 				.addData("dep_name", "测试部门init-hll")
 				.addData("dep_remark", "测试部门init-hll")
 				.post(getActionUrl("deleteDepartmentInfo")).getBodyString();
-		ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class).get();
+		ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class)
+				.orElseThrow(() -> new RuntimeException("获取ActionResult失败!"));
 		assertThat(ar.isSuccess(), is(true));
 	}
 
-	@Method(desc = "获取所有部门信息测试方法",
-			logicStep = "无参数" +
-					"校验结果集")
+	@Method(desc = "获取所有部门信息测试方法", logicStep = "无参数校验结果集")
 	@Test
 	public void getDepartmentInfo() {
 		//无参数
-		bodyString = new HttpClient()
+		String bodyString = new HttpClient()
 				.post(getActionUrl("getDepartmentInfo")).getBodyString();
-		ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class).get();
+		ActionResult ar = JsonUtil.toObjectSafety(bodyString, ActionResult.class)
+				.orElseThrow(() -> new RuntimeException("获取ActionResult失败!"));
 		assertThat(ar.isSuccess(), is(true));
 	}
 }
