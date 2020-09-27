37c6e579003e19cc4a8ec54c23731ce5f0f61fff
dhw <duhuawei@beyondsoft.com>
2020-09-27 11:09:40 +0800

    修改获取当前部署的Agent信息未加agent类型问题

--- hrds_B/src/main/java/hrds/b/biz/agentdepoly/AgentDeployAction.java
+++ hrds_B/src/main/java/hrds/b/biz/agentdepoly/AgentDeployAction.java
@@ -21,6 +21,7 @@ import hrds.commons.utils.PropertyParaValue;
 import hrds.commons.utils.jsch.AgentDeploy;
 import hrds.commons.utils.jsch.ChineseUtil;
 import hrds.commons.utils.key.PrimayKeyGener;
+
 import java.io.File;
 import java.util.List;
 import java.util.Map;
@@ -33,27 +34,27 @@ public class AgentDeployAction extends BaseAction {
 	public Result getDataSourceInfo() {
 		// 1: 查询当前用户的数据源信息
 		return Dbo.queryResult(
-			"select ds.source_id, ds.datasource_name, "
-				+ " sum(case ai.agent_type when ? then 1 else 0 end) as dbflag, "
-				+ " sum(case ai.agent_type when ? then 1 else 0 end) as dfflag, "
-				+ " sum(case ai.agent_type when ? then 1 else 0 end) as nonstructflag,"
-				+ " sum(case ai.agent_type when ? then 1 else 0 end) as halfstructflag,"
-				+ " sum(case ai.agent_type when ? then 1 else 0 end) as ftpflag"
-				+ " from "
-				+ Data_source.TableName
-				+ " ds "
-				+ " left join "
-				+ Agent_info.TableName
-				+ " ai "
-				+ " on ds.source_id = ai.source_id"
-				+ " where ai.user_id = ?"
-				+ " group by ds.source_id order by datasource_name",
-			AgentType.ShuJuKu.getCode(),
-			AgentType.DBWenJian.getCode(),
-			AgentType.WenJianXiTong.getCode(),
-			AgentType.DuiXiang.getCode(),
-			AgentType.FTP.getCode(),
-			getUserId());
+				"select ds.source_id, ds.datasource_name, "
+						+ " sum(case ai.agent_type when ? then 1 else 0 end) as dbflag, "
+						+ " sum(case ai.agent_type when ? then 1 else 0 end) as dfflag, "
+						+ " sum(case ai.agent_type when ? then 1 else 0 end) as nonstructflag,"
+						+ " sum(case ai.agent_type when ? then 1 else 0 end) as halfstructflag,"
+						+ " sum(case ai.agent_type when ? then 1 else 0 end) as ftpflag"
+						+ " from "
+						+ Data_source.TableName
+						+ " ds "
+						+ " left join "
+						+ Agent_info.TableName
+						+ " ai "
+						+ " on ds.source_id = ai.source_id"
+						+ " where ai.user_id = ?"
+						+ " group by ds.source_id order by datasource_name",
+				AgentType.ShuJuKu.getCode(),
+				AgentType.DBWenJian.getCode(),
+				AgentType.WenJianXiTong.getCode(),
+				AgentType.DuiXiang.getCode(),
+				AgentType.FTP.getCode(),
+				getUserId());
 	}
 
 	@Method(desc = "获取数据源下对应某种Agent的信息", logicStep = "根据选择的数据源及Agent类型查询其对应的Agent")
@@ -63,37 +64,37 @@ public class AgentDeployAction extends BaseAction {
 	public List<Map<String, Object>> getAgentInfo(long source_id, String agent_type) {
 
 		return Dbo.queryList(
-			"SELECT *,"
-				+ " ( CASE WHEN agent_type = ? THEN ? "
-				+ "   WHEN agent_type = ? THEN ?"
-				+ "   WHEN agent_type = ? THEN ? "
-				+ "   WHEN agent_type = ? THEN ? "
-				+ "   WHEN agent_type = ? THEN ? END) agent_zh_name,"
-				+ "(CASE WHEN agent_status = ? THEN ? "
-				+ "  WHEN agent_status = ? THEN ? END) connection_status"
-				+ " FROM agent_info WHERE source_id = ? AND agent_type = ? AND user_id = ?",
-			AgentType.ShuJuKu.getCode(),
-			AgentType.ShuJuKu.getValue(),
-			AgentType.DBWenJian.getCode(),
-			AgentType.DBWenJian.getValue(),
-			AgentType.DuiXiang.getCode(),
-			AgentType.DuiXiang.getValue(),
-			AgentType.WenJianXiTong.getCode(),
-			AgentType.WenJianXiTong.getValue(),
-			AgentType.FTP.getCode(),
-			AgentType.FTP.getValue(),
-			AgentStatus.WeiLianJie.getCode(),
-			AgentStatus.WeiLianJie.getValue(),
-			AgentStatus.YiLianJie.getCode(),
-			AgentStatus.YiLianJie.getValue(),
-			source_id,
-			agent_type,
-			getUserId());
+				"SELECT *,"
+						+ " ( CASE WHEN agent_type = ? THEN ? "
+						+ "   WHEN agent_type = ? THEN ?"
+						+ "   WHEN agent_type = ? THEN ? "
+						+ "   WHEN agent_type = ? THEN ? "
+						+ "   WHEN agent_type = ? THEN ? END) agent_zh_name,"
+						+ "(CASE WHEN agent_status = ? THEN ? "
+						+ "  WHEN agent_status = ? THEN ? END) connection_status"
+						+ " FROM agent_info WHERE source_id = ? AND agent_type = ? AND user_id = ?",
+				AgentType.ShuJuKu.getCode(),
+				AgentType.ShuJuKu.getValue(),
+				AgentType.DBWenJian.getCode(),
+				AgentType.DBWenJian.getValue(),
+				AgentType.DuiXiang.getCode(),
+				AgentType.DuiXiang.getValue(),
+				AgentType.WenJianXiTong.getCode(),
+				AgentType.WenJianXiTong.getValue(),
+				AgentType.FTP.getCode(),
+				AgentType.FTP.getValue(),
+				AgentStatus.WeiLianJie.getCode(),
+				AgentStatus.WeiLianJie.getValue(),
+				AgentStatus.YiLianJie.getCode(),
+				AgentStatus.YiLianJie.getValue(),
+				source_id,
+				agent_type,
+				getUserId());
 	}
 
 	@Method(
-		desc = "获取当前部署的Agent信息",
-		logicStep = "1 : 根据选择的数据源及Agent类型查询其对应的Agent信息" + "2 : 将系统默认路径放入")
+			desc = "获取当前部署的Agent信息",
+			logicStep = "1 : 根据选择的数据源及Agent类型查询其对应的Agent信息" + "2 : 将系统默认路径放入")
 	@Param(name = "agent_id", desc = "数据源ID", range = "不能为空的整数")
 	@Param(name = "agent_type", desc = "Agent类型 (AgentType)", range = "不能为空的字符串")
 	@Return(desc = "当前Agent的部署信息", range = "可以为空,因为会出现是第一次部署")
@@ -101,36 +102,37 @@ public class AgentDeployAction extends BaseAction {
 
 		/* 1 : 根据选择的数据源及Agent类型查询其对应的Agent信息 */
 		Map<String, Object> queryOneObject =
-			Dbo.queryOneObject(
-				"SELECT t2.* FROM "
-					+ Agent_info.TableName
-					+ " t1 left join "
-					+ Agent_down_info.TableName
-					+ " t2 on t1.agent_ip = t2.agent_ip AND t1.agent_port = t2.agent_port WHERE "
-					+ "t1.agent_id = ? AND t1.user_id = ?",
-				agent_id,
-				getUserId());
+				Dbo.queryOneObject(
+						"SELECT t2.* FROM "
+								+ Agent_info.TableName
+								+ " t1 left join "
+								+ Agent_down_info.TableName
+								+ " t2 on t1.agent_ip = t2.agent_ip AND t1.agent_port = t2.agent_port WHERE "
+								+ "t1.agent_id = ? AND t1.user_id = ? AND t2.agent_type=?",
+						agent_id,
+						getUserId(),
+						agent_type);
 		/* 2 : 将系统默认路径放入 */
 		queryOneObject.put(
-			"agentDeployPath", PropertyParaValue.getString("agentDeployPath", "/home/hyshf/"));
+				"agentDeployPath", PropertyParaValue.getString("agentDeployPath", "/home/hyshf/"));
 		return queryOneObject;
 	}
 
 	@Method(
-		desc = "保存此次部署Agent的信息",
-		logicStep =
-			"1 : 部署路径以页面选择的为基准,页面选择为两种(系统默认-0/自定义-1),如果为系统默认则取系统参数的路径,反之使用自定义路径"
-				+ "2 : 检查当前部署的信息是否含有Down_id,如果有表示为编辑,否则为新增")
+			desc = "保存此次部署Agent的信息",
+			logicStep =
+					"1 : 部署路径以页面选择的为基准,页面选择为两种(系统默认-0/自定义-1),如果为系统默认则取系统参数的路径,反之使用自定义路径"
+							+ "2 : 检查当前部署的信息是否含有Down_id,如果有表示为编辑,否则为新增")
 	@Param(name = "agent_down_info", desc = "保存部署Agent信息", range = "不能为空的整数", isBean = true)
 	@Param(
-		name = "customPath",
-		desc = "自定义路径",
-		range = "不能为空(0-表示系统默认路径,1相反)",
-		valueIfNull = {"0"})
+			name = "customPath",
+			desc = "自定义路径",
+			range = "不能为空(0-表示系统默认路径,1相反)",
+			valueIfNull = {"0"})
 	@Param(name = "oldAgentDir", desc = "Agent旧的部署目录", range = "可以为空,为空表示为第一次部署", nullable = true)
 	@Param(name = "oldLogPath", desc = "Agent旧日志信息", range = "可以为空,为空表示为第一次部署", nullable = true)
 	public void saveAgentDownInfo(
-		Agent_down_info agent_down_info, String customPath, String oldAgentDir, String oldLogPath) {
+			Agent_down_info agent_down_info, String customPath, String oldAgentDir, String oldLogPath) {
 
 		/* 1 : 部署路径以页面选择的为基准,页面选择为两种(系统默认-0/自定义-1),如果为系统默认则取系统参数的路径,反之使用自定义路径 */
 		if (StringUtil.isNotBlank(customPath)) {
@@ -140,17 +142,17 @@ public class AgentDeployAction extends BaseAction {
 				agent_down_info.setSave_dir(PropertyParaValue.getString("agentDeployPath", "/home/hyshf/"));
 				//将Agent的名称转换为 拼音_端口 作为Agent的目录
 				String agentDirName =
-					ChineseUtil.getPingYin(agent_down_info.getAgent_name())
-						+ "_"
-						+ agent_down_info.getAgent_port();
+						ChineseUtil.getPingYin(agent_down_info.getAgent_name())
+								+ "_"
+								+ agent_down_info.getAgent_port();
 				agent_down_info.setLog_dir(
-					PropertyParaValue.getString("agentDeployPath", "/home/hyshf/")
-						+ File.separator
-						+ agentDirName
-						+ File.separator
-						+ "running"
-						+ File.separator
-						+ "running.log");
+						PropertyParaValue.getString("agentDeployPath", "/home/hyshf/")
+								+ File.separator
+								+ agentDirName
+								+ File.separator
+								+ "running"
+								+ File.separator
+								+ "running.log");
 			}
 		}
 		// Agent开始部署
