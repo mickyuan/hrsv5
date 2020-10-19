b8e63d05ca95cd7cb0a95ed6a49bd0b7ff2c78e9
hulili930715 <78733419@qq.com>
2020-10-19 18:16:59 +0800

    DropDataTable(删除数表,添加Hive和HBase的删除实现)

--- hrds-commons/src/main/java/hrds/commons/collection/DropDataTable.java
+++ hrds-commons/src/main/java/hrds/commons/collection/DropDataTable.java
@@ -3,7 +3,6 @@ package hrds.commons.collection;
 import fd.ng.core.annotation.DocClass;
 import fd.ng.core.annotation.Method;
 import fd.ng.core.annotation.Param;
-import fd.ng.core.annotation.Return;
 import fd.ng.core.utils.StringUtil;
 import fd.ng.db.conf.Dbtype;
 import fd.ng.db.jdbc.DatabaseWrapper;
@@ -11,9 +10,12 @@ import fd.ng.db.jdbc.SqlOperator;
 import hrds.commons.codes.Store_type;
 import hrds.commons.collection.bean.LayerBean;
 import hrds.commons.entity.Data_store_layer;
-import hrds.commons.exception.AppSystemException;
 import hrds.commons.exception.BusinessException;
+import hrds.commons.hadoop.hadoop_helper.HBaseHelper;
+import hrds.commons.hadoop.readconfig.ConfigReader;
+import org.apache.hadoop.conf.Configuration;
 
+import java.io.IOException;
 import java.util.List;
 
 @DocClass(desc = "删除数据类", author = "BY-HLL", createdate = "2020/5/22 0022 下午 03:18")
@@ -37,7 +39,9 @@ public class DropDataTable {
 	public static void dropTableByDataLayer(DatabaseWrapper db, long dsl_id, String tableSpace, String tableName) {
 		//获取存储层信息
 		LayerBean layerBean = SqlOperator.queryOneObject(db, LayerBean.class, "select * from " + Data_store_layer.TableName +
-				" where dsl_id=?", dsl_id).orElseThrow(() -> (new BusinessException("获取存储层数据信息的SQL失败!")));
+			" where dsl_id=?", dsl_id).orElseThrow(() -> (new BusinessException("获取存储层数据信息的SQL失败!")));
+		//添加存储层配置信息到 layerBean
+		layerBean.setLayerAttr(ConnectionTool.getLayerMap(db, layerBean.getDsl_id()));
 		dropTableByDataLayer(db, layerBean, "", tableName);
 	}
 
@@ -50,72 +54,79 @@ public class DropDataTable {
 		try (DatabaseWrapper dbDataConn = ConnectionTool.getDBWrapper(db, layerBean.getDsl_id())) {
 			//获取当前操作存储层类型
 			Store_type store_type = Store_type.ofEnumByCode(layerBean.getStore_type());
-			//根据表名获取删除表的SQL语句
-			String dropTableSQL = getDropTableSQL(store_type, dbDataConn, tableSpace, tableName);
-			//执行删除SQL
-			int execute = SqlOperator.execute(dbDataConn, dropTableSQL);
-			//校验修改结果
-			if (execute != 0) {
-				throw new BusinessException("修改关系型数据库表失败!");
-			}
-			//提交操作
-			SqlOperator.commitTransaction(dbDataConn);
-		} catch (Exception e) {
-			throw new AppSystemException("删除表的sql执行失败!,请检查表名是否存在!", e);
-		}
-	}
-
-	@Method(desc = "获取删除表的sql", logicStep = "获取删除表的sql")
-	@Param(name = "store_type", desc = "存储层类型", range = "存储层类型")
-	@Param(name = "dbDataConn", desc = "DatabaseWrapper对象", range = "DatabaseWrapper对象")
-	@Param(name = "tableSpace", desc = "表空间", range = "String类型")
-	@Param(name = "table_name", desc = "表名", range = "表名")
-	@Return(desc = "删除表的sql", range = "删除表的sql")
-	private static String getDropTableSQL(Store_type store_type, DatabaseWrapper dbDataConn, String tableSpace, String table_name) {
-		String dropTableSQL = "";
-		//关系型数据库
-		if (store_type == Store_type.DATABASE) {
-			//DB2V1 和 DB2V2
-			if (dbDataConn.getDbtype() == Dbtype.DB2V1 || dbDataConn.getDbtype() == Dbtype.DB2V2) {
-				if (StringUtil.isBlank(tableSpace)) {
-					dropTableSQL = "DROP " + table_name;
-				} else {
-					dropTableSQL = "DROP " + tableSpace + "." + table_name;
+			//关系型数据库
+			String dropTableSQL;
+			if (store_type == Store_type.DATABASE) {
+				//DB2V1 和 DB2V2
+				if (dbDataConn.getDbtype() == Dbtype.DB2V1 || dbDataConn.getDbtype() == Dbtype.DB2V2) {
+					if (StringUtil.isBlank(tableSpace)) {
+						dropTableSQL = "DROP " + tableName;
+					} else {
+						dropTableSQL = "DROP " + tableSpace + "." + tableName;
+					}
 				}
-			}
-			//TERADATA
-			else if (dbDataConn.getDbtype() == Dbtype.TERADATA) {
-				if (StringUtil.isBlank(tableSpace)) {
-					dropTableSQL = "DROP TABLE " + table_name;
-				} else {
-					dropTableSQL = "DROP TABLE " + tableSpace + "." + table_name;
+				//TERADATA
+				else if (dbDataConn.getDbtype() == Dbtype.TERADATA) {
+					if (StringUtil.isBlank(tableSpace)) {
+						dropTableSQL = "DROP TABLE " + tableName;
+					} else {
+						dropTableSQL = "DROP TABLE " + tableSpace + "." + tableName;
+					}
 				}
-			}
-			//通用数据库语句
-			else {
-				if (StringUtil.isBlank(tableSpace)) {
-					dropTableSQL = "DROP TABLE " + table_name;
-				} else {
-					dropTableSQL = "DROP TABLE " + tableSpace + "." + table_name;
+				//通用数据库语句
+				else {
+					if (StringUtil.isBlank(tableSpace)) {
+						dropTableSQL = "DROP TABLE " + tableName;
+					} else {
+						dropTableSQL = "DROP TABLE " + tableSpace + "." + tableName;
+					}
 				}
-			}
-		} else if (store_type == Store_type.HIVE) {
-			if (StringUtil.isBlank(tableSpace)) {
-				dropTableSQL = "DROP TABLE " + table_name;
+				//执行删除SQL
+				int execute = SqlOperator.execute(dbDataConn, dropTableSQL);
+				//校验修改结果
+				if (execute != 0) {
+					throw new BusinessException("修改关系型数据库表失败!");
+				}
+				//提交操作
+				SqlOperator.commitTransaction(dbDataConn);
+			} else if (store_type == Store_type.HIVE) {
+				if (StringUtil.isBlank(tableSpace)) {
+					dropTableSQL = "DROP TABLE " + tableName;
+				} else {
+					dropTableSQL = "DROP TABLE " + tableSpace + "." + tableName;
+				}
+				//执行删除SQL
+				int execute = SqlOperator.execute(dbDataConn, dropTableSQL);
+				//校验修改结果
+				if (execute != 0) {
+					throw new BusinessException("删除Hive存储类型的数据表失败!");
+				}
+			} else if (store_type == Store_type.HBASE) {
+				//根据存储层信息获取存储层的 Configuration
+				Configuration conf = ConfigReader.getConfiguration(layerBean);
+				//获取HBaseHelper
+				try (HBaseHelper helper = HBaseHelper.getHelper(conf)) {
+					//1.判断需要重命名的表是否存在,如果存在则判断修改后的表名是否存在
+					if (helper.existsTable(tableName)) {
+						//重命名表
+						helper.dropTable(tableName);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+					throw new BusinessException("删除HBase存储类型的数据表失败!");
+				}
+			} else if (store_type == Store_type.SOLR) {
+				//TODO 删除 SOLR 层表配置暂未实现!
+				throw new BusinessException("删除 SOLR 层表配置暂未实现!!");
+			} else if (store_type == Store_type.ElasticSearch) {
+				//TODO 删除 ElasticSearch 层表配置暂未实现!
+				throw new BusinessException("删除 ElasticSearch 层表配置暂未实现!!");
+			} else if (store_type == Store_type.MONGODB) {
+				//TODO 删除 MONGODB 层表配置暂未实现!
+				throw new BusinessException("删除 MONGODB 层表配置暂未实现!!");
 			} else {
-				dropTableSQL = "DROP TABLE " + tableSpace + "." + table_name;
+				throw new BusinessException("获取删除存储层表SQL时,未找到匹配的存储层类型!");
 			}
-		} else if (store_type == Store_type.HBASE) {
-			//TODO 删除 HBASE 层表配置暂未实现!
-		} else if (store_type == Store_type.SOLR) {
-			//TODO 删除 SOLR 层表配置暂未实现!
-		} else if (store_type == Store_type.ElasticSearch) {
-			//TODO 删除 ElasticSearch 层表配置暂未实现!
-		} else if (store_type == Store_type.MONGODB) {
-			//TODO 删除 MONGODB 层表配置暂未实现!
-		} else {
-			throw new BusinessException("获取删除存储层表SQL时,未找到匹配的存储层类型!");
 		}
-		return dropTableSQL;
 	}
 }
