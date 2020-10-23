9b308989e0a5c08edf8bc38d0a569bf8859c002b
MickYuan <384077767@qq.com>
2020-10-23 17:44:12 +0800

    Update SparkJobRunner.java

--- hrds_H/src/main/java/hrds/h/biz/spark/running/SparkJobRunner.java
+++ hrds_H/src/main/java/hrds/h/biz/spark/running/SparkJobRunner.java
@@ -95,7 +95,8 @@ public class SparkJobRunner {
      * @return 合法参数字符串
      */
     static String obtainSatisfyShellString(String convertedShellArg) {
-        return convertedShellArg.replace("^", "\"");
+        return convertedShellArg.replace("\"", "")
+                .replace("^", "\"");
     }
 
 }
