1808a75a7ed716ef2324941c772656029847ef29
hulili930715 <78733419@qq.com>
2020-11-06 15:58:15 +0800

    GetApiInfo(添加生成接口文档类)

---
+++ hrds_F/src/main/java/hrds/GetApiInfo.java
@@ -0,0 +1,10 @@
+package hrds;
+
+import fd.ng.core.conf.AppinfoConf;
+import hrds.commons.ParsingAnnotaion;
+
+public class GetApiInfo {
+    public static void main(String[] args) {
+        ParsingAnnotaion.main(new String[]{AppinfoConf.ProjectId});
+    }
+}
