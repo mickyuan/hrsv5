575511be4a211758caa13cade8b4b33c6a2732ba
Qiang-Lee <hliqiang1989@gmail.com>
2020-11-18 16:50:27 +0800

    去除多余的引用

--- hrds_H/src/main/java/hrds/h/biz/scriptsql/SqlScriptAction.java
+++ hrds_H/src/main/java/hrds/h/biz/scriptsql/SqlScriptAction.java
@@ -1,20 +1,11 @@
 package hrds.h.biz.scriptsql;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
 import fd.ng.core.annotation.DocClass;
 import fd.ng.core.annotation.Method;
 import fd.ng.core.annotation.Param;
-import fd.ng.core.utils.JsonUtil;
-import fd.ng.db.jdbc.DatabaseWrapper;
 import hrds.commons.base.BaseAction;
-import hrds.commons.entity.Datatable_field_info;
 import hrds.h.biz.config.MarketConf;
-import hrds.h.biz.config.MarketConfUtils;
-import hrds.h.biz.market.MarketData;
 import hrds.h.biz.realloader.Utils;
-import java.util.List;
-import java.util.Map;
 
 @DocClass(desc = "加工SQL脚本生成类", author = "Mr.Lee", createdate = "2020-11-17 16:22")
 public class SqlScriptAction extends BaseAction {
