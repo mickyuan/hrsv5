f9dca5292ef4f96993011940de49958bc023f521
SHU_TBH <410297778@qq.com>
2020-12-07 16:54:41 +0800

    修改采集时判断表名是否重复的逻辑

--- hrds_B/src/main/java/hrds/b/biz/agent/resourcerecod/tableregister/TableRegisterAction.java
+++ hrds_B/src/main/java/hrds/b/biz/agent/resourcerecod/tableregister/TableRegisterAction.java
@@ -39,6 +39,7 @@ import hrds.commons.utils.AgentActionUtil;
 import hrds.commons.utils.Constant;
 import hrds.commons.utils.DboExecute;
 import hrds.commons.utils.key.PrimayKeyGener;
+
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -47,11 +48,11 @@ import java.util.UUID;
 public class TableRegisterAction extends BaseAction {
 
 	@Method(desc = "保存选择的表信息", logicStep = ""
-		+ "1: 检查认为的信息是否存在 "
-		+ "2: 如果自定义的列信息不为空,将表对应的列信息解析出来 "
-		+ "3: 循环集合校验不可为空字段信息 "
-		+ "4: 如果存在自定义列信息则保存自定义列信息,反之保存表的默认字段信息"
-		+ "5; 保存表关联的储存层数据信息"
+			+ "1: 检查认为的信息是否存在 "
+			+ "2: 如果自定义的列信息不为空,将表对应的列信息解析出来 "
+			+ "3: 循环集合校验不可为空字段信息 "
+			+ "4: 如果存在自定义列信息则保存自定义列信息,反之保存表的默认字段信息"
+			+ "5; 保存表关联的储存层数据信息"
 	)
 	@Param(name = "databaseId", desc = "采集任务ID", range = "不可为空")
 	@Param(name = "source_id", desc = "数据源ID", range = "不可为空")
@@ -60,7 +61,7 @@ public class TableRegisterAction extends BaseAction {
 	@Param(name = "tableColumns", desc = "表对应的列字段信息,格式如: {表名称:[{列信息1},{列信息2},......]}", range = "不可为空", nullable = true)
 	@Param(name = "dsl_id", desc = "表对应储存层ID", range = "不可为空")
 	public void saveTableData(long source_id, long agent_id, long databaseId, Table_info[] tableInfos, String tableColumns,
-		long dsl_id) {
+							  long dsl_id) {
 
 		//1: 检查认为的信息是否存在
 		Object collect_type = checkDatabaseSetExist(databaseId);
@@ -81,9 +82,9 @@ public class TableRegisterAction extends BaseAction {
 					CheckParam.throwErrorMsg("表名称(%s)未设置列信息", tableInfo.getTable_name());
 				}
 				tableColumnList = JSON
-					.parseObject(tableColumnObj.get(tableInfo.getTable_name()).toString(),
-						new TypeReference<List<Table_column>>() {
-						});
+						.parseObject(tableColumnObj.get(tableInfo.getTable_name()).toString(),
+								new TypeReference<List<Table_column>>() {
+								});
 			} else {
 				tableColumnList = databaseTableColumnInfo(databaseId, tableInfo.getTable_name());
 			}
@@ -97,16 +98,16 @@ public class TableRegisterAction extends BaseAction {
 		//修改此次任务的状态信息
 		if (!collect_type.equals(CollectType.ShuJuKuCaiJi.getCode())) {
 			DboExecute
-				.updatesOrThrow("更新的数据超出了范围",
-					"UPDATE " + Database_set.TableName + " SET is_sendok = ? WHERE database_id = ?",
-					IsFlag.Shi.getCode(),
-					databaseId);
+					.updatesOrThrow("更新的数据超出了范围",
+							"UPDATE " + Database_set.TableName + " SET is_sendok = ? WHERE database_id = ?",
+							IsFlag.Shi.getCode(),
+							databaseId);
 		}
 	}
 
 	@Method(desc = "获取全表的信息", logicStep = ""
-		+ "1: 检查当前任务是否存在 "
-		+ "2: 查询任务存在的表信息,返回表集合信息")
+			+ "1: 检查当前任务是否存在 "
+			+ "2: 查询任务存在的表信息,返回表集合信息")
 	@Param(name = "databaseId", desc = "采集任务的ID", range = "不可为空")
 	@Return(desc = "返回当前任务存储层链接下的表信息", range = "为空表示没有该表信息")
 	public List<Table_info> getTableData(long databaseId) {
@@ -116,45 +117,48 @@ public class TableRegisterAction extends BaseAction {
 
 		//2: 查询任务存在的表信息,返回表集合信息
 		return Dbo
-			.queryList(Table_info.class, "SELECT * FROM " + Table_info.TableName + " WHERE database_id = ? ",
-				databaseId);
+				.queryList(Table_info.class, "SELECT * FROM " + Table_info.TableName + " WHERE database_id = ? ",
+						databaseId);
 
 	}
 
 	private Object checkDatabaseSetExist(long databaseId) {
 		//1: 检查当前任务是否存在
 		long countNum = Dbo
-			.queryNumber("SELECT COUNT(1) FROM " + Database_set.TableName + " WHERE database_id = ?",
-				databaseId)
-			.orElseThrow(() -> new BusinessException("SQL查询异常"));
+				.queryNumber("SELECT COUNT(1) FROM " + Database_set.TableName + " WHERE database_id = ?",
+						databaseId)
+				.orElseThrow(() -> new BusinessException("SQL查询异常"));
 		if (countNum == 0) {
 			CheckParam.throwErrorMsg("任务ID(%s)不存在", databaseId);
 		}
 
 		return Dbo.queryOneObject("SELECT collect_type FROM " + Database_set.TableName + " WHERE database_id = ?",
-			databaseId).get("collect_type");
+				databaseId).get("collect_type");
 	}
 
 	@Method(desc = "", logicStep = ""
-		+ "1: 保存表存储信息"
-		+ "2: 保存表的储存关系信息"
-		+ "3: 记录数据表的存储登记")
+			+ "1: 保存表存储信息"
+			+ "2: 保存表的储存关系信息"
+			+ "3: 记录数据表的存储登记")
 	private void saveStorageData(long source_id, long agent_id, long databaseId, long dsl_id, Table_info tableInfo) {
 
 		//获取任务的分类和数据源编号信息
 		Map<String, Object> classifyAndSourceNum = getClassifyAndSourceNum(databaseId);
 		String hyren_name =
-			String
-				.format("%s_%s_%s",
-					classifyAndSourceNum.get("datasource_number"), classifyAndSourceNum.get("classify_num"),
-					tableInfo.getTable_name());
+				String
+						.format("%s_%s_%s",
+								classifyAndSourceNum.get("datasource_number"), classifyAndSourceNum.get("classify_num"),
+								tableInfo.getTable_name());
+//		long countNum = Dbo
+//			.queryNumber("SELECT COUNT(1) FROM " + Data_store_reg.TableName + " WHERE hyren_name = ? AND database_id = ?",
+//				hyren_name, databaseId).orElseThrow(() -> new BusinessException("SQL查询异常"));
 		long countNum = Dbo
-			.queryNumber("SELECT COUNT(1) FROM " + Data_store_reg.TableName + " WHERE hyren_name = ? AND database_id = ?",
-				hyren_name, databaseId).orElseThrow(() -> new BusinessException("SQL查询异常"));
+				.queryNumber("SELECT COUNT(1) FROM " + Data_store_reg.TableName + " WHERE hyren_name = ? ",
+						hyren_name).orElseThrow(() -> new BusinessException("SQL查询异常"));
 		if (countNum != 0) {
 			CheckParam.throwErrorMsg("数据源(%s),分类(%s)下已存在当前表(%s)", classifyAndSourceNum.get("datasource_number"),
-				classifyAndSourceNum.get("classify_num"),
-				tableInfo.getTable_name());
+					classifyAndSourceNum.get("classify_num"),
+					tableInfo.getTable_name());
 		}
 		//5: 保存表存储信息
 		Table_storage_info table_storage_info = new Table_storage_info();
@@ -206,9 +210,9 @@ public class TableRegisterAction extends BaseAction {
 	void checkColumnInformation(Table_column table_column) {
 		Validator.notBlank(table_column.getColumn_name(), "列名称不能为空");
 		Validator
-			.notBlank(table_column.getColumn_ch_name(), String.format("列(%s)中文名称不能为空", table_column.getColumn_name()));
+				.notBlank(table_column.getColumn_ch_name(), String.format("列(%s)中文名称不能为空", table_column.getColumn_name()));
 		Validator
-			.notBlank(table_column.getIs_primary_key(), String.format("列(%s)主键信息不能为空", table_column.getColumn_name()));
+				.notBlank(table_column.getIs_primary_key(), String.format("列(%s)主键信息不能为空", table_column.getColumn_name()));
 
 	}
 
@@ -218,31 +222,31 @@ public class TableRegisterAction extends BaseAction {
 	private List<Table_column> databaseTableColumnInfo(long databaseId, String table_name) {
 
 		long databaseNum =
-			Dbo.queryNumber(
-				"SELECT COUNT(1) FROM " + Database_set.TableName + " WHERE database_id = ?",
-				databaseId)
-				.orElseThrow(() -> new BusinessException("SQL查询异常"));
+				Dbo.queryNumber(
+						"SELECT COUNT(1) FROM " + Database_set.TableName + " WHERE database_id = ?",
+						databaseId)
+						.orElseThrow(() -> new BusinessException("SQL查询异常"));
 		if (databaseNum == 0) {
 			throw new BusinessException("任务(" + databaseId + ")不存在!!!");
 		}
 		// 1、根据colSetId和userId去数据库中查出DB连接信息
 		Map<String, Object> databaseSetInfo = Dbo.queryOneObject(
-			" select t1.database_type, t1.database_ip, t1.database_port, t1.database_name, "
-				+ " t1.database_pad, t1.user_name, t1.database_drive, t1.jdbc_url, t1.agent_id, t1.db_agent, t1.plane_url"
-				+ " from "
-				+ Database_set.TableName
-				+ " t1 "
-				+ " join "
-				+ Agent_info.TableName
-				+ " ai on ai.agent_id = t1.agent_id"
-				+ " where t1.database_id = ? and ai.user_id = ? ",
-			databaseId,
-			getUserId());
+				" select t1.database_type, t1.database_ip, t1.database_port, t1.database_name, "
+						+ " t1.database_pad, t1.user_name, t1.database_drive, t1.jdbc_url, t1.agent_id, t1.db_agent, t1.plane_url"
+						+ " from "
+						+ Database_set.TableName
+						+ " t1 "
+						+ " join "
+						+ Agent_info.TableName
+						+ " ai on ai.agent_id = t1.agent_id"
+						+ " where t1.database_id = ? and ai.user_id = ? ",
+				databaseId,
+				getUserId());
 		long agent_id = Long.parseLong(databaseSetInfo.get("agent_id").toString());
 		// 2、封装数据，调用方法和agent交互，获取列信息
 		String respMsg =
-			SendMsgUtil.getColInfoByTbName(
-				agent_id, getUserId(), databaseSetInfo, table_name, AgentActionUtil.GETTABLECOLUMN);
+				SendMsgUtil.getColInfoByTbName(
+						agent_id, getUserId(), databaseSetInfo, table_name, AgentActionUtil.GETTABLECOLUMN);
 		// 3、将列信息反序列化为Json数组
 		return JSON.parseObject(respMsg, new TypeReference<List<Table_column>>() {
 		});
@@ -254,7 +258,7 @@ public class TableRegisterAction extends BaseAction {
 		tableColumnList.forEach(table_column -> {
 			Validator.notBlank(table_column.getColumn_name(), String.format("表(%s)的列名称未设置", table_name));
 			Validator.notBlank(table_column.getColumn_ch_name(),
-				String.format("表(%s)的列(%s)中文名称未设置", table_name, table_column.getColumn_name()));
+					String.format("表(%s)的列(%s)中文名称未设置", table_name, table_column.getColumn_name()));
 			//设置默认数据信息
 			setTableColumnDefaultData(table_id, table_column);
 			//新增进去
@@ -273,10 +277,10 @@ public class TableRegisterAction extends BaseAction {
 	}
 
 	@Method(desc = "保存编辑选择的表信息", logicStep = ""
-		+ "1: 检查认为的信息是否存在 "
-		+ "2: 如果自定义的列信息不为空,将表对应的列信息解析出来 "
-		+ "3: 循环集合校验不可为空字段信息 "
-		+ "4: 保存表的信息")
+			+ "1: 检查认为的信息是否存在 "
+			+ "2: 如果自定义的列信息不为空,将表对应的列信息解析出来 "
+			+ "3: 循环集合校验不可为空字段信息 "
+			+ "4: 保存表的信息")
 	@Param(name = "databaseId", desc = "采集任务ID", range = "不可为空")
 	@Param(name = "tableInfos", desc = "采集表信息集合", range = "不可为空", isBean = true)
 	@Param(name = "source_id", desc = "数据源ID", range = "不可为空")
@@ -284,7 +288,7 @@ public class TableRegisterAction extends BaseAction {
 	@Param(name = "dsl_id", desc = "表对应储存层ID", range = "不可为空")
 	@Param(name = "tableColumns", desc = "表对应的列字段信息", range = "不可为空", nullable = true)
 	public void updateTableData(long source_id, long agent_id, long databaseId, long dsl_id, Table_info[] tableInfos,
-		String tableColumns) {
+								String tableColumns) {
 
 		//1: 检查认为的信息是否存在
 		Object collect_type = checkDatabaseSetExist(databaseId);
@@ -292,13 +296,13 @@ public class TableRegisterAction extends BaseAction {
 		if (!collect_type.equals(CollectType.ShuJuKuCaiJi.getCode())) {
 			//删除数据表存储关系表
 			Dbo.execute("DELETE FROM " + Dtab_relation_store.TableName + " WHERE tab_id in (SELECT storage_id FROM "
-				+ Table_storage_info.TableName
-				+ " WHERE table_id in (SELECT table_id FROM "
-				+ Table_info.TableName
-				+ " WHERE database_id = ?))", databaseId);
+					+ Table_storage_info.TableName
+					+ " WHERE table_id in (SELECT table_id FROM "
+					+ Table_info.TableName
+					+ " WHERE database_id = ?))", databaseId);
 			//删除表存储信息
 			Dbo.execute("DELETE FROM " + Table_storage_info.TableName + " WHERE table_id in (SELECT table_id FROM "
-				+ Table_info.TableName + " WHERE database_id = ?)", databaseId);
+					+ Table_info.TableName + " WHERE database_id = ?)", databaseId);
 		}
 		//删除表信息
 		Dbo.execute("DELETE FROM " + Table_info.TableName + " WHERE database_id = ?", databaseId);
@@ -324,9 +328,9 @@ public class TableRegisterAction extends BaseAction {
 						CheckParam.throwErrorMsg("表名称(%s)未设置列信息", tableInfo.getTable_name());
 					}
 					tableColumnList = JSON
-						.parseObject(tableColumnObj.get(tableInfo.getTable_name()).toString(),
-							new TypeReference<List<Table_column>>() {
-							});
+							.parseObject(tableColumnObj.get(tableInfo.getTable_name()).toString(),
+									new TypeReference<List<Table_column>>() {
+									});
 				} else {
 					tableColumnList = databaseTableColumnInfo(databaseId, tableInfo.getTable_name());
 				}
@@ -347,14 +351,14 @@ public class TableRegisterAction extends BaseAction {
 					if (tableColumnObj != null && tableColumnObj.containsKey(tableInfo.getTable_name())) {
 						//直接解析列信息,如果有重新定义则能解析出新的,否则不做列的任何更改
 						tableColumnList = JSON
-							.parseObject(tableColumnObj.get(tableInfo.getTable_name()).toString(),
-								new TypeReference<List<Table_column>>() {
-								});
+								.parseObject(tableColumnObj.get(tableInfo.getTable_name()).toString(),
+										new TypeReference<List<Table_column>>() {
+										});
 						updateTableColumn(tableInfo.getTable_id(), tableColumnList);
 					}
 					long countNum = Dbo
-						.queryNumber("SELECT COUNT(1) FROM " + Data_extraction_def.TableName + " WHERE table_id = ?",
-							tableInfo.getTable_id()).orElseThrow(() -> new BusinessException("SQL错误"));
+							.queryNumber("SELECT COUNT(1) FROM " + Data_extraction_def.TableName + " WHERE table_id = ?",
+									tableInfo.getTable_id()).orElseThrow(() -> new BusinessException("SQL错误"));
 					//如果是数据采集的表保存,则需要默认增加一个抽取定义(因为这里和贴源登记是公用的)
 					if (countNum == 0) {
 						Data_extraction_def extraction_def = new Data_extraction_def();
@@ -416,8 +420,8 @@ public class TableRegisterAction extends BaseAction {
 	}
 
 	@Method(desc = "根据表的ID更新表的列信息", logicStep = ""
-		+ "1: 更新时,先删除上次的字段信息 "
-		+ "2: 将此次新的数据信息增加进来,如果新的字段没有column_id则表示为新的字段,则设置默认的数据信息")
+			+ "1: 更新时,先删除上次的字段信息 "
+			+ "2: 将此次新的数据信息增加进来,如果新的字段没有column_id则表示为新的字段,则设置默认的数据信息")
 	@Param(name = "tableColumns", desc = "列的数据信息", range = "列信息的实体集合信息", isBean = true)
 	@Param(name = "table_id", desc = "表的ID", range = "表主键信息，不可为空")
 	void updateTableColumn(long table_id, List<Table_column> tableColumnList) {
@@ -439,13 +443,13 @@ public class TableRegisterAction extends BaseAction {
 	Map<String, Object> getClassifyAndSourceNum(long database_id) {
 
 		return Dbo.queryOneObject("SELECT t2.classify_num,t4.datasource_number FROM "
-			+ Database_set.TableName
-			+ " t1 JOIN "
-			+ Collect_job_classify.TableName
-			+ " t2 ON t1.classify_id = t2.classify_id JOIN "
-			+ Agent_info.TableName
-			+ " t3 ON t1.agent_id = t3.agent_id JOIN "
-			+ Data_source.TableName
-			+ " t4 ON t3.source_id = t4.source_id WHERE t1.database_id = ?", database_id);
+				+ Database_set.TableName
+				+ " t1 JOIN "
+				+ Collect_job_classify.TableName
+				+ " t2 ON t1.classify_id = t2.classify_id JOIN "
+				+ Agent_info.TableName
+				+ " t3 ON t1.agent_id = t3.agent_id JOIN "
+				+ Data_source.TableName
+				+ " t4 ON t3.source_id = t4.source_id WHERE t1.database_id = ?", database_id);
 	}
 }
